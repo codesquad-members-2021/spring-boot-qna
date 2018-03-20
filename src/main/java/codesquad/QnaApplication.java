f73faff6afbcc0b3374c680777edd09663fe5c07
javajigi <javajigi@gmail.com>
2018-03-20 15:33:09 +0900

    initial commit

---
+++ src/main/java/codesquad/QnaApplication.java
@@ -0,0 +1,11 @@
+package codesquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QnaApplication.class, args);
+    }
+}
