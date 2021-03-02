421063d1ddd0b0bb46506d451633abfaa81c6008
MJbae <gently.gogo@gmail.com>
2021-03-02 17:16:49 +0900

    Feat: User Domain 구현

---
+++ src/main/java/com/codessquad/qna/domain/User.java
@@ -0,0 +1,40 @@
+package com.codessquad.qna.domain;
+
+public class User {
+    private String userID;
+    private String password;
+    private String name;
+    private String email;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
