c3f546c6a45d656ceeb487299a8061bd15ca546a
tmdgusya <tmdgusya@naver.com>
2021-03-02 17:55:09 +0900

    fix : User Service getUser 또한 매개변수 타입 int->String 변경

--- src/main/java/com/codessquad/qna/service/UserService.java
+++ src/main/java/com/codessquad/qna/service/UserService.java
@@ -2,16 +2,17 @@ package com.codessquad.qna.service;
 
 import com.codessquad.qna.entity.User;
 import com.codessquad.qna.repository.UserRepository;
-import com.codessquad.qna.repository.UserRepositoryList;
+import com.codessquad.qna.repository.UserRepositoryMap;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
 public class UserService {
 
-    private final UserRepository userRepository = new UserRepositoryList(new ArrayList<>());
+    private final UserRepository userRepository = new UserRepositoryMap(new HashMap<>());
 
     public void save(User user) {
         userRepository.save(user);
@@ -19,6 +20,10 @@ public class UserService {
 
     public List<User> getUsers() {
         return userRepository.getUsers();
+    }
+
+    public User getUser(String userId) {
+        return userRepository.getUser(userId);
     }
 
 }
