0bcec097e952c7859d0b1ddbea1f965470d40a96
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-02 18:52:49 +0900

    Add getForm in UserController

--- src/main/java/com/codessquad/qna/user/UserController.java
+++ src/main/java/com/codessquad/qna/user/UserController.java
@@ -1,9 +1,14 @@
 package com.codessquad.qna.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    @GetMapping("/form")
+    public String getForm() {
+        return "/user/form.html";
+    }
 }
