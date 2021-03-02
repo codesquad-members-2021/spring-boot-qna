50a237e5b1860f9366e8f5a664d4a18289c4ea24
tmdgusya <tmdgusya@naver.com>
2021-03-02 17:15:17 +0900

    feat : add function CreateAccount (Sign up)
    * 요구사항에서 주어진 form.html 에서 POST 방식으로 가입에 필요한 요소인 id, password, name, email 을 입력하고 가입할 시 해당 userService 의 save 를 통해서 UserRepository 에 저장될 수 있도록 하였습니다.

---
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.codessquad.qna.controller;
+
+import com.codessquad.qna.entity.User;
+import com.codessquad.qna.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    /**
+     * @param userId
+     * @param password
+     * @param name
+     * @param email
+     * @return redirect User list (/users)
+     */
+    @PostMapping("/user")
+    public String createAccount(String userId, String password, String name, String email) {
+        User user = new User(userId, password, name, email);
+        userService.save(user);
+        logger.info(user.toString());
+        return "redirect:/users";
+    }
+
+}
