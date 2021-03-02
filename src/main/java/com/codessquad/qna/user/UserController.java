563f3c390c72e0ceba12f2ea81a59fa8437670c3
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-02 18:51:31 +0900

    Create UserController

---
+++ src/main/java/com/codessquad/qna/user/UserController.java
@@ -0,0 +1,9 @@
+package com.codessquad.qna.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+}
