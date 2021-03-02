f53ba14f7cd4b7ac031de06d42b9d4091fc1ace9
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-02 19:40:15 +0900

    Add createUser in UserController
    model mapping 위해 form name 수정, User 추가

---
+++ src/main/java/com/codessquad/qna/user/User.java
@@ -0,0 +1,60 @@
+package com.codessquad.qna.user;
+
+public class User {
+    private long userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public User() {
+    }
+
+    public User(long userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/codessquad/qna/user/UserController.java
+++ src/main/java/com/codessquad/qna/user/UserController.java
@@ -1,13 +1,18 @@
 package com.codessquad.qna.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping
     public String getUsers() {
         return "/user/list.html";
@@ -16,5 +21,11 @@ public class UserController {
     @GetMapping("/form")
     public String getForm() {
         return "/user/form.html";
+    }
+
+    @PostMapping
+    public String createUser(User user) {
+        logger.debug(user.toString());
+        return "redirect:/users";
     }
 }
