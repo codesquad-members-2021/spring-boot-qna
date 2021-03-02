8a9ab351f29f1c74dba22a8d6d791f1ed4d6a0cf
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-02 19:39:13 +0900

    Add getUsers in UserController

--- src/main/java/com/codessquad/qna/user/UserController.java
+++ src/main/java/com/codessquad/qna/user/UserController.java
@@ -4,9 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    @GetMapping
+    public String getUsers() {
+        return "/user/list.html";
+    }
+
     @GetMapping("/form")
     public String getForm() {
         return "/user/form.html";
