743de50e398682910b9705a9a6cfb44e861928dd
MJbae <gently.gogo@gmail.com>
2021-03-02 17:41:19 +0900

    Feat: 회원가입 폼에 대한 요청에 대한 처리방식 변경(static -> MVC)
    1. static .html file 이동(static/user -> templates/users)
    2. index.html 내 회원가입에 대한 a tag 클릭에 따른 요청 url 변경(user/form.html -> user/form)
    3. userController에서 /user/form 요청에 대해 template 파일의 이름값 반환(user/form)

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -1,2 +1,13 @@
-package com.codessquad.qna.controller;public class UserController {
-}
+    package com.codessquad.qna.controller;
+
+    import org.springframework.stereotype.Controller;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.PostMapping;
+
+    @Controller
+    public class UserController {
+        @GetMapping("/user/form")
+        public String createForm() {
+            return "users/form";
+        }
+    }
