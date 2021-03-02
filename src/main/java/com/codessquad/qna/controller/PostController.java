9b72393ef89fdd3d2e31f69cd9d736e95ef7b245
tmdgusya <tmdgusya@naver.com>
2021-03-02 22:17:54 +0900

    fix : JavaDoc 추가

--- src/main/java/com/codessquad/qna/controller/PostController.java
+++ src/main/java/com/codessquad/qna/controller/PostController.java
@@ -16,6 +16,13 @@ public class PostController {
     @Autowired
     PostService postService;
 
+    /**
+     * 질문 게시글을 게시판에 등록합니다.
+     * @param writer
+     * @param title
+     * @param contents
+     * @return
+     */
     @PostMapping("/questions")
     public String addPost(@RequestParam String writer, @RequestParam String title, @RequestParam String contents) {
         Post post = new Post(writer, title, contents);
@@ -23,12 +30,23 @@ public class PostController {
         return "redirect:/";
     }
 
+    /**
+     * Repository 에 있는 모든 게시물을 불러옵니다.
+     * @param model
+     * @return
+     */
     @GetMapping("/")
     public String getAllPosts(Model model) {
         model.addAttribute("posts", postService.getPosts());
         return "index";
     }
 
+    /**
+     * 매개변수로 오는 id 값을 기반으로 해당 포스트를 불러옵니다.
+     * @param id
+     * @param model
+     * @return
+     */
     @GetMapping("/questions/{id}")
     public String getPost(@PathVariable int id, Model model) {
         model.addAttribute("post", postService.getPost(id-1));
