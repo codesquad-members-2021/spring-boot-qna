28cb340329cbb8fbdbf5b0e4ec2fdb859dce17fa
tmdgusya <tmdgusya@naver.com>
2021-03-02 17:54:24 +0900

    fix : 찾아오는 방식에 따른 자료구조 변경
    처음에는 List 로 구현하려했으나, 요구사항이 id 로 해당 user 를 가져오는 요구사항일시 제일 최적회된 자료구조는 Map 이라고 생각하여 해당 자료구조를 List -> Map 으로 변경하였습니다. List 로 한다면 n 번째까지 전부 순회하여 해당 id 가 있는지 판단하고 그 index 에서 추출해내는 것인데, map 의 경우 id 로 받아오면 바로 get 이 가능하므로, 시간복잡도상에서 차이가 난다고 생각하여 map 자료구조로 변경하였습니다.

---
+++ src/main/java/com/codessquad/qna/repository/UserRepositoryMap.java
@@ -0,0 +1,34 @@
+package com.codessquad.qna.repository;
+
+import com.codessquad.qna.entity.User;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class UserRepositoryMap implements UserRepository{
+
+    private final Map<String, User> users;
+
+    public UserRepositoryMap(Map<String, User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public void save(User user) {
+        users.put(user.getId(), user);
+    }
+
+    @Override
+    public User getUser(String userId) {
+        return users.get(userId);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+}
