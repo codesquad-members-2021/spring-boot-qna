ba713ddeac868eedc7862ee831055f93b91ee747
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-02 16:21:55 +0900

    더 이상 필요없는 로그를 출력하는 코드를 지워라

--- src/main/java/com/codessquad/qna/web/user/UserController.java
+++ src/main/java/com/codessquad/qna/web/user/UserController.java
@@ -1,7 +1,5 @@
 package com.codessquad.qna.web.user;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +11,12 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    Logger logger = LoggerFactory.getLogger(UserController.class);
-
     private List<User> userList = new ArrayList<>();
 
     @PostMapping("user/create")
     public String onRegister(String userId, String password, String name, String email) {
-        logger.info("onRegister called");
         User createdUser = new User(userId, password, name, email);
-        logger.info("createdUser : " + createdUser);
         userList.add(createdUser);
-        logger.info("userList.size()" + userList.size());
-
         return "redirect:/user/list";
     }
 
