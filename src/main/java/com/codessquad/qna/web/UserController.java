3fe763ea314fea340317cd7c20d279ee096fa322
95degree <95degree@naver.com>
2021-03-03 15:14:07 +0900

    Post방식으로 변경
    GET으로 받아오면 url에 유저에 정보가 보여 이를 방지하기 위해 Post방식으로 변경했습니다.

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -1,5 +1,13 @@
 package com.codessquad.qna.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class UserController {
+    @PostMapping("/user/create")
+    public String create(String userId, String password, String name, String email){
+        System.out.println("userId : " + userId);
+        return "index";
+    }
 }
