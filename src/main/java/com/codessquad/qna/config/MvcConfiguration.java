484282a0976e9aa06817b2a47c55a9cf29f23853
JoonKi-Kim <joonki402@gmail.com>
2021-03-04 03:26:58 +0900

    html 중복 제거
    중복되는 html heade, navbar, script 중복 제거

--- src/main/java/com/codessquad/qna/config/MvcConfiguration.java
+++ src/main/java/com/codessquad/qna/config/MvcConfiguration.java
@@ -1,4 +1,20 @@
 package com.codessquad.qna.config;
 
-public class MvcConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        registry.addViewController("/users/form").setViewName("user/form");
+        registry.addViewController("/users/login").setViewName("user/login");
+        registry.addViewController("/users").setViewName("user/list");
+    }
+
 }
