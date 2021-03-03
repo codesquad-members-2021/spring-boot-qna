26c52a43e506d93a7a485db88223abb089dbd474
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-03 15:25:14 +0900

    Update createUser in UserController for add and return users
    사용자 저장 위한 List 추가. 저장 완료 시 getUsers로 리디렉트 하여 목록을 뷰로 전달함.

--- src/main/java/com/codessquad/qna/user/UserController.java
+++ src/main/java/com/codessquad/qna/user/UserController.java
@@ -6,26 +6,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
     Logger logger = LoggerFactory.getLogger(this.getClass());
+    private List<User> users = Collections.synchronizedList(new ArrayList<>(Arrays.asList(
+            new User("javajigi", "1234", "자바지기", "javajigi@sample.net"),
+            new User("slipp", "1234", "슬립", "slipp@sample.net")
+    )));
 
     @GetMapping
-    public String getUsers() {
-        return "/user/list.html";
+    public ModelAndView getUsers() {
+        return new ModelAndView("/user/list", "users", users);
     }
 
     @GetMapping("/form")
     public String getForm() {
-        return "/user/form.html";
+        return "/user/form";
     }
 
     @PostMapping
     public String createUser(User user) {
         logger.debug(user.toString());
+        users.add(user);
+
         return "redirect:/users";
     }
 }
