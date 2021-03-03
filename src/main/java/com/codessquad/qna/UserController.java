24c296f3dccebad72ea0ed1f1bd319d8d6c2a653
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-03-03 15:49:53 +1300

    사용자를 ArrayList에 추가

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -3,12 +3,18 @@ package com.codessquad.qna;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
+
+    private List<User> userList = new ArrayList<>();
 
     @PostMapping("/user/create")
     public String create(User user) {
         System.out.println("user: " + user.toString());
+        userList.add(user);
         return "welcome";
     }
 }
