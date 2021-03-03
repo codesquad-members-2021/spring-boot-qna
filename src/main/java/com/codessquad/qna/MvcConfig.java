ccdee7007b872f61c2fce69f1bfbe79948e536b2
tmdgusya <tmdgusya@naver.com>
2021-03-03 23:31:14 +0900

    feat : @Component Scan 추가
    - 각 Class 파일에 등록된 Component 들을 Scan 할 수 있도록 ComponentScan 어노테이션을 추가하였습니다.

--- src/main/java/com/codessquad/qna/MvcConfig.java
+++ src/main/java/com/codessquad/qna/MvcConfig.java
@@ -1,11 +1,13 @@
-package com.codessquad.qna.config;
+package com.codessquad.qna;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@ComponentScan
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
