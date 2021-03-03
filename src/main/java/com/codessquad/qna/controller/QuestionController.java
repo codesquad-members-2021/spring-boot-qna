d543874575a5a0369c615dc638dbf482f11a7ceb
gisungchoi <chlrltjd159@naver.com>
2021-03-03 16:13:30 +0900

    feat : renderQuestion() 정의
    아이디 값으로 들어온 질문을 찾아서 보여준다.

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -3,6 +3,9 @@ package com.codessquad.qna.controller;
 import com.codessquad.qna.domain.Question;
 import com.codessquad.qna.repository.QuestionRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -15,9 +18,17 @@ public class QuestionController {
     }
 
     @PostMapping("/question")
-    public String createQuestion(Question question){
+    public String createQuestion(Question question) {
         questionRepository.save(question);
         return "redirect:/";
+    }
+
+    @GetMapping("/questions/{questionId}")
+    public String renderQuestion(@PathVariable int questionId, Model model) {
+        Question getQuestion = questionRepository.findById(questionId);
+
+        model.addAttribute("question", getQuestion);
+        return "/qna/show";
     }
 
 }
