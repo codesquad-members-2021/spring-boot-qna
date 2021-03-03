f910804b22c17723b5f86368e323957e485c4352
MJbae <gently.gogo@gmail.com>
2021-03-03 22:11:28 +0900

    Feat: 질문하기 기능 구현
    1. static 디렉토리의 qna/form.html 이동(-> templates/questions/form.html)
    2. index.html에서 질문하기 버튼 클릭에 따른 요청 URI 변경(-> questions/form)
    3. QuestionController::createQuestion에 매개변수 question을 전달함(매개변수의 값은 form.html로부터 전달)
    4. 전달 받은 question 속성을 Question Domain 객체에 할당하고 questionRepository에 저장함
    5. 내부 로직 완료 후 홈 디렉토리로 리다이렉트

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -1,2 +1,36 @@
-package com.codessquad.qna.controller;public class QuestionController {
+package com.codessquad.qna.controller;
+
+import com.codessquad.qna.domain.Question;
+import com.codessquad.qna.repository.QuestionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class QuestionController {
+    QuestionRepository questionRepository = new QuestionRepository();
+
+    @GetMapping("/questions/form")
+    public String createForm(){
+        return "/questions/form";
+    }
+
+    @PostMapping("/questions")
+    public String createQuestion(Question question){
+        Logger logger = LoggerFactory.getLogger(QuestionController.class);
+        Question newQuestion = new Question();
+
+        newQuestion.setWriter(question.getWriter());
+        newQuestion.setTitle(question.getTitle());
+        newQuestion.setContents(question.getContents());
+
+        questionRepository.save(newQuestion);
+
+        logger.info("Question in questionRepository: " + newQuestion);
+
+        return "redirect:/";
+    }
 }
