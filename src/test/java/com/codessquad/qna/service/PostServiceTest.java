b8aae44a90b93ee963fa9afe4ce489bab94cf39e
tmdgusya <tmdgusya@naver.com>
2021-03-03 23:26:08 +0900

    test : Service 로직 수정으로 인한 테스트 코드 추가
    - failGetUser 테스트를 통해서 유저가 없을때 CanNotFindUserException 을 리턴하는지 확인하였습니다. 그리고 또한 기존의 Optional 처리를 해주던 것을 제거하였습니다.

--- src/test/java/com/codessquad/qna/service/PostServiceTest.java
+++ src/test/java/com/codessquad/qna/service/PostServiceTest.java
@@ -1,25 +1,26 @@
 package com.codessquad.qna.service;
 
+import com.codessquad.qna.MvcConfig;
 import com.codessquad.qna.entity.Post;
-import com.codessquad.qna.repository.PostRepository;
-import com.codessquad.qna.repository.PostRepositoryImpl;
+import com.codessquad.qna.exception.CanNotFindPostException;
+import com.codessquad.qna.exception.CanNotFindUserException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
-
-import java.util.ArrayList;
 
 
 class PostServiceTest {
 
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MvcConfig.class);
     PostService postService;
 
     @BeforeEach
     void rollback() {
-        postService = new PostService();
+        postService = applicationContext.getBean("postService", PostService.class);
     }
 
     @Test
@@ -27,7 +28,7 @@ class PostServiceTest {
     void addPost() {
         Post post = new Post("TIL", "roach", "오늘은 스프링을 하고있다.");
         postService.addPost(post);
-        assertThat(postService.getPost(1).get()).isEqualTo(post);
+        assertThat(postService.getPost(1)).isEqualTo(post);
     }
 
     @Test
@@ -37,7 +38,15 @@ class PostServiceTest {
         Post post1 = new Post("TIL2", "roach", "오늘은 스프링을 하고있다. 123");
         postService.addPost(post);
         postService.addPost(post1);
-        assertThat(postService.getPost(1).get()).isEqualTo(post);
+        assertThat(postService.getPost(1)).isEqualTo(post);
+    }
+
+    @Test
+    @DisplayName("없는 포스트를 가져오려고 할때 CanNotFindPostException 이 발생하는지 확인한다.")
+    void failGetPost() {
+        assertThatExceptionOfType(CanNotFindPostException.class)
+                .isThrownBy(() -> postService.getPost(100))
+                .withMessage("해당 POST 를 찾을 수 없습니다.");
     }
 
 }
