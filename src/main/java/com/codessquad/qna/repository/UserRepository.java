5bbe910ceef970271090664baa651b0a7db482c6
gisungchoi <chlrltjd159@naver.com>
2021-03-03 13:42:15 +0900

    feat : UserRepository 정의
    service까지는 현재 필요하지 않은거 같아서 repository만 작성했습니다.
    기본적인 동작을 정의합니다.

---
+++ src/main/java/com/codessquad/qna/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.codessquad.qna.repository;
+
+import com.codessquad.qna.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+    private List<User> users = new ArrayList<>();
+
+    public String save(User user) {
+        users.add(user);
+        return user.getUserId();
+    }
+
+    public List<User> findAll() {
+        return Collections.unmodifiableList(users);
+    }
+}
