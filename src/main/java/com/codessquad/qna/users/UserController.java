010595d36e1a77139b058e89beb61134bbac358b
정이삭 <isaac56@naver.com>
2021-03-03 16:29:01 +0900

    Implements showing user list
    user list를 보여주는 기능 구현하였습니다.
    
    - User class 생성했습니다.
    - UserController 생성했습니다.
    - /users으로 Get 요청 시 UserController에 ArrayList로 등록되어 있는 user list를 보여줍니다.

---
+++ src/main/java/com/codessquad/qna/users/UserController.java
@@ -0,0 +1,19 @@
+package com.codessquad.qna.users;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.*;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    private final List<User> users = new ArrayList<>();
+    @GetMapping()
+    String getUsers(Model model){
+        model.addAttribute("users",users);
+        return "users/list";
+    }
+}
