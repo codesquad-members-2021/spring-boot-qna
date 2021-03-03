eb86453f3a8240c9a148bf98ab1041a5d2dd058e
gisungchoi <chlrltjd159@naver.com>
2021-03-03 15:44:33 +0900

    feat : findAll() 메소드 정의
    리스트를 반환해준다.

--- src/main/java/com/codessquad/qna/repository/QuestionRepository.java
+++ src/main/java/com/codessquad/qna/repository/QuestionRepository.java
@@ -4,14 +4,19 @@ import com.codessquad.qna.domain.Question;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Repository
 public class QuestionRepository {
 
-    private List<Question> questions=new ArrayList<>();
+    private List<Question> questions = new ArrayList<>();
 
-    public void save(Question question){
+    public void save(Question question) {
         questions.add(question);
+    }
+
+    public List<Question> findAll() {
+        return Collections.unmodifiableList(questions);
     }
 }
