bbc92d9fbc6143c302dfb6c527d794450d63bba0
K <PizzaCola.K@gmail.com>
2021-03-03 11:21:20 +0900

    feat: See Question detail
    - /questions/{id}로 질문 상세 페이지에 접속하면, Question을 이용하여
    Model을 만들고 페이지를 구성합니다.
    - 답변 기능은 아직 구현하지 않았습니다.

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -5,6 +5,7 @@ import com.codessquad.qna.question.QuestionRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
@@ -16,6 +17,12 @@ public class QuestionController {
 
     private final List<Question> questions = new ArrayList<>();
 
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("questions", questions);
+        return "index";
+    }
+
     @PostMapping("/questions")
     public String query(QuestionRequest questionRequest) {
         Question question = new Question(questionRequest);
@@ -26,9 +33,18 @@ public class QuestionController {
         return "redirect:/";
     }
 
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("questions", questions);
-        return "index";
+    @GetMapping("/questions/{id}")
+    public String qnaShow(@PathVariable("id") String id, Model model) {
+        if (!id.matches("[1-9]\\d*")) {
+            return "redirect:/";
+        }
+        int questionIndex = Integer.parseInt(id) - 1;
+        if (questionIndex < questions.size()) {
+            Question question = questions.get(questionIndex);
+            model.addAttribute(question);
+            return "qna/show";
+        }
+        return "redirect:/";
     }
+
 }
