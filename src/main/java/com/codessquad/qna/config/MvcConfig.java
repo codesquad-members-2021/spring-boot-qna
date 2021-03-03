545f5d5902544ae8dcd6ae74283959bb438c7568
pbg0205 <pbg0205@naver.com>
2021-03-03 17:24:48 +0900

    feat : Webconfigurer을 통한 html & url 연결
    - config class 생성을 통한 url 맵핑 작업

---
+++ src/main/java/com/codessquad/qna/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.codessquad.qna.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registry.addViewController("users/").setViewName("user/list");
+    }
+}
