204d7100ef1fbb0f46260918275ab34f9e392a75
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-03-03 15:17:04 +0900

    회원 프로필 정보보기 구현 Controller 클래스에 profile() 메소드 추가, url로 받은 userId에 해당하는 유저의 정보 model에 저장 profile.html에서 전달받은 유저의 정보 출력

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -3,25 +3,40 @@ package com.codessquad.qna;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class UserController {
     private List<User> users = new ArrayList<>();
 
     @PostMapping("/user/create")
-    public String create(User user){
+    public String create(User user) {
         System.out.println("user : " + user);
         users.add(user);
         return "redirect:/users";
     }
 
     @GetMapping("/users")
-    public String list(Model model){
+    public String list(Model model) {
         model.addAttribute("users", users);
         return "/user/list";
     }
+
+    @GetMapping("/users/{userId}")
+    public String profile(@PathVariable String userId, Model model) {
+        User selectedUser = null;
+        for (User user : users) {
+            if (user.getUserId().equals(userId)) {
+                selectedUser = user;
+            }
+        }
+        model.addAttribute("user", selectedUser);
+        return "user/profile";
+    }
+
 }
