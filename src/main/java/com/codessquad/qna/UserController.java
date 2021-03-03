2c4622ffb0a7145743a1e1201850b18b420f6db6
noel <68936@naver.com>
2021-03-04 07:41:52 +0900

    [feat] 회원가입 기능 구현

---
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -0,0 +1,29 @@
+package com.codessquad.qna;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private List<User> userList = new ArrayList<>();
+
+    @GetMapping("/user/form.html")
+    private String registerDisplay(){
+        return "user/form";
+    }
+    @PostMapping("/user/create")
+    private String register(User user){
+        userList.add(user);
+        logger.info(user.toString());
+        return "redirect:/users";
+    }
+
+}
