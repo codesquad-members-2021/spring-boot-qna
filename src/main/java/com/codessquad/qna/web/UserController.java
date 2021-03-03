97d3482f4f9c716143cb90da381d67cec2d05ce1
JoonKi-Kim <joonki402@gmail.com>
2021-03-03 16:27:00 +0900

    회원 목록 기능 구현
    list.html 에서 인자를 받아 데이터 조회가능합니다.

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -1,14 +1,28 @@
 package com.codessquad.qna.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
-    @PostMapping("/user/create")
+    private List<User> users = new ArrayList<>();
+
+    @PostMapping("/users")
     public String create(User user) {
         System.out.println("user Info: " + user);
-        return "index";
+        users.add(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String list(Model model) {
+        model.addAttribute("user", users);
+        return "/users/list";
     }
 
 }
