6d12bd1c059a86d61b0b52567f3e0b245715bcf0
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-03 17:21:02 +0900

    더 이상 필요없는 logger 코드를 제거하라

--- src/main/java/com/codessquad/qna/web/qna/QnaController.java
+++ src/main/java/com/codessquad/qna/web/qna/QnaController.java
@@ -1,7 +1,5 @@
 package com.codessquad.qna.web.qna;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +11,10 @@ import java.util.List;
 
 @Controller
 public class QnaController {
-    Logger logger = LoggerFactory.getLogger(QnaController.class);
     private List<Question> questionList = new ArrayList<>();
 
     @PostMapping("/questions")
     public String createQuestion(String writerId, String title, String contents) {
-        logger.info("onRequestCreateQuestion called");
         Question newQuestion = new Question(questionList.size() + 1, writerId, title, contents);
         questionList.add(newQuestion);
         return "redirect:/";
@@ -26,14 +22,12 @@ public class QnaController {
 
     @GetMapping("/")
     public String getQuestionList(Model model) {
-        logger.info("onRequestShowQuestions called");
         model.addAttribute("questions", questionList);
         return "index";
     }
 
     @GetMapping("/questions/{questionId}")
     public String getOneQuestion(@PathVariable("questionId") int questionId, Model model) {
-        logger.info("onGetQuestionById called");
         Question foundQuestion = questionList.stream()
                 .filter((question -> question.getId() == questionId))
                 .findFirst().orElse(null);
