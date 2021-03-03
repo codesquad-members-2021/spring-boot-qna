b17c80cf7095145387b0fe4ab4d94d6154111bae
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-03-03 21:00:58 +0900

    유저에 대한 정보를 받는 클래스 생성

--- src/main/java/com/codessquad/qna/web/User.java
+++ src/main/java/com/codessquad/qna/web/User.java
@@ -6,20 +6,36 @@ public class User {
     private String name;
     private String email;
 
-    public void setEmail(String email) {
-        this.email = email;
+    public String getUserId() {
+        return userId;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
