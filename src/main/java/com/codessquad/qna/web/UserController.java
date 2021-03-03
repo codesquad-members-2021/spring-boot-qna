f38e40c23cfc4a0e851a31e0930f5b9ac889548f
Polynomeer <polynomeer@gmail.com>
2021-03-03 12:22:22 +0900

    feat: Add users page to show user list
    Added users page to show user list.
    Added UserController::list to show user list after creating user account.

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -1,13 +1,27 @@
 package com.codessquad.qna.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
+    private final List<User> users = new ArrayList<>();
+
+    @GetMapping("/users")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "list";
+    }
+
     @PostMapping("/users/create")
     public String create(User user) {
         System.out.println(user);
-        return "index";
+        users.add(user);
+        return "redirect:/users";
     }
 }
