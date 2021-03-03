1c11159ec2d6c15761b35ca38d30016f6b5e5604
pbg0205 <pbg0205@naver.com>
2021-03-03 12:28:30 +0900

    feat : UserController 생성
    - UserController를 생성해 회원가입에 관련한 url 요청을 처리하도록
    합니다.

---
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -0,0 +1,35 @@
+package com.codessquad.qna.web;
+
+import com.codessquad.qna.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    private List<User> userList = new ArrayList<>();
+
+    @GetMapping("/form")
+    public String form() {
+        return "user/form";
+    }
+
+    @PostMapping("")
+    public String create(User user) {
+        System.out.println("user: " + user);
+        userList.add(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("")
+    public String list(Model model) {
+        model.addAttribute("userList", userList);
+        return "user/list";
+    }
+}
