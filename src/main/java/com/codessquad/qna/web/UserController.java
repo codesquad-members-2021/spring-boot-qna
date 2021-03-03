14f3f7ef95340a708006ab6f990b433dc26d16ec
Polynomeer <polynomeer@gmail.com>
2021-03-03 10:41:57 +0900

    refactor: Change GET to POST
    Changed GET to POST in UserController::create method to hide user information from url.

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -1,11 +1,11 @@
 package com.codessquad.qna.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-    @GetMapping("/users/create")
+    @PostMapping("/users/create")
     public String create(String userId, String password, String name, String email) {
         System.out.println("userId : " + userId + ", name : " + name);
         return "index";
