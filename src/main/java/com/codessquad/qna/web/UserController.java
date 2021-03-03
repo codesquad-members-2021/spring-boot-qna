a259b707568e5c13ef5f74c083a1859a36a8d33b
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-03-03 21:23:38 +0900

    List 내에서 이름을 누르면 해당 사람의 프로필로 넘어가는 메서드 구현

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -3,6 +3,7 @@ package com.codessquad.qna.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -14,16 +15,26 @@ public class UserController {
 
     @PostMapping("/create")
     public String create(User user) {
-        System.out.println("user Id: " + user);
         userList.add(user);
         return "redirect:/list";
     }
 
     @GetMapping("/list")
-    public String list(Model model) {
+    public String getList(Model model) {
         model.addAttribute("users", userList);
         return "user/list";
     }
 
+    @GetMapping("/users/{userId}")
+    public String getOneUserProfile(@PathVariable("userId") String userId, Model model) {
+        User foundUser = getUserById(userId);
+        model.addAttribute("user", foundUser);
+        return "user/profile";
+    }
 
+    public User getUserById(String userId){
+        return userList.stream()
+                .filter(user -> user.getUserId().equals(userId))
+                .findFirst().orElse(null);
+    }
 }
