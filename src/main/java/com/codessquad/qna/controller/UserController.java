7755cd5baa19b93e43572dc46b9caeed52f9aeec
jeong-inho <derosatam76@gmail.com>
2021-03-03 17:17:20 +0900

    Create signUp method in UserController /users url을 통해 포스트 요청이 들어오면 유저정보를 받아 새로운 유저 객체를 만들고 Users 리스트에 추가하는 signUp 메소드 생성

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -1,19 +1,35 @@
 package com.codessquad.qna.controller;
 
+import com.codessquad.qna.model.User;
+import com.codessquad.qna.repository.Users;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
     private Logger logger = LoggerFactory.getLogger(UserController.class);
+    Users users = new Users();
 
     @GetMapping("/user/form")
     public String viewForm() {
         logger.info("회원가입 페이지 요청");
         return "user/form";
+    }
+
+    @PostMapping("/users")
+    public String signUp(User user) {
+        User newUser = new User();
+        newUser.setUserId(user.getUserId());
+        newUser.setPassword(user.getPassword());
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+        users.addUser(newUser);
+        logger.info("회원가입 요청 " + newUser.toString());
+        return "redirect:/users";
     }
 
 }
