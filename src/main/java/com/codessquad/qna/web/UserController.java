35a54ddbbf35f34ba2545cb9e5afe8a5e6031420
95degree <95degree@naver.com>
2021-03-03 15:39:12 +0900

    UserInfo에 메소드 추가
    UserInfo를 정상적으로 받아오는지 출력하기 위해 set메소드와 toString메소드를 추가했습니다.

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
     @PostMapping("/user/create")
-    public String create(String userId, String password, String name, String email){
-        System.out.println("userId : " + userId);
+    public String create(UserInfo userinfo){
+        System.out.println("user : " + userinfo);
         return "index";
     }
 }
--- src/main/java/com/codessquad/qna/web/UserInfo.java
+++ src/main/java/com/codessquad/qna/web/UserInfo.java
@@ -1,4 +1,29 @@
 package com.codessquad.qna.web;
 
 public class UserInfo {
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString(){
+        return "userId ="+userId+"password = "+password+"name = "+name+"email ="+email;
+    }
 }
