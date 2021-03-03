b0e9e40001ae784a667736f9e1115de4ea24bca0
MJbae <gently.gogo@gmail.com>
2021-03-03 21:25:26 +0900

    Feat: Question Domain 객체를 관리하는 Repository 객체 생성

--- src/main/java/com/codessquad/qna/repository/QuestionRepository.java
+++ src/main/java/com/codessquad/qna/repository/QuestionRepository.java
@@ -1,2 +1,18 @@
-package com.codessquad.qna.repository;public class QuestionRepository {
+package com.codessquad.qna.repository;
+
+import com.codessquad.qna.domain.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionRepository {
+    private final List<Question> questionList = new ArrayList<>();
+
+    public void save(Question newQuestion){
+        questionList.add(newQuestion);
+    }
+
+    public List<Question> getAll(){
+        return questionList;
+    }
 }
