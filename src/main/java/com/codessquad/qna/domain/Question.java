77ed74c1e18409041140772c3326d142ec301a55
gisungchoi <chlrltjd159@naver.com>
2021-03-03 15:26:55 +0900

    feat : Question domain 작성
    질문 글에 사용할 domain을 만들었다.

---
+++ src/main/java/com/codessquad/qna/domain/Question.java
@@ -0,0 +1,31 @@
+package com.codessquad.qna.domain;
+
+public class Question {
+    private String writer;
+    private String title;
+    private String contents;
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
