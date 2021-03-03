1c3139e59d6c46db9875ed3a4625bee743cb0f7e
gisungchoi <chlrltjd159@naver.com>
2021-03-03 13:44:48 +0900

    feat : UserController에서 Repository 사용
    빈으로 등록될 때 repository를 갖기 위해 생성자로 받도록 했습니다.

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -1,13 +1,24 @@
 package com.codessquad.qna.controller;
 
+import com.codessquad.qna.domain.User;
+import com.codessquad.qna.repository.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/create")
     public String renderUserCreateForm() {
@@ -15,12 +26,15 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    public String createUser() {
-        return "redirect:/users";
+    public String createUser(User user) {
+        userRepository.save(user);
+        return "redirect:/user/users";
     }
 
     @GetMapping("/users")
-    public String renderUserList() {
-        return "";
+    public String renderUserList(Model model) {
+        List<User> getUsers = userRepository.findAll();
+        model.addAttribute("users", getUsers);
+        return "user/list";
     }
 }
