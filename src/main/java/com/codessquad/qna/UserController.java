2c7adaa49a5a3586bdeb4244a25276a0a0bfb0c7
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-03-03 12:35:24 +0900

    회원 목록 기능 추가 Controller 클래스에 회원가입한 유저를 담은 users 리스트 추가 회원목록 조회하는 list() 메소드 추가 create()의 반환값을 redirect로 지정해서 url로 이동하도록 변경 list.html에서 사용자 입력값 올바르게 출력하도록 템플릿엔진 for문 추가

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -1,14 +1,27 @@
 package com.codessquad.qna;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
+    private List<User> users = new ArrayList<>();
 
     @PostMapping("/user/create")
     public String create(User user){
         System.out.println("user : " + user);
-        return "welcome";
+        users.add(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String list(Model model){
+        model.addAttribute("users", users);
+        return "/user/list";
     }
 }
