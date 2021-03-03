423f9a431695b21c67ba0fb48bd5b2415739a4bc
bong6981 <kbk5190@naver.com>
2021-03-03 21:03:58 +0900

    feat : Implement getProfile()
    - /users/list.html에서회원 이름 클릭시 profile로 이동하도록 구현
    - list.html에 이름 부분에 a태그 추가, 경로는 /users/{{userId}}로 설정
    - 해당 경로에대한 get매핑한 메소드 추가
    - @PathVariable을 사용하여 url로 가져온 변수명을 사용
    - arrayList 중 이름으로 해당 User를  찾는 findByName 메소드 추가
    - 해당 User를 찾아 Model에 전달
    - /users/profile.html은 model로 넘겨 받은 데이터 user의 이름과 email이
      출력될 수 있도록 수정

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -3,10 +3,12 @@ package com.codessquad.qna.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -21,7 +23,24 @@ public class UserController {
     @GetMapping("/users")
     public String getUsers(Model model) {
         model.addAttribute("users", users);
-        System.out.println(users.get(0).getEmail());
-        return "/user/list";
+        return "/users/list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String getProfile(@PathVariable String userId, Model model) {
+        //id 중복 확인, null일경우 처리
+        User foundUser = fingByUserId(userId);
+        model.addAttribute("user", foundUser);
+        return "/users/profile";
+    }
+
+    private User fingByUserId(String userId) {
+        User foundUser = null;
+        for (User user : users) {
+            if (user.getUserId().equals(userId)) {
+                foundUser = user;
+            }
+        }
+        return foundUser;
     }
 }
