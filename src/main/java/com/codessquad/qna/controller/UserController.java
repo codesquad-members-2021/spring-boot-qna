15c4be71b28ca9fd50be604183dbfd5b0e829121
jeong-inho <derosatam76@gmail.com>
2021-03-03 17:29:51 +0900

    Create viewProfile method in UserController /users/{userId}로 get 요청이 들어오면 해당 유정의 프로필을 화면에 보여주기 위해 아이디로 검색 후 찾아서 프로필 페이지로 보내주는 viewProfile 메소드 생성

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -38,6 +39,13 @@ public class UserController {
         model.addAttribute("users", this.users.getAllUser());
         logger.info("유저 리스트 패이지 요청");
         return "user/list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String viewProfile(@PathVariable("userId") String userId, Model model) {
+        model.addAttribute("user", this.users.findUser(userId));
+        logger.info(userId + "유저의 프로필 페이지 요청");
+        return "user/profile";
     }
 
 }
