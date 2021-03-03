7118f1dda283e4591aab2c9da70bc72689401d02
malaheaven <yunhg95@gmail.com>
2021-03-03 17:17:43 +0900

    feat: 회원가입을 구현하기 위해 User Model이 되는 클래스 생성

---
+++ src/main/java/com/codessquad/qna/domain/User.java
@@ -0,0 +1,35 @@
+package com.codessquad.qna.domain;
+
+public class User {
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
