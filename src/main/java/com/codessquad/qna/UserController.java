582baeb3f34ed00380af1fcbbc9290a1c427db75
noel <68936@naver.com>
2021-03-04 08:36:57 +0900

    [feat] 회원 프로필 정보보기

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -30,6 +31,18 @@ public class UserController {
     private String getMemberList(Model model){
         model.addAttribute("users",userList);
         return "user/list";
+    }
+    @GetMapping("/users/{userId}")
+    private String displayProfile(@PathVariable("userId") String userId, Model model){
+
+        for(User findUser:userList){
+            if(findUser.getUserId().equals(userId)) {
+                model.addAttribute("userID",findUser.getUserId());
+                model.addAttribute("email",findUser.getEmail());
+                break;
+            }
+        }
+        return "user/profile";
     }
 
 }
