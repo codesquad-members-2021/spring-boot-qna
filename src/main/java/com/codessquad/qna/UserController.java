af1831b2264da82700f44f6ee43e0e953622a785
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-03-03 15:27:11 +1300

    회원가입 기능 구현
    1. 입력한 데이터를 URL에 포함시키지 않기 위해서 get방식에서 post방식으로 바꿈.
    
    2. 입력받은 데이터를 파라미터로 모두 넣으면 메서드가 넘 길어지므로 User 클래스로 따로 뺀다.
    
    3. 실제로 Mapping이 잘 되는지 확인하기위해서 print 메서드를 추가하고 toString() 오버라이드.

---
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -0,0 +1,14 @@
+package com.codessquad.qna;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @PostMapping("/user/create")
+    public String create(User user) {
+        System.out.println("user: " + user.toString());
+        return "welcome";
+    }
+}
