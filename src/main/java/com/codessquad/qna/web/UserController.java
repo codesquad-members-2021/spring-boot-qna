16d016a8865bcf535c0eec21b7886c0902588f14
JoonKi-Kim <joonki402@gmail.com>
2021-03-04 10:58:48 +0900

    회원 프로필 보기 구현
    @ParamVariable 을 사용하여 userId를 받으면 해당 프로필을 볼 수 있게
    설정하였습니다

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -3,8 +3,8 @@ package com.codessquad.qna.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,17 +13,28 @@ import java.util.List;
 public class UserController {
     private List<User> users = new ArrayList<>();
 
-    @PostMapping("/users")
+    @PostMapping("users")
     public String create(User user) {
         System.out.println("user Info: " + user);
         users.add(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/users")
+    @GetMapping("users")
     public String list(Model model) {
-        model.addAttribute("user", users);
-        return "/user/list";
+        model.addAttribute("users", users);
+        return "user/list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String viewProfile(@PathVariable("userId") String userId, Model model) {
+        for(User user : users) {
+            if(user.getUserId().equals(userId)) {
+                model.addAttribute("user", user);
+                return "user/profile";
+            }
+        }
+        return "user/profile";
     }
 
 }
