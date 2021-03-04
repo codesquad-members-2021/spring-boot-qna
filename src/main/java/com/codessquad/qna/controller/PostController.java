fad80a941f203ebcaddbeb4a29767d0337615357
tmdgusya <tmdgusya@naver.com>
2021-03-04 18:25:41 +0900

    refactor : @ModelAttribute 를 이용하여 코드 단축

--- src/main/java/com/codessquad/qna/controller/PostController.java
+++ src/main/java/com/codessquad/qna/controller/PostController.java
@@ -8,10 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -25,14 +22,12 @@ public class PostController {
 
     /**
      * 질문 게시글을 게시판에 등록합니다.
-     * @param writer
-     * @param title
-     * @param contents
+     * @param servedPost
      * @return
      */
     @PostMapping("/questions")
-    public String addPost(@RequestParam String writer, @RequestParam String title, @RequestParam String contents) {
-        Post post = new Post(writer, title, contents);
+    public String addPost(@ModelAttribute Post servedPost) {
+        Post post = servedPost;
         postService.addPost(post);
         return "redirect:/";
     }
