c04d4512e34eaf5243959120c94153345a5e2e40
K <PizzaCola.K@gmail.com>
2021-03-04 22:50:50 +0900

    feat: Use H2 Database when maintain Question
    - 이제 Question을 관리할 때 H2 DB를 이용합니다.
    - id를 관리할 때, strategy가 GenerationType.AUTO이면 Question과 User가
    자동생성되는 id 값을 공유합니다. 그래서, GenerationType.IDENTITY로
    변경하였습니다.
    - 일단, Question 내에서 writer를 User로 두는 것을 다시 단순한 String으로
    변경하였습니다.

---
+++ src/main/java/com/codessquad/qna/domain/Question.java
@@ -0,0 +1,61 @@
+package com.codessquad.qna.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String writer;
+    private String title;
+    private String contents;
+    private LocalDateTime time;
+    private int point;
+
+    protected Question() {}
+
+    public Question(String writer, String title, String contents) {
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+}
--- src/main/java/com/codessquad/qna/domain/User.java
+++ src/main/java/com/codessquad/qna/domain/User.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 20, unique = true)
