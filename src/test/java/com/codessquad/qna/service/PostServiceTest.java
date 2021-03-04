6e24bb7a9f9f348d4a0fcc438f4002de28067623
tmdgusya <tmdgusya@naver.com>
2021-03-04 16:30:35 +0900

    fix : 매개변수 수정으로 인한 수정 및 테스트코드 개선
    - 기본키 이므로 같을 수 없기 때문에 해당 테스트를 id 값을 비교하는 것으로 개선하였음. 왜냐하면 DB 에서 가져오는 경우도 있기때문에

--- src/test/java/com/codessquad/qna/service/PostServiceTest.java
+++ src/test/java/com/codessquad/qna/service/PostServiceTest.java
@@ -28,7 +28,7 @@ class PostServiceTest {
     void addPost() {
         Post post = new Post("TIL", "roach", "오늘은 스프링을 하고있다.");
         postService.addPost(post);
-        assertThat(postService.getPost(1)).isEqualTo(post);
+        assertThat(postService.getPost(1L).getPostId()).isEqualTo(post.getPostId());
     }
 
     @Test
@@ -38,14 +38,14 @@ class PostServiceTest {
         Post post1 = new Post("TIL2", "roach", "오늘은 스프링을 하고있다. 123");
         postService.addPost(post);
         postService.addPost(post1);
-        assertThat(postService.getPost(1)).isEqualTo(post);
+        assertThat(postService.getPost(1L).getPostId()).isEqualTo(post.getPostId());
     }
 
     @Test
     @DisplayName("없는 포스트를 가져오려고 할때 CanNotFindPostException 이 발생하는지 확인한다.")
     void failGetPost() {
         assertThatExceptionOfType(CanNotFindPostException.class)
-                .isThrownBy(() -> postService.getPost(100))
+                .isThrownBy(() -> postService.getPost(100L))
                 .withMessage("해당 POST 를 찾을 수 없습니다.");
     }
 
