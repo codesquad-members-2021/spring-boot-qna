d0fbcc6eb0ef217485d08addb99158295ea760b6
Polynomeer <polynomeer@gmail.com>
2021-03-04 15:02:50 +0900

    refactor: Binding html using Handlebars Reusing templates
    Binding html files using Handlebars Reusing templates to simplify codes.
    Added @RequestMapping in UserController to remove duplicates.
    Changed all relative path to absolute path

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -5,31 +5,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("users")
 public class UserController {
     private final List<User> users = new ArrayList<>();
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/{userId}")
     public String profile(@PathVariable String userId, Model model) {
         for (User user : users) {
             if (user.getUserId().equals(userId)) {
                 model.addAttribute(user);
             }
         }
-        return "profile";
+        return "/user/profile";
     }
 
-    @GetMapping("/users")
+    @GetMapping()
     public String list(Model model) {
         model.addAttribute("users", users);
-        return "list";
+        return "/user/list";
     }
 
-    @PostMapping("/users/create")
+    @PostMapping("/create")
     public String create(User user) {
         System.out.println(user);
         users.add(user);
