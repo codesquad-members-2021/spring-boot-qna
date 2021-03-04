262b003e536d7317556426ec8ec0fbce11dd0f76
tmdgusya <tmdgusya@naver.com>
2021-03-04 18:57:29 +0900

    feat : create DateFormat class
    - DateFormat 은 웹 싸이트 전반적인 시간표준이 될것 같아 따로 클래스로 분리하였습니다.

---
+++ src/main/java/com/codessquad/qna/util/DateFormat.java
@@ -0,0 +1,9 @@
+package com.codessquad.qna.util;
+
+import java.text.SimpleDateFormat;
+
+public class DateFormat {
+
+    public static final SimpleDateFormat ISO8601 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+}
