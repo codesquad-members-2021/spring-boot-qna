5bd8d8c560f7435fc033a50dd05c58ee0fa821b6
Jihye Woo <woojihye2339@gmail.com>
2021-03-05 03:01:48 +0900

    feat : 회원가입 요청 처리 메소드 구현
      > UserController에 create() 구현
       - @PostMapping("/user/create")
       - 회원가입 페이지에 데이터 입력 후 submit 버튼 클릭 시 호출
       - templates/user/list.html 뷰 리턴

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -2,6 +2,7 @@ package com.codessquad.qna.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -9,6 +10,12 @@ public class UserController {
     @GetMapping("/user/form")
     public String getForm(){
         return "user/form";
+    }
+
+    @PostMapping("/user/create")
+    public String create(String userId, String password, String name, String email){
+        System.out.printf("%s %s %s %s ", userId, password, name, email);
+        return "user/list";
     }
 
 }
