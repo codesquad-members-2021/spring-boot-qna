03018be012bab11fc30cf3438c8a212d4de25788
malaheaven <yunhg95@gmail.com>
2021-03-04 15:30:58 +0900

    feat: [회원목록 기능] list()메소드 생성, create() 메소드는 회원가입후 목록 페이지를 바로 보여주기 위해 redirect 적용

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -2,21 +2,33 @@ package com.codessquad.qna.controller;
 
 import com.codessquad.qna.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
+    private List<User> users = new ArrayList<User>();
 
-    @GetMapping("/user/form")
+    @GetMapping("/users/form")
     public String moveToForm() {
-        return "user/form";
+        return "users/form";
     }
 
     @PostMapping("/user/create")
-    public String createUser(User user){
+    public String create(User user){
         System.out.println(user);
-        return "user/form";
+        users.add(user);
+        System.out.println(users.toString());
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String list(Model model){
+        model.addAttribute("users",users);
+        return "users/list";
     }
 }
