bf6b41cfcde798e3156c3ad36bf40cb1cb266b76
tmdgusya <tmdgusya@naver.com>
2021-03-04 18:37:37 +0900

    refactor : Entity Class Setter 제거

--- src/main/java/com/codessquad/qna/entity/Post.java
+++ src/main/java/com/codessquad/qna/entity/Post.java
@@ -43,24 +43,12 @@ public class Post {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
     public String getBody() {
         return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
     }
 
     public String getDate() {
