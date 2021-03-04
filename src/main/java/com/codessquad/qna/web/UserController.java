23a264ba77c17aad76d43789e4614c4c37009bee
95degree <95degree@naver.com>
2021-03-04 16:20:02 +0900

    list 메소드 추가
    회원가입하는 유저의 정보를 가져오기 위해 list()를 추가 했습니다.

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -1,13 +1,25 @@
 package com.codessquad.qna.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
+    private List<UserInfo> userInfos = new ArrayList<>();
+
     @PostMapping("/user/create")
     public String create(UserInfo userinfo){
+        userInfos.add(userinfo);
         System.out.println("user : " + userinfo);
         return "index";
+    }
+
+    @GetMapping("/user/list")
+    public String list(){
+        return "list";
     }
 }
