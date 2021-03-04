ce58eae31a7263f3b4eaf7e92ffbbc4631b2388e
정이삭 <isaac56@naver.com>
2021-03-04 14:52:32 +0900

    Add checking duplicate id to createUser method (UserController class)
    사용자 가입 시 userId가 중복되는 걸 막도록 로직 변경

--- src/main/java/com/codessquad/qna/users/User.java
+++ src/main/java/com/codessquad/qna/users/User.java
@@ -1,5 +1,7 @@
 package com.codessquad.qna.users;
 
+import java.util.Objects;
+
 public class User {
     private String userId;
     private String password;
@@ -46,5 +48,18 @@ public class User {
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getUserId(), user.getUserId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUserId());
     }
 }
--- src/main/java/com/codessquad/qna/users/UserController.java
+++ src/main/java/com/codessquad/qna/users/UserController.java
@@ -15,30 +15,32 @@ public class UserController {
     private final List<User> users = Collections.synchronizedList(new ArrayList<>());
 
     @GetMapping()
-    String getUsers(Model model){
-        model.addAttribute("users",users);
+    String getUsers(Model model) {
+        model.addAttribute("users", users);
         return "users/list";
     }
 
     @PostMapping()
-    String createUser(User user){
-        users.add(user);
+    String createUser(User user) {
+        if (!users.contains(user)) {
+            users.add(user);
+        }
         return "redirect:/users";
     }
 
     @GetMapping("/{userId}")
-    String getProfile(@PathVariable String userId, Model model){
+    String getProfile(@PathVariable String userId, Model model) {
         Optional<User> user = findUser(userId);
-        if(user.isPresent()){
+        if (user.isPresent()) {
             model.addAttribute("user", user.get());
             return "users/profile";
         }
         return "redirect:/users";
     }
 
-    private Optional<User> findUser(String userId){
-        for(User user : users){
-            if(user.getUserId().equals(userId)){
+    private Optional<User> findUser(String userId) {
+        for (User user : users) {
+            if (user.getUserId().equals(userId)) {
                 return Optional.of(user);
             }
         }
