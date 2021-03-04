ac255918eb196a74dbfce2c0f97a1dd4113bb980
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-03-04 18:21:29 +0900

    Qna 생성 메서드 추가

--- src/main/java/com/codessquad/qna/web/QuestionController.java
+++ src/main/java/com/codessquad/qna/web/QuestionController.java
@@ -1,2 +1,28 @@
-package com.codessquad.qna.web;public class QuestionController {
+package com.codessquad.qna.web;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class QuestionController {
+
+    List<Qna> qnaList = new ArrayList<>();
+
+    @PostMapping("/qna")
+    public String qnaMain(String writer, String title, String contents) {
+        Qna newQna = new Qna(qnaList.size()+ 1, writer,title,contents);
+        System.out.println("QnA: "+writer );
+        qnaList.add(newQna);
+        return "redirect:/";
+    }
+
 }
