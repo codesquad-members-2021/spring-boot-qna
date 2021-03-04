9eee2e9d8c326523da0a5455b13864a0e65e2eb2
Sally Oh <min27604@hotmail.com>
2021-03-04 21:43:47 +0900

    feat: Creat UserController
    - 템플릿 안에 있는 index.html을 불러오는 메서드를 추가하였습니다.
    - user/form으로 들어오면 template/user/form.html을 반환하는 메서드를 추가하였습니다.
    - form.html에서 회원가입 버튼을 누르면 사용자가 입력한 내용을 User로 받아 users 리스트에 추가하고 모델로 감싸 user/list를 반환하게 했습니다.

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -1,2 +1,33 @@
-package com.codessquad.qna.controller;public class UserController {
+package com.codessquad.qna.controller;
+
+import com.codessquad.qna.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    private List<User> users = new ArrayList<>();
+
+    @GetMapping("user/form")
+    public String getForm() {
+        return "user/form";
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("user/create")
+    public String create(User user, Model model) {
+        users.add(user);
+        System.out.println(user);
+        model.addAttribute("user", user);
+        return "user/list";
+    }
 }
