950134aa3e7f42f70567867463bca3223aef430d
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-03-04 18:39:01 +1300

    회원 프로필 정보보기 구현

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -3,7 +3,9 @@ package com.codessquad.qna;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,5 +26,16 @@ public class UserController {
     public String list(Model model) {
         model.addAttribute("userList", userList);
         return "list";
+    }
+
+    @GetMapping("/userList/{userId}")
+    @ResponseBody
+    public String profile(@PathVariable String userId, Model model) {
+        for (User user : userList) {
+            if(user.getUserId() == userId) {
+                model.addAttribute("user", user);
+            }
+        }
+        return "profile";
     }
 }
