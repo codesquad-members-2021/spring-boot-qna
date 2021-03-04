4c38cdc115a02467f36912d8a131a34f80b02606
jeong-inho <derosatam76@gmail.com>
2021-03-04 17:50:27 +0900

    Create findQuestion method in Questions class id에 해당하는 질문을 찾기위해 Questions 클래스에 findQuestion 메소드 생성

--- src/main/java/com/codessquad/qna/repository/Questions.java
+++ src/main/java/com/codessquad/qna/repository/Questions.java
@@ -11,14 +11,23 @@ public class Questions {
 
     public void addQuestion(Question question) {
         Question newQuestion = new Question();
+        newQuestion.setId(this.questionList.size() + 1);
         newQuestion.setWriter(question.getWriter());
         newQuestion.setTitle(question.getTitle());
         newQuestion.setContents(question.getContents());
+        newQuestion.setDate();
         questionList.add(newQuestion);
     }
 
     public List<Question> getAllQuestion() {
         return this.questionList;
+    }
+
+    public Question findQuestion(int id) {
+        return this.questionList.stream()
+                .filter(question -> question.getId() == id)
+                .findFirst()
+                .get();
     }
 
 }
