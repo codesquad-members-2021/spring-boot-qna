61ce9286cb95ec6e5026df4ff1e48455bb645bc5
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-05 01:13:57 +0900

    Add getQuestion for show contents of question

--- src/main/java/com/codessquad/qna/question/QuestionController.java
+++ src/main/java/com/codessquad/qna/question/QuestionController.java
@@ -2,6 +2,7 @@ package com.codessquad.qna.question;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,6 +30,15 @@ public class QuestionController {
     @GetMapping
     public ModelAndView getQuestions() {
         return new ModelAndView("/qna/list", "questions", questions);
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView getQuestion(@PathVariable int id) {
+        ModelAndView modelAndView = new ModelAndView("/qna/show");
+        modelAndView.addObject("question", questions.get(id));
+        modelAndView.addObject("id", id);
+
+        return modelAndView;
     }
 
     @PostMapping
