78f314c15d4583cc7700c4052ec32a31ebad1f5a
jeong-inho <derosatam76@gmail.com>
2021-03-04 17:24:17 +0900

    Create viewMain method in QuestionController class 메인페이지를 요청 시 저장되어있는 질문들을 화면에 보여주기 위해 viewMain 메소드 생성, 그에 따라 MvcConfig 클래스에서 관리하던 기본 url 삭제, index.html 파일에 정적으로 보이던 질문들을 동적으로 변경

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -5,6 +5,8 @@ import com.codessquad.qna.repository.Questions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -12,6 +14,12 @@ public class QuestionController {
 
     private Logger logger = LoggerFactory.getLogger(UserController.class);
     Questions questions = new Questions();
+
+    @GetMapping("/")
+    public String viewMain(Model model) {
+        model.addAttribute("questions", this.questions.getAllQuestion());
+        return "index";
+    }
 
     @PostMapping("/question/form")
     public String createQuestion(Question question) {
