b853886897d84cac5d615b948f960c941927a8e7
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-03-05 11:01:10 +1300

    동적처리를 위한 Mapping 메서드들 추가

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -15,27 +16,47 @@ public class UserController {
 
     private List<User> userList = new ArrayList<>();
 
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("questionList", QuestionController.questionList());
+        return "index";
+    }
+
+    @GetMapping("/qna/form")
+    public String qnaForm() {
+        return "/qna/form";
+    }
+
+    @GetMapping("/user/form")
+    public String userForm() {
+        return "/user/form";
+    }
+
+    @GetMapping("/user/login")
+    public String login() {
+        return "/user/login";
+    }
+
     @PostMapping("/user/create")
     public String create(User user) {
         System.out.println("user: " + user.toString());
         userList.add(user);
-        return "redirect:list";
+        return "redirect:/user/list";
     }
 
     @GetMapping("/user/list")
     public String list(Model model) {
         model.addAttribute("userList", userList);
-        return "list";
+        return "/user/list";
     }
 
     @GetMapping("/userList/{userId}")
-    @ResponseBody
     public String profile(@PathVariable String userId, Model model) {
         for (User user : userList) {
-            if(user.getUserId() == userId) {
+            if(user.getUserId().equals(userId)) {
                 model.addAttribute("user", user);
             }
         }
-        return "profile";
+        return "/qna/profile";
     }
 }
