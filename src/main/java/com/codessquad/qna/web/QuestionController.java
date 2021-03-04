d94ea4641ead8d1c3708569a53aa7fbf58b60f38
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-03-04 20:21:50 +0900

    사용하지 않는 코드 삭제

--- src/main/java/com/codessquad/qna/web/QuestionController.java
+++ src/main/java/com/codessquad/qna/web/QuestionController.java
@@ -1,15 +1,12 @@
 package com.codessquad.qna.web;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Controller
