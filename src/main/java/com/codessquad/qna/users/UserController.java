6dc60482cb07f669aead79d3e5ac5e6c5e06959d
정이삭 <isaac56@naver.com>
2021-03-04 14:45:34 +0900

    Implements showing user profile
    /user/{userId} 경로로 프로필을 조회하는 기능 구현하였습니다.

--- src/main/java/com/codessquad/qna/users/UserController.java
+++ src/main/java/com/codessquad/qna/users/UserController.java
@@ -3,6 +3,7 @@ package com.codessquad.qna.users;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +13,7 @@ import java.util.*;
 @RequestMapping("/users")
 public class UserController {
     private final List<User> users = Collections.synchronizedList(new ArrayList<>());
+
     @GetMapping()
     String getUsers(Model model){
         model.addAttribute("users",users);
@@ -24,4 +26,22 @@ public class UserController {
         return "redirect:/users";
     }
 
+    @GetMapping("/{userId}")
+    String getProfile(@PathVariable String userId, Model model){
+        Optional<User> user = findUser(userId);
+        if(user.isPresent()){
+            model.addAttribute("user", user.get());
+            return "users/profile";
+        }
+        return "redirect:/users";
+    }
+
+    private Optional<User> findUser(String userId){
+        for(User user : users){
+            if(user.getUserId().equals(userId)){
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty();
+    }
 }
