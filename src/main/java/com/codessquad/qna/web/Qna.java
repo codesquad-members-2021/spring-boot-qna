9174ff0daa2f37eecde6441801b901fbe23015dc
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-03-04 18:20:57 +0900

    QnA를 관리하는 클래스 추가

--- src/main/java/com/codessquad/qna/web/Qna.java
+++ src/main/java/com/codessquad/qna/web/Qna.java
@@ -1,2 +1,69 @@
-package com.codessquad.qna.web;public class Qna {
+package com.codessquad.qna.web;
+
+import java.util.Date;
+
+public class Qna {
+
+    private int id;
+    private String writer;
+    private String title;
+    private String contents;
+    private Date date;
+
+    public Qna(int id, String writer, String title, String contents){
+        this.id = id;
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+        this.date = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Qna{" +
+                "writer='" + writer + '\'' +
+                ", title='" + title + '\'' +
+                ", contents='" + contents + '\'' +
+                '}';
+    }
 }
