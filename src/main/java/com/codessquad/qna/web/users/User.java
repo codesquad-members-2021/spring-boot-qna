03a0a0229b01d6cccb28ca9ff488cf6df5d2649e
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-05 15:53:22 +0900

    User클래스의 id에 Generation strategy를 추가하라
    - GenerationType.IDENTITY를 추가함
    - id에 대해 getter/setter 추가함

--- src/main/java/com/codessquad/qna/web/users/User.java
+++ src/main/java/com/codessquad/qna/web/users/User.java
@@ -1,14 +1,11 @@
 package com.codessquad.qna.web.users;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 20)
@@ -26,6 +23,14 @@ public class User {
 
     public User() {
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getUserId() {
