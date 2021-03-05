26c41a730f400374c928eb20eda51e7b496cdc29
Sally Oh <min27604@hotmail.com>
2021-03-05 22:04:40 +0900

    refactor: Fix URL directing

--- src/main/java/com/codessquad/qna/config/MvcConfig.java
+++ src/main/java/com/codessquad/qna/config/MvcConfig.java
@@ -12,7 +12,6 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/user/form").setViewName("user/form");
-        registry.addViewController("/user/list").setViewName("user/list");
         registry.addViewController("/user/login").setViewName("user/login");
         registry.addViewController("/questions/form").setViewName("qna/form");
     }
--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -6,23 +6,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("user")
 public class UserController {
     private List<User> users = new ArrayList<>();
 
-    @PostMapping("user/create")
-    public String create(User user, Model model) {
+    @PostMapping("/create")
+    public String create(User user) {
         users.add(user);
         System.out.println(user);
-        model.addAttribute("user", user);
+        return "redirect:/user";
+    }
+
+    @GetMapping("")
+    public String list(Model model) {
+        model.addAttribute("users", users);
         return "user/list";
     }
 
-    @GetMapping("user/{userId}")
+    @GetMapping("/{userId}")
     public String profile(@PathVariable String userId, Model model) {
         for (User user : users) {
             if (user.getUserId().equals(userId)) {
