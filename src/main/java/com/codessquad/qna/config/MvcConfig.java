19677b30dc162c676d746692c2155988b60759d9
malaheaven <yunhg95@gmail.com>
2021-03-05 10:53:05 +0900

    feat: URL과 html 쉽게 연결하기 위해 MvcConfig 클래스 생성하여 코드 적용

---
+++ src/main/java/com/codessquad/qna/config/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.codessquad.qna.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/users/profile").setViewName("users/profile");
+        registry.addViewController("/users/form").setViewName("users/form");
+        registry.addViewController("/users/login").setViewName("user/login");
+        registry.addViewController("/questions/form").setViewName("qna/form");
+    }
+}
