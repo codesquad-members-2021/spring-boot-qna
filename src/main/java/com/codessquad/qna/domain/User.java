827d1093c8495404bc4d6cebf105bb569c5a0908
K <PizzaCola.K@gmail.com>
2021-03-05 11:44:15 +0900

    refactor: 사용하지 않는 User의 getter setter 삭제
    - User의 getter와 setter를 정리하였습니다.
    - User와 Question은 HTTP 요청 내용을 담는 커맨드 객체로도 사용하고 있는데,
    이는 될수 있으면 생성자로 받아오도록 하였습니다.
    - 기본 생성자는 jpa에서만 사용하도록 protected로 설정하였습니다.

--- src/main/java/com/codessquad/qna/domain/User.java
+++ src/main/java/com/codessquad/qna/domain/User.java
@@ -16,44 +16,29 @@ public class User {
     private String name;
     private String email;
 
-    public Long getId() {
-        return id;
+    protected User() {}
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Long getId() {
+        return id;
     }
 
     public String getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public boolean isMatchingPassword(String oldPassword) {
