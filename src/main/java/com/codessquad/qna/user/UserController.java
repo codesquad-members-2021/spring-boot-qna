fa5284fbbe15f45e172dd718ee049dcfac18995a
95degree <95degree@naver.com>
2021-03-05 16:36:26 +0900

    UserController 리팩토링
    mvcConfig에 맞게 올바른 html을 출력해 주기 위해 UserController를 리팩토링 했습니다.

--- src/main/java/com/codessquad/qna/user/UserController.java
+++ src/main/java/com/codessquad/qna/user/UserController.java
@@ -5,35 +5,37 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     private List<User> users = new ArrayList<>();
 
-    @PostMapping("/users")
+    @PostMapping("")
     public String signup(User user){
         users.add(user);
         System.out.println("user : " + user);
         return "redirect:/users";
     }
 
-    @GetMapping("/users")
+    @GetMapping("")
     public String list(Model model){
         model.addAttribute("users",users);
-        return "users/list";
+        return "user/list";
     }
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/{userId}")
     public String viewUserProfile(@PathVariable String userId, Model model){
         for(User user : users){
             if(user.getUserId().equals(userId)){
                 model.addAttribute("user",user);
-                return "users/profile";
+                return "user/profile";
             }
         }
-        return "redirce:/users";
+        return "redirect:/users";
     }
 }
