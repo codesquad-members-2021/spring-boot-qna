8f854a2120e9c0605e91cc27c47053e7b302e084
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-05 18:28:24 +0900

    서브네비바에서 현재 선택된 개체에 Active가 적용되도록 수정하라
    - 파이로님이 도와주셔서 자바스크립트로 해당 기능을 구현하게 되었다.
    - 기왕 JQuery 있는거 한번 써보고 싶어서 JQuery를 통해 구현함

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,19 @@
-String.prototype.format = function() {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
-    return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-  });
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+            ;
+    });
 };
+
+$(document).ready(() => { // Powered By Pyro
+    let currHref = location.pathname
+    $(".sub-nav-li").each((index, item) => {
+        let hrefOfItem = $(item).children().first().attr("href")
+        if (currHref === hrefOfItem) {
+            $(item).addClass("active")
+        }
+    })
+})
