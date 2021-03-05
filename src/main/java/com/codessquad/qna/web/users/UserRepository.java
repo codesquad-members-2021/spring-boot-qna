e7e07ad62f9b71d78a91076d8fceb1c141025149
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-05 19:42:41 +0900

    JpaRepository대신 CrudRepository를 쓰도록 수정하라

--- src/main/java/com/codessquad/qna/web/users/UserRepository.java
+++ src/main/java/com/codessquad/qna/web/users/UserRepository.java
@@ -1,7 +1,7 @@
 package com.codessquad.qna.web.users;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 
 }
