5e2409d84dbda4f8bcb5ea4473671e219431bc92
gisungchoi <chlrltjd159@naver.com>
2021-03-05 17:10:57 +0900

    feat : update put 메소드로 변경
    put 메소드를 사용하기 위새 hideen값으로 _method를 보낸다.

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -6,10 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -53,7 +50,7 @@ public class UserController {
         return "user/userUpdateForm";
     }
 
-    @PostMapping("/update")
+    @PutMapping("/update")
     public String userUpdate(User user, String newPassword) {
         User getUser = userService.findById(user.getId()).orElseThrow(IllegalArgumentException::new);
         if (user.checkPassword(getUser)) {
