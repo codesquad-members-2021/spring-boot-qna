97fd82e2d411763ec3e272d03ad289b0d2ba5495
janeljs <jisunlim818@gmail.com>
2021-03-05 18:46:30 +0900

    Remove unnecessary import statements

--- src/main/java/com/codessquad/qna/controller/HttpSessionUtils.java
+++ src/main/java/com/codessquad/qna/controller/HttpSessionUtils.java
@@ -3,7 +3,6 @@ package com.codessquad.qna.controller;
 import com.codessquad.qna.domain.User;
 
 import javax.servlet.http.HttpSession;
-import java.util.Optional;
 
 public class HttpSessionUtils {
     public static final String USER_SESSION_KEY = "sessionUser";
--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -1,22 +1,18 @@
 package com.codessquad.qna.controller;
 
-import com.codessquad.qna.domain.Question;
-import com.codessquad.qna.domain.User;
+import com.codessquad.qna.domain.*;
 import com.codessquad.qna.repository.QuestionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 import javax.servlet.http.HttpSession;
+import static com.codessquad.qna.controller.HttpSessionUtils.*;
 
-import static com.codessquad.qna.controller.HttpSessionUtils.getSessionUser;
-import static com.codessquad.qna.controller.HttpSessionUtils.isLoginUser;
 
 @Controller
 @RequestMapping("/questions")
