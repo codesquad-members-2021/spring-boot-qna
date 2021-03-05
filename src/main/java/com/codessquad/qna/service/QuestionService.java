c4e29009206f345488c734aec0b561084f56da44
gisungchoi <chlrltjd159@naver.com>
2021-03-05 16:44:54 +0900

    feat : QuestionService  class 정의
    비지니스 로직은 아직 없지만 Repository의 중간역할을 하게했습니다.

---
+++ src/main/java/com/codessquad/qna/service/QuestionService.java
@@ -0,0 +1,29 @@
+package com.codessquad.qna.service;
+
+import com.codessquad.qna.domain.Question;
+import com.codessquad.qna.repository.QuestionRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class QuestionService {
+
+    private QuestionRepository questionRepository;
+
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    @Transactional
+    public void write(Question question) {
+        questionRepository.save(question);
+    }
+
+    public Optional<Question> findById(Long questionId) {
+        return questionRepository.findById(questionId);
+    }
+
+}
