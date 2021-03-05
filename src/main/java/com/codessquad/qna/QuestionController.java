577db98ff9b21697a5df574c322c9a55c89efe77
noel <68936@naver.com>
2021-03-05 22:55:24 +0900

    [refactor] 불필요한 코드 제거

--- src/main/java/com/codessquad/qna/QuestionController.java
+++ src/main/java/com/codessquad/qna/QuestionController.java
@@ -1,7 +1,5 @@
 package com.codessquad.qna;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,37 +13,35 @@ import java.util.Objects;
 
 @Controller
 public class QuestionController {
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
     private List<Question> questionList = new ArrayList<>();
 
     @PostMapping("/questions")
-    private String questions(Question question, Model model){
-        logger.info(question.toString());
-        question.setId(questionList.size()+1); // 질문 객체 아이디 정의
+    private String questions(Question question, Model model) {
+        question.setId(questionList.size() + 1); // 질문 객체 아이디 정의
         question.setTime(LocalDateTime.now()); // 게시글 시간 설정
         questionList.add(question);
-        model.addAttribute("questions",questionList);
+        model.addAttribute("questions", questionList);
         return "redirect:/";
     }
+
     @GetMapping("/")
-    private String questionsList(Model model){
-        model.addAttribute("invalidMember",false);
-        model.addAttribute("invalidPassword",false);
-        model.addAttribute("questions",questionList);
+    private String questionsList(Model model) {
+        model.addAttribute("questions", questionList);
         return "/index";
     }
+
     @GetMapping("/questions/{id}")
-    private String showQuestionContents(@PathVariable("id") int targetId, Model model){
-        for(Question findQuestion : questionList){
-            if(Objects.equals(findQuestion.getId(),targetId)){
-                model.addAttribute("invalidMember",false);
-                model.addAttribute("title",findQuestion.getTitle());
-                model.addAttribute("writer",findQuestion.getWriter());
-                model.addAttribute("time",findQuestion.getTime());
-                model.addAttribute("contents",findQuestion.getContents());
+    private String showQuestionContents(@PathVariable("id") int targetId, Model model) {
+        for (Question findQuestion : questionList) {
+            if (Objects.equals(findQuestion.getId(), targetId)) {
+                model.addAttribute("invalidMember", false);
+                model.addAttribute("title", findQuestion.getTitle());
+                model.addAttribute("writer", findQuestion.getWriter());
+                model.addAttribute("time", findQuestion.getTime());
+                model.addAttribute("contents", findQuestion.getContents());
                 break;
-            }else{
-                model.addAttribute("invalidMember",true);
+            } else {
+                model.addAttribute("invalidMember", true);
             }
         }
         return "qna/show";
--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -1,7 +1,5 @@
 package com.codessquad.qna;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,75 +13,72 @@ import java.util.Objects;
 @Controller
 public class UserController {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
     private List<User> userList = new ArrayList<>();
 
     @PostMapping("/user/create")
-    private String register(User user){
+    private String register(User user) {
         userList.add(user);
-        logger.info(user.toString());
         return "redirect:/users";
     }
 
     @GetMapping("/users")
-    private String getMemberList(Model model){
-        model.addAttribute("users",userList);
+    private String getMemberList(Model model) {
+        model.addAttribute("users", userList);
         return "user/list";
     }
 
     @GetMapping("/users/{userId}")
-    private String displayProfile(@PathVariable("userId") String userId, Model model){
-        userId = Objects.toString(userId,""); // null 값일 경우 "" 정의
+    private String displayProfile(@PathVariable("userId") String userId, Model model) {
+        userId = Objects.toString(userId, ""); // null 값일 경우 "" 정의
 
-        for(User findUser:userList){
-            if(findUser.getUserId().equals(userId)) {
-                model.addAttribute("invalidMember",true);
-                model.addAttribute("userID",findUser.getUserId());
-                model.addAttribute("email",findUser.getEmail());
-            }else{
-                model.addAttribute("invalidMember",true);
+        for (User findUser : userList) {
+            if (findUser.getUserId().equals(userId)) {
+                model.addAttribute("invalidMember", false);
+                model.addAttribute("userID", findUser.getUserId());
+                model.addAttribute("email", findUser.getEmail());
+            } else {
+                model.addAttribute("invalidMember", true);
             }
         }
         return "user/profile";
     }
 
     @GetMapping("users/{userId}/form")
-    private String changeMemberInfo(@PathVariable("userId") String userId, Model model){
-        userId = Objects.toString(userId,""); // null 값일 경우 "" 정의
+    private String changeMemberInfo(@PathVariable("userId") String userId, Model model) {
+        userId = Objects.toString(userId, ""); // null 값일 경우 "" 정의
 
-        for(User findUser:userList){
-            if(Objects.equals(findUser.getUserId(),userId)) {
-                model.addAttribute("invalidMember",false);
-                model.addAttribute("userID",findUser.getUserId());
-                model.addAttribute("password",findUser.getPassword());
-                model.addAttribute("name",findUser.getName());
-                model.addAttribute("email",findUser.getEmail());
+        for (User findUser : userList) {
+            if (Objects.equals(findUser.getUserId(), userId)) {
+                model.addAttribute("invalidMember", false);
+                model.addAttribute("userID", findUser.getUserId());
+                model.addAttribute("password", findUser.getPassword());
+                model.addAttribute("name", findUser.getName());
+                model.addAttribute("email", findUser.getEmail());
                 break;
-            }else{
-                model.addAttribute("invalidMember",true);
+            } else {
+                model.addAttribute("invalidMember", true);
             }
         }
         return "user/updateForm";
     }
 
     @PostMapping("/users/{userId}/update")
-    private String updateMemberList(User updateUser, Model model){
+    private String updateMemberList(User updateUser, Model model) {
 
-        for(int index = 0; index<userList.size() ; index++){
-            boolean findIdCheck = Objects.equals(userList.get(index).getUserId(),updateUser.getUserId()); // null 비교가능
-            boolean findPasswordCheck = Objects.equals(userList.get(index).getPassword(),updateUser.getPassword()); // null 비교가능
+        for (int index = 0; index < userList.size(); index++) {
+            boolean findIdCheck = Objects.equals(userList.get(index).getUserId(), updateUser.getUserId()); // null 비교가능
+            boolean findPasswordCheck = Objects.equals(userList.get(index).getPassword(), updateUser.getPassword()); // null 비교가능
 
-            if (findIdCheck && findPasswordCheck){
-                model.addAttribute("invalidPassword",false);
-                userList.get(index).setName(Objects.toString(updateUser.getName(),"")); // null default
-                userList.get(index).setEmail(Objects.toString(updateUser.getEmail(),"")); // null default
-            }else{
-                model.addAttribute("invalidPassword",true);
+            if (findIdCheck && findPasswordCheck) {
+                model.addAttribute("invalidPassword", false);
+                userList.get(index).setName(Objects.toString(updateUser.getName(), "")); // null default
+                userList.get(index).setEmail(Objects.toString(updateUser.getEmail(), "")); // null default
+            } else {
+                model.addAttribute("invalidPassword", true);
             }
-            model.addAttribute("users",userList);
+            model.addAttribute("users", userList);
         }
         return "user/list";
     }
 
 }
-
