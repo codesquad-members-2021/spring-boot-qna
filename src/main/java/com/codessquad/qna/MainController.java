defaf32d090323100c131b05286c91c982e3698a
정이삭 <isaac56@naver.com>
2021-03-05 12:27:35 +0900

    Implements showing question list
    MainController를 사용하여 MainPage에서 Question List를 보여주는 기능 구현

---
+++ src/main/java/com/codessquad/qna/MainController.java
@@ -0,0 +1,21 @@
+package com.codessquad.qna;
+
+import com.codessquad.qna.questions.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping
+    String getMainPage(Model model) {
+        model.addAttribute("questions", questionService.getQuestions());
+        return "index";
+    }
+}
