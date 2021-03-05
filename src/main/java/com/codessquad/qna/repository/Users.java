c366c38408d8ae0e5a057b3bc6f29074efc34065
jeong-inho <derosatam76@gmail.com>
2021-03-05 16:56:38 +0900

    Update updateUser method in Users class Users 클래스의 UpdateUser 메소드에 newPassword 인자를 추가하여 기존에 비밀번호가 일치하면 정보를 변경하도록 수정, 현재 불필요한 deleteUser 메소드 삭제

--- src/main/java/com/codessquad/qna/repository/Users.java
+++ src/main/java/com/codessquad/qna/repository/Users.java
@@ -10,8 +10,13 @@ public class Users {
 
     private final List<User> userList = new ArrayList<>();
 
-    public void addUser(User user) {
-        userList.add(user);
+    public boolean addUser(User user) {
+        int index = getUserIndex(user.getUserId());
+        if (index == -1) {
+            userList.add(user);
+            return true;
+        }
+        return false;
     }
 
     public List<User> getAllUser() {
@@ -20,31 +25,29 @@ public class Users {
 
     public User findUser(String userId) {
         int index = getUserIndex(userId);
-        return this.userList.get(index);
-    }
-
-    public void deleteUser(String userId) {
-        int index = getUserIndex(userId);
-        this.userList.remove(index);
+        if (index != -1) {
+            return this.userList.get(index);
+        }
+        return new User();
     }
 
     private int getUserIndex(String userId) {
-        int index = IntStream.range(0, this.userList.size())
+        return IntStream.range(0, this.userList.size())
                 .filter(i -> this.userList.get(i).getUserId().equals(userId))
                 .findFirst()
                 .orElse(-1);
-        if (index == -1) {
-            throw new IllegalStateException("해당 유저가 없습니다.");
-        }
-        return index;
     }
 
-    public void updateUser(String userId, User user) {
+    public boolean updateUser(String userId, User user, String newPassword) {
         User oldUser = findUser(userId);
-        oldUser.setUserId(user.getUserId());
-        oldUser.setPassword(user.getPassword());
-        oldUser.setName(user.getName());
-        oldUser.setEmail(user.getEmail());
+        if (oldUser.getUserId() != null && user.getPassword().equals(oldUser.getPassword())) {
+            oldUser.setUserId(user.getUserId());
+            oldUser.setPassword(newPassword);
+            oldUser.setName(user.getName());
+            oldUser.setEmail(user.getEmail());
+            return true;
+        }
+        return false;
     }
 
 }
