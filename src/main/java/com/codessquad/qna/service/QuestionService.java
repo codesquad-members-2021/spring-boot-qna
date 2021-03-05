5933d963cad96d5c9d941fd7bcce07034c2db31f
gisungchoi <chlrltjd159@naver.com>
2021-03-05 17:06:24 +0900

    feat : 글 쓴 시간 update
    글 쓴 시간을 넣어줬다.

--- src/main/java/com/codessquad/qna/service/QuestionService.java
+++ src/main/java/com/codessquad/qna/service/QuestionService.java
@@ -5,6 +5,7 @@ import com.codessquad.qna.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -19,6 +20,7 @@ public class QuestionService {
 
     @Transactional
     public void write(Question question) {
+        question.setWriteTime(LocalDateTime.now());
         questionRepository.save(question);
     }
 
