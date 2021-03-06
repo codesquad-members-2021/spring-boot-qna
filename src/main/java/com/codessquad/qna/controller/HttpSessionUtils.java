7a197dd1c80cb4aca72e53702accdf3fb7097bb6
janeljs <jisunlim818@gmail.com>
2021-03-06 21:39:33 +0900

    Add fields to Answer class

--- src/main/java/com/codessquad/qna/controller/HttpSessionUtils.java
+++ src/main/java/com/codessquad/qna/controller/HttpSessionUtils.java
@@ -8,10 +8,7 @@ public class HttpSessionUtils {
     public static final String USER_SESSION_KEY = "sessionUser";
 
     public static boolean isLoginUser(HttpSession session) {
-        if (getSessionUser(session)==null) {
-            return false;
-        }
-        return true;
+        return getSessionUser(session) != null;
     }
 
     public static User getSessionUser(HttpSession session) {
--- src/main/java/com/codessquad/qna/domain/Answer.java
+++ src/main/java/com/codessquad/qna/domain/Answer.java
@@ -1,4 +1,21 @@
 package com.codessquad.qna.domain;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_answer_author"))
+    private User author;
+
+    @Lob
+    private String contents;
+    private LocalDateTime date;
+
+
 }
