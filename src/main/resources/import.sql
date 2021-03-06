a7fff43e634953e3c257951f62ce716435a9a7f0
tmdgusya <tmdgusya@naver.com>
2021-03-07 00:45:16 +0900

    feat : 새로운 테스트 데이터 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-insert into USER (USER_ID, PASSWORD, NAME, EMAIL) values ('roach', '1234', 'roach', 'tmdgusya@naver.com');
-
+insert into USER (USER_ID, PASSWORD, NAME, EMAIL) values ('roach', '1234', 'roach', 'roach@naver.com');
+insert into USER (USER_ID, PASSWORD, NAME, EMAIL) values ('honux', '1234', 'honux', 'honux@naver.com');
