acc361086d190070e563f64c23362d21aac4c814
고정완 (Can) <jw.gho@ecubelabs.com>
2021-03-07 13:33:13 +0900

    [refactor] uri 를 전체적으로 싹 수정
    user/create -> users/create
    question/create -> questions/create
    기왕 하는 김에 RequestMapping 이 더 통일감이 있다고 생각하여 바꾸었습니다.

--- src/main/java/com/codessquad/qna/web/question/QuestionController.java
+++ src/main/java/com/codessquad/qna/web/question/QuestionController.java
@@ -2,27 +2,28 @@ package com.codessquad.qna.web.question;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/questions")
 public class QuestionController {
     QuestionRepository questionRepository = new QuestionRepository();
 
-    @PostMapping("/question/create")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
     public String question_create(Question question) {
         questionRepository.add(question);
         return "redirect:/questions";
     }
 
-    @GetMapping("/questions")
+    @RequestMapping(method = RequestMethod.GET)
     public String question_list(Model model) {
         model.addAttribute("questions", questionRepository.getQuestions());
         return "question/list";
     }
 
-    @GetMapping("/questions/{questionId}")
+    @RequestMapping(value = "/{questionId}", method = RequestMethod.GET)
     public String question_profile(@PathVariable("questionId") int questionId, Model model) {
         model.addAttribute("question", questionRepository.getQuestion(questionId));
         return "question/show";
--- src/main/java/com/codessquad/qna/web/user/UserController.java
+++ src/main/java/com/codessquad/qna/web/user/UserController.java
@@ -2,40 +2,41 @@ package com.codessquad.qna.web.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     private UserRepository userRepository = new UserRepository();
 
-    @PostMapping("/user/create")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
     public String user_create(User user) {
         userRepository.add(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/users")
+    @RequestMapping(method = RequestMethod.GET)
     public String user_list(Model model) {
         model.addAttribute("users", userRepository.getUsers());
         return "user/list";
     }
 
-    @GetMapping("/users/{userId}")
+    @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
     public String user_profile(@PathVariable("userId") String userId, Model model) {
         model.addAttribute("user", userRepository.getUser(userId));
         return "user/profile";
     }
 
-    @GetMapping("/users/{userId}/form")
+    @RequestMapping(value = "/{userId}/form", method = RequestMethod.GET)
     public String user_form(@PathVariable("userId") String userId, Model model) {
         model.addAttribute("user", userRepository.getUser(userId));
         return "user/updateForm";
     }
 
-    @PostMapping("/users/{userId}/update")
+    @RequestMapping(value = "/{userId}/update", method = RequestMethod.POST)
     public String user_update(@PathVariable("userId") String userId, User user) {
         userRepository.update(userId, user);
         return "redirect:/users";
