5392bbdc8e2b76a7d52afb782eee423cab621112
malaheaven <yunhg95@gmail.com>
2021-03-07 18:17:26 +0900

    feat: [회원정보 수정] contoller 메소드 작성 (updateForm() -> 회원정보 수정 화면, update() -> 회원정보 수정)
    첫번째, 회원정보 수정 화면 기능 구현
    두번째, 회원정보 수정 구현

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -15,23 +15,44 @@ public class UserController {
     private List<User> users = new ArrayList<User>();
 
     @PostMapping("/users")
-    public String create(User user){
+    public String create(User user) {
         users.add(user);
         return "redirect:/users";
     }
 
     @GetMapping("/users")
-    public String list(Model model){
-        model.addAttribute("users",users);
+    public String list(Model model) {
+        model.addAttribute("users", users);
         return "users/list";
     }
 
     @GetMapping("/users/{userId}")
-    public String profile(@PathVariable("userId") String userId, Model model){
-        for(User user: users){
-            if(user.getUserId().equals(userId))
-                model.addAttribute("user",user);
+    public String profile(@PathVariable("userId") String userId, Model model) {
+        for (User user : users) {
+            if (user.getUserId().equals(userId))
+                model.addAttribute("user", user);
         }
-        return"users/profile";
+        return "users/profile";
+    }
+
+    @GetMapping("/users/{userId}/form")
+    public String updateForm(@PathVariable("userId") String userId, Model model) {
+        for (User user : users) {
+            if (user.getUserId().equals(userId))
+                model.addAttribute("user", user);
+        }
+        return "users/updateForm";
+    }
+
+    @PostMapping("/users/{userId}/update")
+    public String update(@PathVariable("userId") String userId, User updateUserInfo, Model model) {
+        for (User user : users) {
+            if (user.getUserId().equals(userId)) {
+                user.setName(updateUserInfo.getName());
+                user.setEmail(updateUserInfo.getEmail());
+            }
+            model.addAttribute("user", user);
+        }
+        return "redirect:/users";
     }
 }
