9fca0928db5d55b59d29efd4a78b21b68a2c008f
malaheaven <yunhg95@gmail.com>
2021-03-07 13:39:48 +0900

    feat:[질문 상세보기] show 메소드 추가
    질문 객체를 추가할 때 ArrayList.size() + 1을 질문 객체의 id로 사용
    ArrayList에서 index - 1 해당하는 데이터를 조회한 후 Model에 저장해 /qna/show.html에 전달

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -4,6 +4,7 @@ import com.codessquad.qna.model.Question;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.*;
@@ -15,6 +16,7 @@ public class QuestionController {
 
     @PostMapping("/questions")
     public String create(Question question){
+        question.setId(questions.size()+1);
         questions.add(question);
         return "redirect:/";
     }
@@ -23,5 +25,12 @@ public class QuestionController {
     public String list(Model model){
         model.addAttribute("questions",questions);
         return "index";
+    }
+
+    @GetMapping("/questions/{id}")
+    public String show(@PathVariable("id") int index, Model model){
+        Question question = questions.get(index - 1);
+        model.addAttribute("question",question);
+        return "qna/show";
     }
 }
