5fe2f63a00b21a6d259c1c200a67a3fe2072e5ef
K <PizzaCola.K@gmail.com>
2021-03-07 12:33:33 +0900

    test: Add Users for test
    - 테스트를 위해 유저 정보를 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (1, 'javajigi', 'test', '자바지기', 'javajigi@slipp.net');
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (2, 'sanjigi', 'test', '산지기', 'sanjigi@slipp.net');
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (3, 'pzcak', '123', 'K', 'k@k.com');
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (4, 'kyu', '123', 'Kyu', 'kyu@kyu.com');
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (5, 'cooper', '123', 'Cooper', 'cooper@cooper.com');
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (6, 'august', '123', 'August', 'august@august.com');
