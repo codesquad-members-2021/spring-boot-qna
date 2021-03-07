0e162da30d9947e576a0c1ac667b7f61098a93f4
Jihye Woo <woojihye2339@gmail.com>
2021-03-07 12:06:06 +0900

    feat : 질문하기 페이지 이동(href 링크)
      > MvcConfig 클래스, 뷰 컨트롤러 추가
       - url 경로 "/questions/form"와 뷰 "qna/form"
    
      > index.html의 질문하기 카테고리 url 링크 수정
        - a href="questions/form"
        - QuestionController의 getForm()과 매핑

--- src/main/java/com/codessquad/qna/config/MvcConfig.java
+++ src/main/java/com/codessquad/qna/config/MvcConfig.java
@@ -13,5 +13,6 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
         registry.addViewController("/users/form").setViewName("user/form");
+        registry.addViewController("/questions/form").setViewName("qna/form");
     }
 }
