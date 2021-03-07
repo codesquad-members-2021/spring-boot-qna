d907e01b47cf33eab8bf65beea9f4842fb852bf5
Jihye Woo <woojihye2339@gmail.com>
2021-03-07 12:36:07 +0900

    feat : 질문 목록 관리 리스트
      > QuestionController, ArrayList<Question> questions
        - 질문 목록을 관리하는 ArrayList 생성
    
      > create()에서 인자로 받은 질문 데이터(question)을 사용자 목록(questions)에 저장

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class QuestionController {
+
+    List<Question> questions = new ArrayList<>();
 
     @GetMapping("/questions/form")
     public String getForm(){
@@ -15,7 +20,7 @@ public class QuestionController {
 
     @PostMapping("/questions")
     public String create(Question question){
-        System.out.println(question);
+        questions.add(question);
         return "redirect:/";
     }
 
