adb53d8cdd640ac2064abc5a475aa6989aed8628
Jihye Woo <woojihye2339@gmail.com>
2021-03-07 14:06:41 +0900

    refactor: 컨트롤러 공통 url 묶어주기
      > UserController의 "users"
      > QuestionController의 "questions"

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -7,21 +7,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/questions")
 public class QuestionController {
 
     List<Question> questions = new ArrayList<>();
 
-    @GetMapping("/questions/form")
+    @GetMapping("/form")
     public String getForm(){
         return "qna/form";
     }
 
-    @GetMapping("/questions/{index}")
+    @GetMapping("/{index}")
     public String getQuestionDetail(@PathVariable("index") String stringIdx, Model model){
         int index = getIndex(stringIdx);
         Question question = questions.get(index - 1);
@@ -29,7 +31,7 @@ public class QuestionController {
         return "/qna/show";
     }
 
-    @PostMapping("/questions")
+    @PostMapping("/")
     public String create(Question question){
         question.setId(nextId());
         questions.add(question);
--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -6,27 +6,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     private List<User> users = new ArrayList<>();
 
-    @PostMapping("/user/create")
+    @PostMapping("/create")
     public String create(User user){
         users.add(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/users")
+    @GetMapping("")
     public String getUserList(Model model){
         model.addAttribute("users", users);
         return "user/list";
     }
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/{userId}")
     public String getUserProfile(@PathVariable("userId") String userId, Model model){
         for(User user : users){
             if(user.getUserId().equals(userId)){
