ef685014e1232e32141b3891bc760de1caa81d7d
95degree <95degree@naver.com>
2021-03-08 17:21:30 +0900

    viewQuestion() 생성
    선택한 질문을 출력해 주기 위해 viewQuestion()를 생성했습니다.

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -4,6 +4,7 @@ import com.codessquad.qna.domain.Question;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -23,5 +24,10 @@ public class QuestionController {
     public String list(Model model){
         model.addAttribute("questions", questions);
         return "index";
+    }
+
+    @GetMapping("/questions/{index}")
+    public String viewQuestion(@PathVariable int index, Model model){
+
     }
 }
