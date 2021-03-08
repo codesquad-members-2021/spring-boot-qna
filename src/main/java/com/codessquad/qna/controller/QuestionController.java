ee8d1c6f7414c97320678714550f0ff4b985c970
95degree <95degree@naver.com>
2021-03-08 16:20:58 +0900

    createQuestion() 추가
    질문기능 구현을 처리하기 위해 createQuestion()를 추가 했습니다.

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -1,4 +1,11 @@
 package com.codessquad.qna.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class QuestionController {
+    @PostMapping
+    public String createQuestion(){
+    }
 }
