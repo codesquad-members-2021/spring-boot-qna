9471f000cad440037fa31121460cd6180aa29719
aksmf1442 <aksmf1442@naver.com>
2021-03-08 17:07:38 +0900

    Refactor : MemoryUserRepository
    성능 향상을 위해서 Collentions.synchrozinedMap을 ConcurrenthasMap으로 변경했다.

--- src/main/java/com/codessquad/qna/repository/MemoryUserRepository.java
+++ src/main/java/com/codessquad/qna/repository/MemoryUserRepository.java
@@ -3,16 +3,15 @@ package com.codessquad.qna.repository;
 
 import com.codessquad.qna.domain.User;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemoryUserRepository implements UserRepository {
 
-    private final Map<String, User> userMap = Collections.synchronizedMap(new HashMap<>());
+    private final Map<String, User> userMap = new ConcurrentHashMap<>();
 
     @Override
     public User save(User user) {
