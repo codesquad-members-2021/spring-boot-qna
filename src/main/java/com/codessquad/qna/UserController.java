148565d842d3ed2d7b6589ec9aa1e756a22f7a03
rla36 <bhmarsd@naver.com>
2021-03-08 10:37:09 +0900

    회원 프로필 정보 보기 기능 구현

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -1,6 +1,9 @@
 package com.codessquad.qna;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -8,7 +11,7 @@ import java.util.List;
 
 @Controller
 public class UserController {
-    List<User> users = new ArrayList<>();
+    private List<User> users = new ArrayList<>();
 
     @PostMapping("/user/create")
     public String createUser(User user) {
@@ -16,5 +19,16 @@ public class UserController {
         return "redirect:/users";
     }
 
+    @GetMapping("/users")
+    public String getUserList(Model model) {
+        model.addAttribute("users", users);
+        return "list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String getSpecificUser(@PathVariable String userId) {
+
+        return "user/profile";
+    }
 
 }
