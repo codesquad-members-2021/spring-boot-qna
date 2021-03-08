6329c0a5460758edaf517f28e8f21a4ca4843659
JoonKi-Kim <joonki402@gmail.com>
2021-03-08 14:20:36 +0900

    요구사항에 맞게 UserRepository CrudRepository로 변경

--- src/main/java/com/codessquad/qna/config/HiddenHttp.java
+++ src/main/java/com/codessquad/qna/config/HiddenHttp.java
@@ -5,15 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 @Configuration
 public class HiddenHttp {
+
     @Bean
     public FilterRegistrationBean hiddenHttpMethodFilter() {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new HiddenHttpMethodFilter());
         filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
         return filterRegistrationBean;
     }
+
 }
--- src/main/java/com/codessquad/qna/repository/UserRepository.java
+++ src/main/java/com/codessquad/qna/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.codessquad.qna.repository;
 
 import com.codessquad.qna.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 }
