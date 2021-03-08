e930204f0f75cabfaefbbad673cb7a0c5e402387
95degree <95degree@naver.com>
2021-03-08 16:56:09 +0900

    list() 추가
    메인 페이지에 질문목록을 볼 수 있게 하기 위해 list()를 추가 했습니다.

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -2,6 +2,8 @@ package com.codessquad.qna.controller;
 
 import com.codessquad.qna.domain.Question;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -14,6 +16,12 @@ public class QuestionController {
     @PostMapping("/questions")
     public String createQuestion(Question question){
         questions.add(question);
+        return "redirect:/";
+    }
+
+    @GetMapping("/")
+    public String list(Model model){
+        model.addAttribute("questions", questions);
         return "redirect:/";
     }
 }
