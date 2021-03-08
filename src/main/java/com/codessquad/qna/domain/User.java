3aa970cb3e89a152c65c4da3779656f9d4b8fabf
pbg0205 <pbg0205@naver.com>
2021-03-08 14:44:19 +0900

    feat : User AllArgsConstructor 생성
    - 생성자를 통해 updateForm에서 입력한 정보를 객체로 전달할 수 있도록
    구현하였습니다.

--- src/main/java/com/codessquad/qna/domain/User.java
+++ src/main/java/com/codessquad/qna/domain/User.java
@@ -6,6 +6,13 @@ public class User {
     private String name;
     private String email;
 
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
     public String getUserId() {
         return userId;
     }
@@ -20,22 +27,6 @@ public class User {
 
     public String getEmail() {
         return email;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public boolean matchId(String id) {
