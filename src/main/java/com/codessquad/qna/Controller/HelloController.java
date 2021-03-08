fe9af9a95f99a4acaa88706c1ec605c1d2477643
dong <ultraraptor@naver.com>
2021-03-08 17:10:00 +0900

    머스태취 문법 연습용 컨트롤러 및 뷰페이지 샘플 제작

---
+++ src/main/java/com/codessquad/qna/Controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.codessquad.qna.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hihosilver(String name, int age, Model model) {
+        model.addAttribute("name",name);
+        model.addAttribute("age",age);
+        //http://localhost:8080/hello?name=dsf&age=20
+        return "welcome";
+    }
+
+}
