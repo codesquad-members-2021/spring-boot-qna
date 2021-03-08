b87611cd5c1fc7c18233b3c73c5ab4e9524af542
tmdgusya <tmdgusya@naver.com>
2021-03-08 16:45:04 +0900

    fix : 오류나던 sql insert 변경

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-insert into USER (USER_ID, PASSWORD, NAME, EMAIL) values ('roach', '1234', 'roach', 'roach@naver.com');
-insert into USER (USER_ID, PASSWORD, NAME, EMAIL) values ('honux', '1234', 'honux', 'honux@naver.com');
+insert into USER (ID, USER_ID, PASSWORD, NAME, EMAIL) values (1, 'roach', '1234', 'roach', 'roach@naver.com');
+insert into User (ID, USER_ID, PASSWORD, NAME, EMAIL) values (2, 'honux', '1234', 'honux', 'honux@naver.com');
