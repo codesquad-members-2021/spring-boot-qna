a7e7da58d647b8bd8984f498b0f6d33b03efc377
tmdgusya <tmdgusya@naver.com>
2021-03-08 17:01:11 +0900

    refactor : 로그인된 유저 없을시 예외 리턴하도록 변경
    NotExistLoggedUserInSession 를 리턴하여 세션에 로그인된 유저가 없음을 알려준다.

--- src/main/java/com/codessquad/qna/util/HttpSessionUtils.java
+++ src/main/java/com/codessquad/qna/util/HttpSessionUtils.java
@@ -1,6 +1,7 @@
 package com.codessquad.qna.util;
 
 import com.codessquad.qna.entity.User;
+import com.codessquad.qna.exception.NotExistLoggedUserInSession;
 
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
@@ -14,9 +15,15 @@ public class HttpSessionUtils {
         return sessionUser != null;
     }
 
+    /**
+     * 세션에 로그인된 사용자가 없으면 NotExistLoggedUserInSession 를 throw
+     * 있으면 정상적인 유저를 리턴함 따라서 사용하는 곳에서 get() 을 사용해도됨.
+     * @param httpSession
+     * @return
+     */
     public static Optional<User> getUserFromSession(HttpSession httpSession) {
         if(!isLoggedUser(httpSession)){
-            return Optional.empty();
+            throw new NotExistLoggedUserInSession();
         }
         return Optional.of((User) httpSession.getAttribute(USER_SESSION_KEY));
     }
