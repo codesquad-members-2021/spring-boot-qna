bd90ed62a682578313cea8f9f3c07b7a5583db99
dong <ultraraptor@naver.com>
2021-03-08 15:59:20 +0900

    회원가입 목록 구현을 위한 user 도메인 구현

---
+++ src/main/java/com/codessquad/qna/domain/User.java
@@ -0,0 +1,54 @@
+package com.codessquad.qna.domain;
+
+public class User {
+
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
