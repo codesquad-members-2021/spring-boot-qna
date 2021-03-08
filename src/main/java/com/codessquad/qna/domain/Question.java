2bee1b7666cee1bdde8d16e16a7be14b16492db5
95degree <95degree@naver.com>
2021-03-08 16:29:25 +0900

    Question 클래스에 getter,setter추가
    사용자가 전달한 값을 처리하기 위한 멤버변수를 추가 하고 getter,setter를 추가했습니다.

--- src/main/java/com/codessquad/qna/domain/Question.java
+++ src/main/java/com/codessquad/qna/domain/Question.java
@@ -1,4 +1,31 @@
 package com.codessquad.qna.domain;
 
 public class Question {
+    private String writer;
+    private String title;
+    private String contents;
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getContents() {
+        return contents;
+    }
 }
