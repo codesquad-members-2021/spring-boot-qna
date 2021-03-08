f246d67c3243a3cf028f240f983fbf185d1a0ef0
pbg0205 <pbg0205@naver.com>
2021-03-08 22:37:58 +0900

    feat : 멤버변수 questionRepository 선언
    - Question에 관한 정보를 db에서 관리하게 하기 위해 기존의 List<Question>
    를 questionRepository로 변경하여db에서 자료를 관리할 수 있도록 하였습
    니다.

--- src/main/java/com/codessquad/qna/web/QuestionController.java
+++ src/main/java/com/codessquad/qna/web/QuestionController.java
@@ -1,23 +1,23 @@
 package com.codessquad.qna.web;
 
-import com.codessquad.qna.domain.Question;
+import com.codessquad.qna.domain.question.Question;
+import com.codessquad.qna.domain.question.QuestionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class QuestionController {
 
     Logger logger = LoggerFactory.getLogger(QuestionController.class);
 
-    private List<Question> questionList = new ArrayList<>();
+    @Autowired
+    private QuestionRepository questionRepository;
 
     @GetMapping("/questions/form")
     public String getQuestionFormPage() {
@@ -26,11 +26,10 @@ public class QuestionController {
 
     @PostMapping("/questions")
     public String submitQuestion(String writer, String title, String contents, Model model) {
-        long id = questionList.size() + 1;
-        Question question = new Question(id, writer, title, contents);
+        Question question = new Question(writer, title, contents);
 
+        questionRepository.save(question);
         model.addAttribute(question);
-        questionList.add(question);
 
         logger.info(question.toString());
 
@@ -39,13 +38,13 @@ public class QuestionController {
 
     @GetMapping("/")
     public String getQuestionListPage(Model model) {
-        model.addAttribute("questionList", questionList);
+        model.addAttribute("questionList", questionRepository.findAll());
         return "home";
     }
 
     @GetMapping("/questions/{index}")
     public String getQuestionDetail(@PathVariable(("index")) long index, Model model) {
-        Question question = questionList.get((int) index - 1);
+        Question question = questionRepository.findById(index).get();
         model.addAttribute("question", question);
 
         logger.info(question.toString());
