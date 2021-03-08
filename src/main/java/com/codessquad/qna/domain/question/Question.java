516df363ff4f1132cae7d88590420de1ec9f50c3
pbg0205 <pbg0205@naver.com>
2021-03-08 22:31:24 +0900

    feat : Question @Entity 선언
    - Question class에 @Entity를 선언해 entity임을 선언하고 db에 접근하는
    객체임을 선언하였습니다.

--- src/main/java/com/codessquad/qna/domain/question/Question.java
+++ src/main/java/com/codessquad/qna/domain/question/Question.java
@@ -1,17 +1,28 @@
-package com.codessquad.qna.domain;
+package com.codessquad.qna.domain.question;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Entity
 public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long index;
+
     private String writer;
     private String title;
     private String contents;
     private String createdTime;
 
-    public Question(long index, String writer, String title, String contents) {
-        this.index = index;
+    public Question() {
+    }
+
+    public Question(String writer, String title, String contents) {
         this.writer = writer;
         this.title = title;
         this.contents = contents;
