6bfd37bc5d0bd47e4d8b93ed773de9adae59cdb9
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-03-09 21:01:03 +0900

    DB 연결로 인한 수정 작업 진행:

--- src/main/java/com/codessquad/qna/web/QuestionController.java
+++ src/main/java/com/codessquad/qna/web/QuestionController.java
@@ -1,39 +1,40 @@
 package com.codessquad.qna.web;
 
+import com.codessquad.qna.domain.Qna;
+import com.codessquad.qna.domain.QnaRepository;
+import com.codessquad.qna.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class QuestionController {
-
-    List<Qna> qnaList = new ArrayList<>();
+    @Autowired
+    private QnaRepository qnaRepository;
 
     @PostMapping("/qna")
-    public String qnaMain(String writer, String title, String contents) {
-        Qna newQna = new Qna(qnaList.size() + 1, writer, title, contents);
-        System.out.println("QnA: " + writer);
-        qnaList.add(newQna);
+    public String qnaMain(Qna qna) {
+        qnaRepository.save(qna);
         return "redirect:/";
     }
 
     @GetMapping("/")
     public String getQnaList(Model model) {
-        model.addAttribute("qnaList", qnaList);
+        model.addAttribute("qnaList", qnaRepository.findAll());
         return "index";
     }
 
     @GetMapping("qna/show/{qnaId}")
-    public String getOneQuestion(@PathVariable("qnaId") int qnaId, Model model) {
-        Qna foundQuestion = qnaList.stream()
-                .filter((qua -> qua.getId() == qnaId))
-                .findFirst().orElse(null);
-        model.addAttribute("Qna", foundQuestion);
-        return "qna/show";
+    public ModelAndView getOneQuestion(@PathVariable long qnaId) {
+        ModelAndView modelAndView = new ModelAndView("qna/show");
+        modelAndView.addObject("Qna", qnaRepository.findById(qnaId).get());
+        return modelAndView;
     }
 }
--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -1,40 +1,41 @@
 package com.codessquad.qna.web;
 
+import com.codessquad.qna.domain.User;
+import com.codessquad.qna.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
-    private final List<User> userList = new ArrayList<>();
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/create")
     public String create(User user) {
-        userList.add(user);
+        userRepository.save(user);
         return "redirect:/list";
     }
 
     @GetMapping("/list")
     public String getList(Model model) {
-        model.addAttribute("users", userList);
+        model.addAttribute("users", userRepository.findAll());
         return "user/list";
     }
 
-    @GetMapping("/users/{userId}")
-    public String getOneUserProfile(@PathVariable("userId") String userId, Model model) {
-        User foundUser = getUserById(userId);
-        model.addAttribute("user", foundUser);
-        return "user/profile";
+    @PostMapping("/login")
+    public String login() {
+        return "";
     }
 
-    public User getUserById(String userId){
-        return userList.stream()
-                .filter(user -> user.getUserId().equals(userId))
-                .findFirst().orElse(null);
+    @GetMapping("/users/{id}")
+    public ModelAndView getOneUserProfile(@PathVariable long id) {
+        ModelAndView modelAndView = new ModelAndView("user/profile");
+        modelAndView.addObject("user", userRepository.findById(id).get());
+        return modelAndView;
     }
 }
