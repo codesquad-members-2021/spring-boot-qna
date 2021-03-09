4e9d380376ce8055f76867c802426c61bd15dcc3
jeong-inho <derosatam76@gmail.com>
2021-03-09 11:03:03 +0900

    Modify update method in UserService class 기존 update 메소드에서 User의 정보를 변경하던 일을 User class에게 넘기고 User class에서 제공해주는 update 메소드를 통해 정보를 변경하도록 수정

--- src/main/java/com/codessquad/qna/service/UserService.java
+++ src/main/java/com/codessquad/qna/service/UserService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @Service
 public class UserService {
@@ -40,20 +39,14 @@ public class UserService {
         return user.orElseGet(User::new);
     }
 
-    public boolean update(Long id, User user, String newPassword) {
-        AtomicBoolean isUpdate = new AtomicBoolean(false);
-        Optional<User> optionalUser = this.userRepository.findById(id);
-        optionalUser.ifPresent(targetUser -> {
-            if (targetUser.getPassword().equals(user.getPassword())) {
-                targetUser.setUserId(user.getUserId());
-                targetUser.setPassword(newPassword);
-                targetUser.setName(user.getName());
-                targetUser.setEmail(user.getEmail());
-                this.userRepository.save(targetUser);
-                isUpdate.set(true);
-            }
-        });
-        return isUpdate.get();
+    public boolean update(Long id, User user, String oldPassword) {
+        User targetUser = findById(id);
+        if (targetUser.getPassword().equals(oldPassword)) {
+            targetUser.update(user);
+            this.userRepository.save(targetUser);
+            return true;
+        }
+        return false;
     }
 
 }
