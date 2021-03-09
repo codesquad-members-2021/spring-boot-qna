97c9eae4861c42a450a772e5a6ff5bc364de3ca5
jeong-inho <derosatam76@gmail.com>
2021-03-09 11:03:49 +0900

    Delete unnecessary code

--- src/main/java/com/codessquad/qna/service/QuestionService.java
+++ src/main/java/com/codessquad/qna/service/QuestionService.java
@@ -1,7 +1,6 @@
 package com.codessquad.qna.service;
 
 import com.codessquad.qna.model.Question;
-import com.codessquad.qna.model.User;
 import com.codessquad.qna.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.IntStream;
 
 @Service
 public class QuestionService {
