aff98764eba417e0effe94989dd3326f2ac56425
Polynomeer <polynomeer@gmail.com>
2021-03-10 00:01:56 +0900

    refactor: Add UserRepository and UserService
    Added UserService to separate user service logic.
    Added UserRepository to separate user repository from UserController.

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.codessquad.qna.controller;
 
 import com.codessquad.qna.domain.User;
+import com.codessquad.qna.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,19 +12,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 @RequestMapping("users")
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    private final List<User> users = new ArrayList<>();
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/{userId}")
     public String profile(@PathVariable String userId, Model model) {
-        User user = findUserById(userId);
+        User user = userService.findUserById(userId);
         if (user == null) {
             return "redirect:/";
         }
@@ -32,7 +36,7 @@ public class UserController {
 
     @GetMapping()
     public String list(Model model) {
-        model.addAttribute("users", users);
+        model.addAttribute("users", userService.getUsers());
         return "/user/list";
     }
 
@@ -40,14 +44,13 @@ public class UserController {
     public String account(User user) {
         logger.info("user: {}", user);
 
-        user.setId(users.size() + 1);
-        users.add(user);
+        userService.addUser(user);
         return "redirect:/users";
     }
 
     @GetMapping("{userId}/info")
     public String form(@PathVariable String userId, Model model) {
-        User user = findUserById(userId);
+        User user = userService.findUserById(userId);
         if (user == null) {
             return "redirect:/users";
         }
@@ -57,7 +60,7 @@ public class UserController {
 
     @PostMapping("{userId}/info")
     public String update(@PathVariable String userId, User userToUpdate) {
-        User user = findUserById(userId);
+        User user = userService.findUserById(userId);
         if (user == null) {
             return "redirect:/users";
         }
@@ -65,12 +68,4 @@ public class UserController {
         return "redirect:/users";
     }
 
-    private User findUserById(String userId) {
-        for (User user : users) {
-            if (user.matchUserId(userId)) {
-                return user;
-            }
-        }
-        return null;
-    }
 }
