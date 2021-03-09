6432e8ee14a6773d32479a6bbbdeec509ecbd2e9
MJbae <gently.gogo@gmail.com>
2021-03-09 20:33:23 +0900

    Feat: UserRepository class를 CrudRepository를 상속하는 인터페이스로 구현

--- src/main/java/com/codessquad/qna/repository/UserRepository.java
+++ src/main/java/com/codessquad/qna/repository/UserRepository.java
@@ -1,36 +1,7 @@
 package com.codessquad.qna.repository;
 
 import com.codessquad.qna.domain.User;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Repository
-public class UserRepository {
-    private final List<User> users = new ArrayList<>();
-
-    public boolean save(User user) {
-        return users.add(user);
-    }
-
-    public List<User> getAll() {
-        return users;
-    }
-
-    public User getOne(String targetId) {
-        return users.stream()
-                .filter(user -> user.getUserId().equals(targetId))
-                .findAny()
-                .get();
-    }
-
-    public boolean isRedundant(String targetId) {
-        return users.stream()
-                .anyMatch(user -> user.getUserId().equals(targetId));
-    }
-
-    public boolean deleteOne(User user) {
-        return users.remove(user);
-    }
+public interface UserRepository extends CrudRepository<User, Long> {
 }
