2ad638600e9c6f4d827459751e54946cb4de42c6
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-03-09 18:26:30 +1300

    /user/list 에 사용자 번호(id) 부여
    - id로 원하는 사용자 탐색 가능해짐

--- src/main/java/com/codessquad/qna/domain/User.java
+++ src/main/java/com/codessquad/qna/domain/User.java
@@ -49,6 +49,14 @@ public class User {
         return email;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "User{" +
