ec87f0cd84d638840d627fd936516f8cd5c22bac
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-03-09 21:02:46 +0900

    DB연결을 위한 Repository 생성

--- src/main/java/com/codessquad/qna/domain/QnaRepository.java
+++ src/main/java/com/codessquad/qna/domain/QnaRepository.java
@@ -1,2 +1,6 @@
-package com.codessquad.qna.domain;public interface QnaRepository {
+package com.codessquad.qna.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QnaRepository extends JpaRepository<Qna, Long> {
 }
--- src/main/java/com/codessquad/qna/domain/UserRepository.java
+++ src/main/java/com/codessquad/qna/domain/UserRepository.java
@@ -1,2 +1,6 @@
-package com.codessquad.qna.domain;public interface UserRepository {
+package com.codessquad.qna.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
