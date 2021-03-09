5092824298c8f2473ec930fc3d158dc4acaeef09
jeong-inho <derosatam76@gmail.com>
2021-03-09 15:31:07 +0900

    Create login method in UserController class 로그인 기능을 위해 UserController class에 login method 생성, 세션 부여

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class UserController {
 
@@ -21,41 +23,60 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
+    @GetMapping("/user/form")
+    public String viewSingUp() {
+        logger.info("회원가입 페이지 요청");
+        return "user/form";
+    }
+
+    @PostMapping("/user/form")
+    public String signUp(User user) {
+        boolean result = this.userService.save(user);
+        logger.info("회원가입 요청");
+        return result ? "redirect:/user/list" : "redirect:/user/singUp";
+    }
+
+    @GetMapping("/user/login")
+    public String viewLogin() {
+        logger.info("로그인 페이지 요청");
+        return "user/login";
+    }
+
+    @PostMapping("/user/login")
+    public String login(String userId, String password, HttpSession session) {
+        boolean isLogin = this.userService.login(userId, password, session);
+        logger.info("로그인 요청");
+        return isLogin ? "redirect:/" : "redirect:/user/login_failed";
+    }
+
+    @GetMapping("/user/list")
     public String viewUserList(Model model) {
         model.addAttribute("users", this.userService.findAll());
         logger.info("유저 리스트 페이지 요청");
         return "user/list";
     }
 
-    @PostMapping("/users")
-    public String signUp(User user) {
-        boolean result = userService.save(user);
-        logger.info("회원가입 요청");
-        return result ? "redirect:/users" : "redirect:/user/form";
-    }
-
-    @GetMapping("/users/{id}")
-    public String viewProfile(@PathVariable("id") Long id, Model model) {
-        User user = this.userService.findById(id);
+    @GetMapping("/user/{userId}")
+    public String viewProfile(@PathVariable("userId") String userId, Model model) {
+        User user = this.userService.findByUserId(userId);
         model.addAttribute("user", user);
         logger.info("유저 프로필 페이지 요청");
-        return (user.getUserId() != null) ? "user/profile" : "redirect:/users";
+        return (user.getId() != null) ? "user/profile" : "redirect:/user/list";
     }
 
-    @GetMapping("/user/{id}/form")
+    @GetMapping("/user/{id}/update")
     public String viewUpdateProfile(@PathVariable("id") Long id, Model model) {
         User user = this.userService.findById(id);
         model.addAttribute("user", user);
         logger.info("유저 정보 수정 페이지 요청");
-        return (user.getUserId() != null) ? "user/updateForm" : "redirect:/users";
+        return (user.getId() != null) ? "user/updateForm" : "redirect:/user/list";
     }
 
     @PutMapping("/user/{id}/update")
     public String updateProfile(@PathVariable("id") Long id, User user, String oldPassword) {
-        boolean result = userService.update(id, user, oldPassword);
+        boolean result = this.userService.update(id, user, oldPassword);
         logger.info("유저 정보 수정 요청");
-        return result ? "redirect:/users" : "redirect:/user/" + id + "/form";
+        return result ? "redirect:/user/list" : "redirect:/user/" + id + "/update";
     }
 
 }
