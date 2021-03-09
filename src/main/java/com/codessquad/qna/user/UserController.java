7b6b21e999096dbb3b837599710e88e69fc2ca75
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-09 17:28:35 +0900

    Update to use put method in UserController

--- src/main/java/com/codessquad/qna/user/UserController.java
+++ src/main/java/com/codessquad/qna/user/UserController.java
@@ -4,10 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -41,7 +38,7 @@ public class UserController {
         return new ModelAndView("/user/updateForm", "user", user);
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public String updateUser(@PathVariable Long id, User user) {
         user.setId(id);
         userRepository.save(user);
