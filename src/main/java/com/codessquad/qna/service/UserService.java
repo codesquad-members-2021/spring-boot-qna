92f4f52a17f2d32a23c54b1f314d633d8e49eb72
jeong-inho <derosatam76@gmail.com>
2021-03-09 15:32:35 +0900

    Create login method in UserService class 로그인 기능을 위해 userId와 password를 비교하고 일치하면 세션을 부여하는 login 메소드 생성

--- src/main/java/com/codessquad/qna/service/UserService.java
+++ src/main/java/com/codessquad/qna/service/UserService.java
@@ -5,6 +5,7 @@ import com.codessquad.qna.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,9 +21,28 @@ public class UserService {
     }
 
     public boolean save(User user) {
-        Optional<User> duplicateUser = userRepository.findByUserId(user.getUserId());
-        if (!duplicateUser.isPresent()) {
+        User duplicateUser = findByUserId(user.getUserId());
+        if (duplicateUser.getId() == null) {
             this.userRepository.save(user);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean login(String userId, String password, HttpSession session) {
+        User targetUser = findByUserId(userId);
+        if (targetUser.getId() != null && targetUser.getPassword().equals(password)) {
+            session.setAttribute("user", targetUser);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean update(Long id, User user, String oldPassword) {
+        User targetUser = findById(id);
+        if (targetUser.getPassword().equals(oldPassword)) {
+            targetUser.update(user);
+            this.userRepository.save(targetUser);
             return true;
         }
         return false;
@@ -34,19 +54,14 @@ public class UserService {
         return userList;
     }
 
-    public User findById(Long id) {
-        Optional<User> user = this.userRepository.findById(id);
+    public User findByUserId(String userId) {
+        Optional<User> user = this.userRepository.findByUserId(userId);
         return user.orElseGet(User::new);
     }
 
-    public boolean update(Long id, User user, String oldPassword) {
-        User targetUser = findById(id);
-        if (targetUser.getPassword().equals(oldPassword)) {
-            targetUser.update(user);
-            this.userRepository.save(targetUser);
-            return true;
-        }
-        return false;
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseGet(User::new);
     }
 
 }
