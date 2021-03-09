d68076fcae15ea271ce18a53f6a0895f09468db8
tmdgusya <tmdgusya@naver.com>
2021-03-09 11:58:28 +0900

    feat : 게시물에 댓글 작성 기능 추가
    세션에 있는 유저를 기반으로 댓글을 작성하도록 했음.

---
+++ src/main/java/com/codessquad/qna/controller/CommentController.java
@@ -0,0 +1,46 @@
+package com.codessquad.qna.controller;
+
+import com.codessquad.qna.entity.Comment;
+import com.codessquad.qna.entity.Post;
+import com.codessquad.qna.entity.User;
+import com.codessquad.qna.service.CommentService;
+import com.codessquad.qna.service.PostService;
+import com.codessquad.qna.util.HttpSessionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class CommentController {
+
+    private Logger logger = LoggerFactory.getLogger(CommentController.class);
+
+    @Autowired
+    PostService postService;
+
+    @Autowired
+    CommentService commentService;
+
+    /**
+     * 세션에 로그인 되어 있어야만 작성 가능하며 로그인 되어 있지 않을시 NotExistLoggedUserInSession 발생
+     * 로그인 되어 있다면 정상적으로 댓글 작성 가능함
+     * @param id
+     * @param body
+     * @param httpSession
+     * @return redirect:/questions/%d
+     */
+    @PostMapping("/comments/{id}")
+    public String addCommend(@PathVariable Long id, String body, HttpSession httpSession) {
+        Post post = postService.getPost(id);
+        User commentAuthor = HttpSessionUtils.getUserFromSession(httpSession);
+        Comment comment = new Comment(post, commentAuthor.getUserId(), body);
+        commentService.addComment(comment);
+        logger.info("add Comment : " + comment.toString());
+        return String.format("redirect:/questions/%d", id);
+    }
+}
