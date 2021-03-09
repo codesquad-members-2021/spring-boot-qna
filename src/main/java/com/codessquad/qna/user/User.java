7b1188d8600a9ba85d5b8ab61e99093b80949f38
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-09 15:13:31 +0900

    Add constrains in User

--- src/main/java/com/codessquad/qna/user/User.java
+++ src/main/java/com/codessquad/qna/user/User.java
@@ -1,5 +1,6 @@
 package com.codessquad.qna.user;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -11,9 +12,13 @@ public class User {
     @Id
     @GeneratedValue
     private Long id;
+    @Column(nullable = false, length = 20, unique = true)
     private String userId;
+    @Column(nullable = false, length = 20)
     private String password;
+    @Column(nullable = false, length = 20)
     private String name;
+    @Column(nullable = false, length = 40, unique = true)
     private String email;
 
     public User() {
