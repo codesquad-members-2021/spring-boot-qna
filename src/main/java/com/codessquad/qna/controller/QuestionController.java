dce7b3aac984fc670c4592770c6e4eeca3f5385d
Polynomeer <polynomeer@gmail.com>
2021-03-10 00:13:20 +0900

    refactor: Add QuestionRepository and QuestionService
    Added QuestionService to separate question service logic.
    Added QuestionRepository to separate question repository from QuestionController.

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -1,6 +1,8 @@
 package com.codessquad.qna.controller;
 
 import com.codessquad.qna.domain.Question;
+import com.codessquad.qna.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,25 +13,29 @@ import java.util.List;
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
-    private final List<Question> questions = new ArrayList<>();
 
-    @GetMapping("")
+    private QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @GetMapping()
     public String list(Model model) {
-        model.addAttribute("questions", questions);
+        model.addAttribute("questions", questionService.getQuestions());
         return "qna/list";
     }
 
-    @PostMapping("")
-    public String create(Question question) {
-        question.setId(questions.size() + 1);
-        questions.add(question);
-
+    @PostMapping()
+    public String question(Question question) {
+        questionService.addQuestion(question);
         return "redirect:/";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") long id, Model model) {
-        model.addAttribute("question", questions.get((int) id - 1));
+        model.addAttribute("question", questionService.getQuestionById(id));
         return "/qna/show";
     }
 }
