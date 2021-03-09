8a2d0a42981fd203a6b1a4c9390a657cbad2b4e4
Jihye Woo <woojihye2339@gmail.com>
2021-03-09 22:35:57 +0900

    refactor : Entity 클래스 setter 제거
      > User 클래스 setter 제거
       - 생성자 구현

--- src/main/java/com/codessquad/qna/web/domain/user/User.java
+++ src/main/java/com/codessquad/qna/web/domain/user/User.java
@@ -16,32 +16,24 @@ public class User {
     @Column(nullable=false, length=20)
     private String userId;
 
+    @Column(nullable=false)
     private String password;
 
+    @Column(nullable=false)
     private String name;
 
+    @Column(nullable=false)
     private String email;
 
-
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUserId(String userId) {
+    public User(String userId, String password, String name, String email) {
         this.userId = userId;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
-    }
-
-    public void setName(String name) {
         this.name = name;
+        this.email = email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    protected User(){
+
     }
 
     public Long getId() {
