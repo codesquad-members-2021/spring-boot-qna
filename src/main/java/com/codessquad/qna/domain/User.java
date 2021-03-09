30fe6310adeb9ccc2cb6093e5ba8ed544323780c
noel <68936@naver.com>
2021-03-09 20:37:07 +0900

    [feat] User 클래스를 DB 테이블에 매핑
    - User 클래스와 DB의 테이블에 매핑한다.
    - DB 테이블은 별도로 생성할 필요없이 자동으로 생성된다.

--- src/main/java/com/codessquad/qna/domain/User.java
+++ src/main/java/com/codessquad/qna/domain/User.java
@@ -1,11 +1,21 @@
 package com.codessquad.qna.domain;
 
+import javax.persistence.*;
 import java.util.Objects;
 
+@Entity //m 데이타베이스의 테이블과 일대일로 매칭되는 객체 단위
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //m 키 생성 전략 : 기본 키 생성을 데이터베이스에 위임, 새로운 레코드가 생성이 될때마다 마지막 PK 값에서 자동으로 +1
+    private Long primaryKey;
+
+    @Column(nullable=false, length=20)
     private String userId;
+    @Column(nullable=false, length=20)
     private String password;
+    @Column(nullable=false, length=20)
     private String name;
+    @Column(nullable=false, length=20)
     private String email;
 
     public String getUserId() {
