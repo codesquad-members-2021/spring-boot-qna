9a9ac8aa09a24eecc552cc9699b79125f8d53194
MJbae <gently.gogo@gmail.com>
2021-03-10 04:41:48 +0900

    Refactor: CrudRepository 인터페이스 상속하여 QuestionRepository 수정

--- src/main/java/com/codessquad/qna/repository/QuestionRepository.java
+++ src/main/java/com/codessquad/qna/repository/QuestionRepository.java
@@ -1,29 +1,9 @@
 package com.codessquad.qna.repository;
 
 import com.codessquad.qna.domain.Question;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Repository
-public class QuestionRepository {
-    private final List<Question> questions = new ArrayList<>();
-
-    public boolean save(Question question) {
-        question.setQuestionId(questions.size() + 1);
-
-        return questions.add(question);
-    }
-
-    public List<Question> getAll() {
-        return questions;
-    }
-
-    public Question getOne(int targetId) {
-        return questions.stream()
-                .filter(question -> question.getQuestionId() == targetId)
-                .findAny()
-                .get();
-    }
+public interface QuestionRepository extends CrudRepository<Question, Long> {
 }
