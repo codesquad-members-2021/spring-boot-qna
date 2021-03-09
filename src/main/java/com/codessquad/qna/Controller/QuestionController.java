326cc92ed7770890c3a25f21583ab2cfec143ee2
dong <ultraraptor@naver.com>
2021-03-09 19:33:40 +0900

    적절한 질문변수명 변경 qList가 아닌 questionList

--- src/main/java/com/codessquad/qna/Controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/Controller/QuestionController.java
@@ -1,10 +1,13 @@
 package com.codessquad.qna.Controller;
 
 import com.codessquad.qna.domain.Question;
+import com.codessquad.qna.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -14,19 +17,47 @@ import java.util.List;
 public class QuestionController {
 
     Logger logger = LoggerFactory.getLogger(UserController.class);
-    final private List<Question> qList = new ArrayList<>();
+    final private List<Question> questionList = new ArrayList<>();
 
-
+    //======================================
+    //1단계 질문하기
     @GetMapping("/qna/form.html")
     public String questionList() {
         logger.info("askQuestion");
         return "qna/form";
     }
 
-    @PostMapping("/questions")
-    public String askQuestion() {
-        return "";
+    @PostMapping("/qna/questions")
+    public String askQuestion(Question question) {
+        //qList.add();
+        question.setIndex(questionList.size()+1);
+        questionList.add(question);
+
+        return "redirect:/qna/list";
     }
+
+    @GetMapping("/qna/list")
+    public String showQuestionList(Model model) {
+        model.addAttribute("questionList",questionList);
+        return "qna/list";
+    }
+    /*
+    @GetMapping("/qna/show")
+    public String list(Model model) {
+        logger.info("questionList");
+
+    }*/
+
+    @GetMapping("/qna/{index}")
+    public String showProfile(@PathVariable int index, Model model) {
+        Question currentQuestion = questionList.get(index-1);
+        model.addAttribute("question",currentQuestion);
+        logger.info("update Question : " + currentQuestion.toString());
+        return "/qna/show";
+    }
+
+    //1단계 질문하기
+    //======================================
 
 
 }
