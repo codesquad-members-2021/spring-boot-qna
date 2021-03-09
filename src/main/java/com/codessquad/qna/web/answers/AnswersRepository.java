1cb2d03c03602ce6800ed8a03fbb5e01b8842a5f
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-09 20:37:32 +0900

    더 이상 쓰지 않는 메서드를 지워라

--- src/main/java/com/codessquad/qna/web/answers/AnswersRepository.java
+++ src/main/java/com/codessquad/qna/web/answers/AnswersRepository.java
@@ -2,8 +2,5 @@ package com.codessquad.qna.web.answers;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface AnswersRepository extends CrudRepository<Answer, Long> {
-    List findByQuestionId(long questionId);
 }
