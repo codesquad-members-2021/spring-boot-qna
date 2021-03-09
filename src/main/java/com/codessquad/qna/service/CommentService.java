6984e4cdd2129fae7aad433f46579312ad64b712
tmdgusya <tmdgusya@naver.com>
2021-03-09 12:44:59 +0900

    feat : @Transaction 추가

--- src/main/java/com/codessquad/qna/service/CommentService.java
+++ src/main/java/com/codessquad/qna/service/CommentService.java
@@ -5,7 +5,10 @@ import com.codessquad.qna.repository.CommentRepostiory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class CommentService {
 
     @Autowired
