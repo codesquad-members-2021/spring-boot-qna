c90421bc3eaad025fd4f13223f534b51a5f35b37
MJbae <gently.gogo@gmail.com>
2021-03-10 03:44:21 +0900

    Refactor: 회원정보 수정 기능에 대한 요청방식 변경(Post->Put)
    1. users/form.html에서 요청방식을 post -> put으로 변경하고 hidden type의 input 태그 추가
    2. UserController에서 맵핑 방식 변경(postmapping -> putmapping)
    3. application.properties에서 hiddenmethod 사용을 가능케 설정을 추가

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -7,10 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -83,7 +80,7 @@ public class UserController {
         return "/users/update";
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public String updateUser(@PathVariable long id, User referenceUser) {
         User presentUser = userRepository.findById(id).get();
 
