21a93c4ee4a93b70fbf5905c092c1b3af158018d
rla36 <bhmarsd@naver.com>
2021-03-09 11:27:09 +0900

    WebMvcConfigurer 구현하는 클래스 추가해 url과 html 쉽게 연결
    미션에서 명시한 webMvcCongifurerAdapter가 deprecated되어
    webMvcConfigurer을 구현하도록 했습니다.

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -1,5 +1,6 @@
-package com.codessquad.qna;
+package com.codessquad.qna.web;
 
+import com.codessquad.qna.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
