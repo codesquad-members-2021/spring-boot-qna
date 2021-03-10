0185576f449d6efc8475f0ba19b0e4821024d01f
malaheaven <yunhg95@gmail.com>
2021-03-10 11:59:53 +0900

    feat: User에 대한 CRUD Repository 인터페이스 생성

---
+++ src/main/java/com/codessquad/qna/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.codessquad.qna.repository;
+
+import com.codessquad.qna.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
