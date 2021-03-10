c8e49c88f3b23e1a587f15a48d67d95395983b52
gisungchoi <chlrltjd159@naver.com>
2021-03-10 15:29:26 +0900

    feat : AnswerService class 정의
    Answer의 비즈니스 로직을 정의했다.

---
+++ src/main/java/com/codessquad/qna/service/AnswerService.java
@@ -0,0 +1,26 @@
+package com.codessquad.qna.service;
+
+import com.codessquad.qna.domain.Answer;
+import com.codessquad.qna.domain.Question;
+import com.codessquad.qna.domain.User;
+import com.codessquad.qna.repository.AnswerRepository;
+import com.codessquad.qna.valid.UserValidation;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+    private final QuestionService questionService;
+
+    public AnswerService(AnswerRepository answerRepository, QuestionService questionService) {
+        this.answerRepository = answerRepository;
+        this.questionService = questionService;
+    }
+
+    public void write(User writer, String contents, Long questionId) {
+        UserValidation.validUserInfo(writer);
+        Question findQuestion = questionService.findById(questionId);
+        Answer answer = new Answer(writer, findQuestion, contents);
+    }
+}
