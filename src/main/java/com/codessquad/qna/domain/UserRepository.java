84193c0d4fef34e4e7acfef49050c190e83d695d
95degree <95degree@naver.com>
2021-03-10 16:10:59 +0900

    UserRepository 인터페이스에 상속 추가
    Repository의 기능을 구현하기 위해 JpaRepository를 상속받도록 추가했습니다.

--- src/main/java/com/codessquad/qna/domain/UserRepository.java
+++ src/main/java/com/codessquad/qna/domain/UserRepository.java
@@ -1,4 +1,6 @@
 package com.codessquad.qna.domain;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
