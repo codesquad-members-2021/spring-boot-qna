3081e6d50f501b453e0011a4f36ecaa0fa361218
janeljs <jisunlim818@gmail.com>
2021-03-11 01:11:17 +0900

    Add addAnswer method in scripts.js

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,1 +1,29 @@
+$(".answer-write input[type=submit]").click(addAnswer);
 
+function addAnswer(e) {
+    console.log("click me");
+    e.preventDefault();
+
+    var queryString = $(".answer-write").serialize();
+    console.log("query : " + queryString);
+
+    var url = $(".answer-write").attr("action");
+
+    console.log("url : " + url);
+    $.ajax({
+        type: 'post',
+        url: url,
+        data: queryString,
+        dataType: 'json',
+        error: onError,
+        success: onSuccess
+    });
+}
+
+function onError() {
+
+}
+
+function onSuccess() {
+
+}
