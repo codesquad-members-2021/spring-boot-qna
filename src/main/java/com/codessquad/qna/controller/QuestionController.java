32937dc6e125e28764c43a243a713a777a1192ac
janeljs <jisunlim818@gmail.com>
2021-03-10 23:12:09 +0900

    Remove unnecessary import statements in QuestionController

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -3,8 +3,6 @@ package com.codessquad.qna.controller;
 import com.codessquad.qna.domain.*;
 import com.codessquad.qna.exception.*;
 import com.codessquad.qna.service.QuestionService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
