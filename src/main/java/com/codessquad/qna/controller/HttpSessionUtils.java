47e14927912025935d0a36c94527c6850eaaf54a
janeljs <jisunlim818@gmail.com>
2021-03-11 01:39:11 +0900

    Add a private constructor inn HttpSessionUtils

--- src/main/java/com/codessquad/qna/controller/HttpSessionUtils.java
+++ src/main/java/com/codessquad/qna/controller/HttpSessionUtils.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpSession;
 public class HttpSessionUtils {
     public static final String USER_SESSION_KEY = "sessionUser";
 
+    private HttpSessionUtils() {
+    }
+
     public static boolean isLoginUser(HttpSession session) {
         return getSessionUser(session) != null;
     }
