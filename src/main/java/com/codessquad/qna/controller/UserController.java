c776a69e972a2e11f42645fe29a4fe48da40261c
malaheaven <yunhg95@gmail.com>
2021-03-10 19:04:45 +0900

    fix: 쓰지않는 import 삭제

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -8,10 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 @RequestMapping("/users")
 public class UserController {
