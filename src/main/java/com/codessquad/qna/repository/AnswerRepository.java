3d5a256edd1c07f2762196c68d8030640bcbde9e
jeong-inho <derosatam76@gmail.com>
2021-03-10 21:33:08 +0900

    Create AnswerRepository class Answer 엔티티를 관리하기 위해 AnswerRepository 인터페이스 생성

---
+++ src/main/java/com/codessquad/qna/repository/AnswerRepository.java
@@ -0,0 +1,13 @@
+package com.codessquad.qna.repository;
+
+import com.codessquad.qna.model.Answer;
+import com.codessquad.qna.model.Question;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface AnswerRepository extends CrudRepository<Answer, Long> {
+
+    List<Answer> findAllByQuestion(Question questionId);
+
+}
