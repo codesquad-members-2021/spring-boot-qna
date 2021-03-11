83a7cf093b8d2bd4340a72c2a67e9e218ff98fc6
tmdgusya <tmdgusya@naver.com>
2021-03-11 16:04:30 +0900

    feat : 게시글 삭제시 댓글 삭제

--- src/main/java/com/codessquad/qna/repository/CommentRepostiory.java
+++ src/main/java/com/codessquad/qna/repository/CommentRepostiory.java
@@ -1,6 +1,7 @@
 package com.codessquad.qna.repository;
 
 import com.codessquad.qna.entity.Comment;
+import com.codessquad.qna.entity.Post;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,7 @@ import java.util.List;
 public interface CommentRepostiory extends CrudRepository<Comment, Long> {
 
     List<Comment> findAll();
+
+    void deleteAllByPost(Post post);
 
 }
--- src/main/java/com/codessquad/qna/service/PostService.java
+++ src/main/java/com/codessquad/qna/service/PostService.java
@@ -2,6 +2,7 @@ package com.codessquad.qna.service;
 
 import com.codessquad.qna.entity.Post;
 import com.codessquad.qna.exception.CanNotFindPostException;
+import com.codessquad.qna.repository.CommentRepostiory;
 import com.codessquad.qna.repository.PostRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +14,11 @@ import java.util.*;
 public class PostService {
 
     private final PostRepository postRepository;
+    private final CommentRepostiory commentRepostiory;
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, CommentRepostiory commentRepostiory) {
         this.postRepository = postRepository;
+        this.commentRepostiory = commentRepostiory;
     }
 
     public void addPost(Post post) {
@@ -36,6 +39,7 @@ public class PostService {
     }
 
     public void deletePost(Post post) {
+        commentRepostiory.deleteAllByPost(post);
         postRepository.delete(post);
     }
 
