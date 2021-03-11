f01865215abcffb41c79a0630417a8c2221577f3
tmdgusya <tmdgusya@naver.com>
2021-03-11 18:53:41 +0900

    feat : Error 처리 개선
    - 401 은 유저가 권한에 엇나갈 행동을 할시 401 상태 Code 를 던지고 알맞은 행동을 할 수 있는 url 로 리다이렉션 시켜주었습니다. 찾을 수 없는 페이지에 도달했을때는 404 로 처리해 주었습니다.

--- src/main/java/com/codessquad/qna/controller/ControllerExceptionHandler.java
+++ src/main/java/com/codessquad/qna/controller/ControllerExceptionHandler.java
@@ -1,13 +1,17 @@
 package com.codessquad.qna.controller;
 
 import com.codessquad.qna.exception.CanNotFindPostException;
-import com.codessquad.qna.exception.UserNotFoundException;
 import com.codessquad.qna.exception.NotExistLoggedUserInSessionException;
+import com.codessquad.qna.exception.UserNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -15,60 +19,49 @@ public class ControllerExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
 
     /**
-     * 잘못된 입력값이 들어왔을때 핸들링 해주는 메소드
-     * 후에 공부해서 에러페이지를 만들어서 처리해야할듯함
-     * @param e
-     * @return
-     */
-    @ExceptionHandler(IllegalArgumentException.class)
-    public String handleIllegalArgumentException(IllegalArgumentException e, Model model) {
-        return handleException(e, model);
-    }
-
-    /**
      * 유저를 가져오지 못할시 처리할 로직 생성
-     * 일단 ErrorPage 로 ErrorMessage 리턴해줌
-     * @return
+     * 없는 페이지에 대한 요청이 므로 404 페이지를 리턴하도록 해줌
+     * @return 404 page
      */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException.class)
     public String handleCanNotFindUserException(UserNotFoundException e, Model model) {
-        return handleException(e, model);
+        return "error/404";
     }
 
     /**
-     * Post 를 찾지 못할시 해당 에러메세지를 에러페이지로 리턴한다.
+     * 없는 페이지에 대한 요청이 므로 404 페이지를 리턴하도록 해줌
      * @param e
      * @param model
-     * @return handleCanNotFindPostException message to error page
+     * @return 404 page
      */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(CanNotFindPostException.class)
     public String handleCanNotFindPostException(CanNotFindPostException e, Model model) {
-        return handleException(e, model);
+        return "error/404";
     }
 
     /**
-     * 세션에 로그인 된 유저가 없을시 발생하는 에러
+     * 세션에 로그인 된 유저가 없는데 유저 권한의 활동을 하려할 시 발생하는 에러
      * 로그인 창으로 redirect
+     * @return 401 Status code
      */
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(NotExistLoggedUserInSessionException.class)
-    public String handleNotExistLoggedUserInSession(NotExistLoggedUserInSessionException e) {
+    public String handleNotExistLoggedUserInSession(NotExistLoggedUserInSessionException e, HttpServletResponse httpServletResponse) {
         logger.error(e.getMessage());
-        return "redirect:/users/loginForm";
+        return "user/login";
     }
 
     /**
      * 해당 권한이 있는 유저가 아닐시 login 창으로 redirect 시킨다.
+     * @return 401 Status code
      */
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(IllegalAccessException.class)
     public String handleIllegalAccessException(IllegalAccessException e, Model model) {
         logger.error(e.getMessage());
-        return "redirect:/users/loginForm";
-    }
-
-    private String handleException(Exception e, Model model) {
-        logger.error(e.getMessage());
-        model.addAttribute("exception", e);
-        return "error";
+        return "user/login";
     }
 
 }
--- src/main/java/com/codessquad/qna/exception/CanNotFindPostException.java
+++ src/main/java/com/codessquad/qna/exception/CanNotFindPostException.java
@@ -1,5 +1,9 @@
 package com.codessquad.qna.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CanNotFindPostException extends RuntimeException{
 
     public CanNotFindPostException() {
