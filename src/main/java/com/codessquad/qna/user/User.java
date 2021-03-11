9bd8d1dc7eb23825bbaab16772907e45d6b173e5
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-12 02:48:34 +0900

    Formatting fields in User

--- src/main/java/com/codessquad/qna/user/User.java
+++ src/main/java/com/codessquad/qna/user/User.java
@@ -9,12 +9,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false, length = 20, unique = true)
     private String userId;
+
     @Column(nullable = false, length = 20)
     private String password;
+
     @Column(nullable = false, length = 20)
     private String name;
+    
     @Column(nullable = false, length = 40, unique = true)
     private String email;
 
