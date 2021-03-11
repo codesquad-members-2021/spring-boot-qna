109ecebac21cc86048d804ed15973a861e161294
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-11 17:05:21 +0900

    댓글의 컨텐츠 크기를 늘려라
    - 기존에는 20으로 너무 작았음.
    - 직접 써보니까 400자 정도면 적당한 것 같아서, length를 400으로 늘림

--- src/main/java/com/codessquad/qna/web/answers/Answer.java
+++ src/main/java/com/codessquad/qna/web/answers/Answer.java
@@ -20,7 +20,7 @@ public class Answer {
     @JoinColumn(foreignKey = @ForeignKey(name = "fk_answer_to_question"))
     private Question question;
 
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false, length = 400)
     private String contents;
 
     private LocalDateTime reportingDate;
