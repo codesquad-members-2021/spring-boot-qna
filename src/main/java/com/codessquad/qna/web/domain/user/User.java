467296572854605863e51122130c09cdaefaf4e9
Jihye Woo <woojihye2339@gmail.com>
2021-03-11 22:15:18 +0900

    feat : User 클래스 userId 필드 unique 속성 추가
      > userId
      - unique = true 속성 추가

--- src/main/java/com/codessquad/qna/web/domain/user/User.java
+++ src/main/java/com/codessquad/qna/web/domain/user/User.java
@@ -13,7 +13,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable=false, length=20)
+    @Column(nullable=false, unique = true, length=20)
     private String userId;
 
     @Column(nullable=false)
