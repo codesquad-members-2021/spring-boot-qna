737b04a223a13fa3baac70df4cd0749b05a1bdc2
tmdgusya <tmdgusya@naver.com>
2021-03-11 16:04:44 +0900

    feat : 댓글 등록 로직 서비스로 이전

--- src/main/java/com/codessquad/qna/service/CommentService.java
+++ src/main/java/com/codessquad/qna/service/CommentService.java
@@ -1,6 +1,8 @@
 package com.codessquad.qna.service;
 
 import com.codessquad.qna.entity.Comment;
+import com.codessquad.qna.entity.Post;
+import com.codessquad.qna.entity.User;
 import com.codessquad.qna.repository.CommentRepostiory;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +13,15 @@ import javax.transaction.Transactional;
 public class CommentService {
 
     private final CommentRepostiory commentRepostiory;
+    private final PostService postService;
 
-    public CommentService(CommentRepostiory commentRepostiory) {
+    public CommentService(CommentRepostiory commentRepostiory, PostService postService) {
         this.commentRepostiory = commentRepostiory;
+        this.postService = postService;
     }
 
-    public void addComment(Comment comment) {
+    public void addComment(Long postId, String userId, String body) {
+        Comment comment = new Comment(postService.getPost(postId), userId, body);
         commentRepostiory.save(comment);
     }
 
