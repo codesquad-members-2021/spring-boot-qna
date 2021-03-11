6f9500f9f3a56476cc5fd6f4927060c180d377b5
MJbae <gently.gogo@gmail.com>
2021-03-11 21:08:15 +0900

    Add: 회원 중복 가입 방지 기능 추가
    1. UserRepository에 userId 기준으로 동일 회원 찾는 메소드 정의
    2. UserService::join 내부에 isExist 메소드 추가하여 중복 확인 시 UserExistExcption 발생

--- src/main/java/com/codessquad/qna/repository/UserRepository.java
+++ src/main/java/com/codessquad/qna/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.codessquad.qna.repository;
 import com.codessquad.qna.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByUserId(String userId);
 }
