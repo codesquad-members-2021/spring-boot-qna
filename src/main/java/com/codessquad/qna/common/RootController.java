34f78a3bf6cc5940fb8da56709d02afc94dcb6b4
고정완 (Can) <jw.gho@ecubelabs.com>
2021-03-11 10:58:35 +0900

    [refactor] Rename IndexController to RootController

--- src/main/java/com/codessquad/qna/common/RootController.java
+++ src/main/java/com/codessquad/qna/common/RootController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 // FIXME: MvcConfig 에서 index 의 리다이렉션을 유도할 수는 없을까?
 @Controller
-public class IndexController {
+public class RootController {
     @GetMapping("/")
     public String index_redirect() {
         return "redirect:/questions";
