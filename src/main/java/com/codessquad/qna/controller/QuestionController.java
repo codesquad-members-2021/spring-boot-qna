f83e608b4a9f1b94f4726dbb70e07d784501982f
95degree <95degree@naver.com>
2021-03-12 11:23:26 +0900

    @Autowired 추가
    QuestionController에서 db를 사용하기 위해 QuestionRepository에 @Autowired를 추가 했습니다.

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -1,6 +1,8 @@
 package com.codessquad.qna.controller;
 
 import com.codessquad.qna.domain.Question;
+import com.codessquad.qna.domain.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,9 @@ import java.util.List;
 @Controller
 public class QuestionController {
     private List<Question> questions = new ArrayList<>();
+
+    @Autowired
+    private QuestionRepository questionRepository;
 
     @PostMapping("/questions")
     public String createQuestion(Question question) {
