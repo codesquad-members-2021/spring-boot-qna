21016aad98962bdcdfe8c32bfa7b44bd6add1909
95degree <95degree@naver.com>
2021-03-12 16:06:23 +0900

    User 생성자 추가
    User 생성자를 추가하고 setter를 제거했습니다.

--- src/main/java/com/codessquad/qna/domain/User.java
+++ src/main/java/com/codessquad/qna/domain/User.java
@@ -15,36 +15,31 @@ public class User {
     private String name;
     private String email;
 
-    public Long getId() {
-        return id;
+    protected User() {
+
     }
 
-    public void setUserId(String userId) {
+    public User(String userId, String password, String name, String email) {
         this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getUserId() {
         return userId;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getPassword() {
         return password;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getEmail() {
