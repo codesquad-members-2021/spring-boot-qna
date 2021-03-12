0564539acd116a177c4161fd57bbf9bb070a0067
tmdgusya <tmdgusya@naver.com>
2021-03-12 15:06:01 +0900

    fix : Repository 제거

--- src/main/java/com/codessquad/qna/repository/CommentRepostiory.java
+++ src/main/java/com/codessquad/qna/repository/CommentRepostiory.java
@@ -3,11 +3,9 @@ package com.codessquad.qna.repository;
 import com.codessquad.qna.entity.Comment;
 import com.codessquad.qna.entity.Post;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface CommentRepostiory extends CrudRepository<Comment, Long> {
 
     List<Comment> findAll();
--- src/main/java/com/codessquad/qna/repository/PostRepository.java
+++ src/main/java/com/codessquad/qna/repository/PostRepository.java
@@ -2,11 +2,9 @@ package com.codessquad.qna.repository;
 
 import com.codessquad.qna.entity.Post;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface PostRepository extends CrudRepository<Post, Long>  {
 
     List<Post> findAll();
