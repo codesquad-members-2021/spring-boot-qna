f2cbce6231c72434e8097d2e74ce23d76d1a185a
rla36 <bhmarsd@naver.com>
2021-03-12 14:05:31 +0900

    회원정보 수정 put으로 수정
    기존에 post였던 method를 put으로 수정했습니다.

--- src/main/java/com/codessquad/qna/web/controllers/UserController.java
+++ src/main/java/com/codessquad/qna/web/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -62,7 +63,7 @@ public class UserController {
         return user;
     }
 
-    @PostMapping("/users/{id}")
+    @PutMapping("/users/{id}")
     public String updateUser(@PathVariable Long id, User newInfoUser) {
         User user = findUserById(id);
         user.update(newInfoUser);
