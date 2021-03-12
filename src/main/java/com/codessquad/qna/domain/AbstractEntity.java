6b81f01101327324cff7f1f6e1c20be24780f633
janeljs <jisunlim818@gmail.com>
2021-03-12 19:27:35 +0900

    Add AbstractEntity class to reduce duplicate code
    - 도메인에 있는 객체들이 상속할 AbstractEntity 클래스 추가
    - 게시글 또는 댓글이 생성 또는 수정된 날짜를 관리하기 위해 `@EnableJpaAuditing` 주석 추가

---
+++ src/main/java/com/codessquad/qna/domain/AbstractEntity.java
@@ -0,0 +1,41 @@
+package com.codessquad.qna.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@MappedSuperclass
+public class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
