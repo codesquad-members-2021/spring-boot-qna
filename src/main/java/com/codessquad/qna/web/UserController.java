cfbd5ff2d88d256d3c7687403688c235e602e7d8
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-03-12 17:20:04 +0900

    비밀번호를 확인하여 원래 설정했던 비밀번호와 이전 비밀번호란의 비밀번호가 같지 않으면 변경하지 않도록 적용

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -2,12 +2,14 @@ package com.codessquad.qna.web;
 
 import com.codessquad.qna.domain.User;
 import com.codessquad.qna.domain.UserRepository;
+import com.codessquad.qna.exception.NoUserException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 
 @Controller
 public class UserController {
@@ -16,6 +18,8 @@ public class UserController {
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    private final Logger logger = LoggerFactory.getLogger(QuestionController.class);
 
     @PostMapping("/")
     public String create(User user) {
@@ -34,19 +38,22 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public String getOneUserProfile(@PathVariable long id, Model model) {
-        model.addAttribute("user", userRepository.findById(id).orElseThrow(NullPointerException::new));
+        model.addAttribute("user", userRepository.findById(id).orElseThrow(NoUserException::new));
         return "user/profile";
     }
 
     @GetMapping("/users/{id}/form")
     public String editUserInfo(@PathVariable long id, Model model) {
-        model.addAttribute("user", userRepository.findById(id).orElseThrow(NullPointerException::new));
+        model.addAttribute("user", userRepository.findById(id).orElseThrow(NoUserException::new));
         return "user/updateForm";
     }
 
     @PostMapping("/users/{id}/form")
     public String update(@PathVariable long id, User updateUser) {
-        User user = userRepository.findById(id).orElseThrow(NullPointerException::new);
+        User user = userRepository.findById(id).orElseThrow(NoUserException::new);
+        if(user.checkPassword(updateUser.getPassword())){
+            logger.info(updateUser.getPassword() + ": 올바르지 않은 패스워드입니다.비밀번호가 유지됩니다.");
+        }
         user.update(updateUser);
 
         userRepository.save(user);
