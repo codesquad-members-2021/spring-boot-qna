a725489ec885697e5a011a6b4c8a3675eee2990f
janeljs <jisunlim818@gmail.com>
2021-03-12 20:23:31 +0900

    Modify AbstractEntity
    - 날짜 포매팅 관련 메서드 추가

--- src/main/java/com/codessquad/qna/domain/AbstractEntity.java
+++ src/main/java/com/codessquad/qna/domain/AbstractEntity.java
@@ -2,16 +2,18 @@ package com.codessquad.qna.domain;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class AbstractEntity {
+    public static final String DATETIME_FORMAT = "yyyy.MM.dd HH:mm";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,6 +26,21 @@ public class AbstractEntity {
 
     public Long getId() {
         return id;
+    }
+
+    public String getFormattedCreatedDate() {
+        return getFormattedDate(createdDate);
+    }
+
+    public String getFormattedModifiedDate() {
+        return getFormattedDate(modifiedDate);
+    }
+
+    private String getFormattedDate(LocalDateTime date) {
+        if (date == null) {
+            return "";
+        }
+        return date.format(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
     }
 
     @Override
