d3e1cbe5f5018145ba59a2b0b5a7e0e23d62830a
malaheaven <yunhg95@gmail.com>
2021-03-12 11:39:44 +0900

    fix: 정렬

--- src/main/java/com/codessquad/qna/repository/QuestionRepository.java
+++ src/main/java/com/codessquad/qna/repository/QuestionRepository.java
@@ -1,2 +1,7 @@
-package com.codessquad.qna.repository;public interface QuestionRepository {
+package com.codessquad.qna.repository;
+
+import com.codessquad.qna.domain.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
--- src/main/java/com/codessquad/qna/repository/UserRepository.java
+++ src/main/java/com/codessquad/qna/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.codessquad.qna.repository;
 import com.codessquad.qna.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
