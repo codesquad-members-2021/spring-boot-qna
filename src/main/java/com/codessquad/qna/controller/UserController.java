9b44db0ff49fe729737589d347746ddd0c692522
95degree <95degree@naver.com>
2021-03-12 14:40:09 +0900

    @PutMapping으로 변경
    같은 위치의 리소스 결과를 보내주기 위해 updateUser()를 @PutMapping으로 변경했습니다.

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -7,10 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,7 +50,7 @@ public class UserController {
         }
     }
 
-    @PostMapping("{id}/update")
+    @PutMapping("{id}/update")
     public String updateUser(@PathVariable Long id, User updateUser) {
         try {
             User targetUser = userRepository.findById(id).get();
