1b6ccc63b8ea531d20dd98ff912ac949d9f2b64b
95degree <95degree@naver.com>
2021-03-12 11:17:42 +0900

    상속 설정
    질문DB를 관리하기 위해 JpaRepository를 상속 받도록 했습니다.

--- src/main/java/com/codessquad/qna/domain/QuestionRepository.java
+++ src/main/java/com/codessquad/qna/domain/QuestionRepository.java
@@ -1,4 +1,6 @@
 package com.codessquad.qna.domain;
 
-public interface QuestionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question,Long> {
 }
