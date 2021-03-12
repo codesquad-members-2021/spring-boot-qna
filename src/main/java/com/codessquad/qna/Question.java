0eb91b7ec050cc95ded57105d44740149fe94907
bibi <bibi6666667@kakao.com>
2021-03-12 14:36:40 +0900

    질문 상세보기 구현
    Question
    - index 인스턴스변수와 getter, setter 추가 및 toString() 수정
    index.html
    - 질문 제목에 URL 설정 (`/questions/{{index}}`)
    QuestionController
    - question 추가할 때 index값 셋팅
    - 질문을 보여주는 showQuestion() 추가
    show.html
    - model에 담겨져 온 question의 제목, 글쓴이, 내용 배치

--- src/main/java/com/codessquad/qna/Question.java
+++ src/main/java/com/codessquad/qna/Question.java
@@ -1,9 +1,18 @@
 package com.codessquad.qna;
 
 public class Question {
+    private int index;
     private String writer;
     private String title;
     private String contents;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
 
     public String getWriter() {
         return writer;
@@ -32,7 +41,8 @@ public class Question {
     @Override
     public String toString() {
         return "Question{" +
-                "writer='" + writer + '\'' +
+                "index=" + index +
+                ", writer='" + writer + '\'' +
                 ", title='" + title + '\'' +
                 ", contents='" + contents + '\'' +
                 '}';
--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -4,6 +4,7 @@ import com.codessquad.qna.Question;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -21,6 +22,7 @@ public class QuestionController {
 
     @PostMapping("/questions")
     public String createQuestion(Question question) {
+        question.setIndex(questions.size() + 1);
         System.out.println(question);
         questions.add(question);
         return "redirect:/";
@@ -28,5 +30,12 @@ public class QuestionController {
 
     public static List<Question> getQuestions() {
         return questions;
+    }
+
+    @GetMapping("/questions/{index}")
+    public String showQuestion(@PathVariable("index") int index, Model model) {
+        Question question = questions.get(index - 1);
+        model.addAttribute("question", question);
+        return "show";
     }
 }
