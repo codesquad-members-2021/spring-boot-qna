1a50b6ac60ba0b0b213abbe1f48a9b8f79da83b7
tmdgusya <tmdgusya@naver.com>
2021-03-12 15:05:14 +0900

    fix : 안쓰는 import 삭제

--- src/main/java/com/codessquad/qna/repository/PostRepository.java
+++ src/main/java/com/codessquad/qna/repository/PostRepository.java
@@ -1,23 +1,13 @@
 package com.codessquad.qna.repository;
 
 import com.codessquad.qna.entity.Post;
-import com.codessquad.qna.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-@Transactional
 public interface PostRepository extends CrudRepository<Post, Long>  {
 
     List<Post> findAll();
-
 }
