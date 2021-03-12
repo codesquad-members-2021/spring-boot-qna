340677e5384a28e8e1d700f8699f3cdffb972c9f
JoonKi-Kim <joonki402@gmail.com>
2021-03-12 12:21:06 +0900

    값을 입력하지 않았을 때 뜨는 alert 창 구현
    js 사용하여 alert 창 구현

---
+++ src/main/resources/static/js/validInput.js
@@ -0,0 +1,12 @@
+function checkInput() {
+    getInput = document.question.getElementsByTagName("input");
+
+    for (var i = 0; i < getInput.length; i++) {
+        if (getInput[i].value.trim() == "") {
+            let getName = getInput[i].getAttribute("id");
+            alert(getName + "를 입력하세요")
+            return;
+        }
+    }
+    document.question.submit();
+}
