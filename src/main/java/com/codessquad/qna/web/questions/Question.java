443bc026db2e59cf48f644d2fd89644b9f74ff24
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-13 14:54:30 +0900

    필요없는 어노테이션을 지워라

--- src/main/java/com/codessquad/qna/web/questions/Question.java
+++ src/main/java/com/codessquad/qna/web/questions/Question.java
@@ -20,7 +20,6 @@ public class Question {
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answers;
 
-    @Column(nullable = false)
     private String title;
 
     private String contents;
