a7198829841cdc163fa80d2b022b6ca44db58082
hiro <kiryuinh32@gmail.com>
2021-03-13 19:46:06 +0900

    DB추가

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -7,8 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -17,7 +19,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping ("/users/create")
+    @PostMapping ("/create")
     public String create(User user) {
 
         userService.create(user);
@@ -25,7 +27,7 @@ public class UserController {
         return "redirect:/users/list";
     }
 
-    @GetMapping("/users/list")
+    @GetMapping("/list")
     public String list(Model model){
 
         model.addAttribute("users", userService.findUsers());
@@ -33,7 +35,7 @@ public class UserController {
         return "/user/list";
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public String profile(@PathVariable("userId") Long id, Model model) {
 
         model.addAttribute("user", userService.findUser(id));
@@ -41,7 +43,7 @@ public class UserController {
         return "/user/profile";
     }
 
-    @GetMapping("/users/{id}/validation")
+    @GetMapping("/{id}/validation")
     public String userValidation(@PathVariable Long id, Model model){
 
         model.addAttribute("id", id);
@@ -63,7 +65,7 @@ public class UserController {
         return "/user/validation_user";
     }
 
-    @PostMapping("/users/{id}")
+    @PostMapping("/{id}")
     public String userUpdate(@PathVariable Long id, User newUser){
 
         User user = userService.findUser(id).get();
