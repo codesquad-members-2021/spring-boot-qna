74bd856d9c1550cfd22c90e9d6058cc6a4763b90
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-13 16:36:51 +0900

    Add logout

--- src/main/java/com/codessquad/qna/user/UserController.java
+++ src/main/java/com/codessquad/qna/user/UserController.java
@@ -80,4 +80,11 @@ public class UserController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("sessionedUser");
+
+        return "redirect:/";
+    }
 }
