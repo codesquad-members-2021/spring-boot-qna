4086c7e12208e29ff8ec75460128864409c5fc21
noel <68936@naver.com>
2021-03-14 03:56:35 +0900

    [chore] 질문하기 화면에서 글쓴이 필드 삭제 및 에러처리
    - 로그인한 사용자가 누구인지 알 수 있기 때문
    - URL로 질문하기 접근시 405 에러페이지로 리턴

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -1,6 +1,7 @@
 package com.codessquad.qna.controller;
 
 import com.codessquad.qna.domain.Question;
+import com.codessquad.qna.domain.User;
 import com.codessquad.qna.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Optional;
@@ -23,11 +25,17 @@ public class QuestionController {
     }
 
     @PostMapping("/questions")
-    private String questions(Question question) {
+    private String questions(Question question, Model model, HttpSession session) {
         Objects.requireNonNull(question, "Exception: question이 NULL 값입니다.");
-
-        questionRepository.save(question);
-        return "redirect:/";
+        final Object sessionValue = session.getAttribute("sessionedUser");
+        if(sessionValue != null) {
+            User user = (User)sessionValue;
+            question.setWriter(user.getName());
+            questionRepository.save(question);
+            return "redirect:/";
+        }
+        model.addAttribute("errorMessage","로그인 정보가 유효하지 않습니다.");
+        return "error/404"; //m 세션아이디와 다를 경우 이동
     }
 
     @GetMapping("/questions/{id}")
