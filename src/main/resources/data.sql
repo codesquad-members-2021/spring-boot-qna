c924523f6cec31699d42880d15140e87465651c4
noel <68936@naver.com>
2021-03-14 03:56:55 +0900

    [chore] 샘플 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,13 @@
-INSERT INTO USER (user_id, password, new_password, name, email) VALUES ('1234', '1234', '', '노을', 'noel@slipp.net');
-INSERT INTO USER (user_id, password, new_password, name, email) VALUES ('12345', '12345', '', '노을2', 'sanjigi@slipp.net');
+INSERT INTO USER (user_id, password, new_password, name, email) VALUES ('freddy', '1234', '', '프레디', 'freddy@codesquad.com');
+INSERT INTO USER (user_id, password, new_password, name, email) VALUES ('roach', '1234', '', '로치', 'roach@codesquad.com');
+INSERT INTO USER (user_id, password, new_password, name, email) VALUES ('jane', '1234', '', '제인', 'jane@codesquad.com');
+INSERT INTO USER (user_id, password, new_password, name, email) VALUES ('cooper', '1234', '', '쿠퍼', 'cooper@codesquad.com');
+INSERT INTO USER (user_id, password, new_password, name, email) VALUES ('pyro', '1234', '', '파이로', 'pyro@codesquad.com');
+INSERT INTO USER (user_id, password, new_password, name, email) VALUES ('honux', '1234', '', '호눅스', 'honux@codesquad.com');
+INSERT INTO USER (user_id, password, new_password, name, email) VALUES ('noel', '1234', '', '노을', 'noel@codesquad.com');
+INSERT INTO USER (user_id, password, new_password, name, email) VALUES ('1234', '1234', '', '테스트', 'test@codesquad.com');
+
+INSERT INTO QUESTION (writer, title, contents,time) VALUES ('프레디', '안녕하세요, 프레디(BOT)입니다.', '안녕하세요, 프레디(BOT)입니다.', '2021-03-14 03:50:53.961');
+INSERT INTO QUESTION (writer, title, contents,time) VALUES ('제인', '안녕하세요, 제인(BOT)입니다.', '안녕하세요, 제인(BOT)입니다.', '2021-03-14 03:50:53.961');
+INSERT INTO QUESTION (writer, title, contents,time) VALUES ('파이로', '안녕하세요, 파이로(BOT)입니다.', '안녕하세요, 파이로(BOT)입니다.', '2021-03-14 03:50:53.961');
+
