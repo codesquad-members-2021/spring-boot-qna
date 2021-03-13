bad57f2abfac8d108793e82b0014b50072c444cc
tmdgusya <tmdgusya@naver.com>
2021-03-13 18:02:22 +0900

    feat : Comment Read Update Delete 기능 추가
    Comment 가 존재한다면 Comment 를 가져올 수 있도록 getCommentIfExist 를 만들었으며, 없을시 NotFoundException 발생 따라서 get() 을 사용해도 됨. 업데이트와 delete 에서 각 Comment 를 가져와서 삭제 또는 수정할 수 있도록 처리하였습니다.

--- src/main/java/com/codessquad/qna/service/CommentService.java
+++ src/main/java/com/codessquad/qna/service/CommentService.java
@@ -2,8 +2,13 @@ package com.codessquad.qna.service;
 
 import com.codessquad.qna.entity.Comment;
 import com.codessquad.qna.entity.User;
+import com.codessquad.qna.exception.NotFoundException;
 import com.codessquad.qna.repository.CommentRepostiory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class CommentService {
@@ -17,8 +22,34 @@ public class CommentService {
     }
 
     public void addComment(Long postId, User user, String body) {
-        Comment comment = new Comment(postService.getPost(postId), user.getUserId(), body);
+        Comment comment = new Comment(postService.getPost(postId), user, body);
         commentRepostiory.save(comment);
+    }
+
+    public Comment findComment(Long commentId) {
+        return getCommentIfExist(commentId);
+    }
+
+    public void updateComment(Comment comment, String body) {
+        comment.update(body, LocalDateTime.now());
+        commentRepostiory.save(comment);
+    }
+
+    @Transactional
+    public void deleteComment(Long commentId, User user) throws IllegalAccessException {
+        Comment comment = getCommentIfExist(commentId);
+        if (!comment.isMatchedUser(user)) {
+            throw new IllegalAccessException("작성자만 답변을 삭제할 수 있습니다.");
+        }
+        commentRepostiory.deleteById(commentId);
+    }
+
+    private Comment getCommentIfExist(Long commentId) {
+        Optional<Comment> comment = commentRepostiory.findById(commentId);
+        if (!comment.isPresent()) {
+            throw new NotFoundException("해당 댓글을 찾을 수 없습니다.");
+        }
+        return comment.get();
     }
 
 }
