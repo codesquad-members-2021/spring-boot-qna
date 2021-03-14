ff3a4d373986141e660d8cd0ca988813eb32b63e
tmdgusya <tmdgusya@naver.com>
2021-03-14 14:24:05 +0900

    refactor : 게시글 조회간 deleteFlag = flase 인 게시물만 조회

--- src/main/java/com/codessquad/qna/repository/PostRepository.java
+++ src/main/java/com/codessquad/qna/repository/PostRepository.java
@@ -1,12 +1,14 @@
 package com.codessquad.qna.repository;
 
 import com.codessquad.qna.entity.Post;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
 
+    @Query("select p from Post p where p.deleteFlag = false")
     List<Post> findAll();
 
 }
