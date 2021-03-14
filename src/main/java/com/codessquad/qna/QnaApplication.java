8f9231bf5a6d1499d427a7f6e48aee16180b6e55
janeljs <jisunlim818@gmail.com>
2021-03-15 05:51:04 +0900

    Modify QnaApplication
    - API 문서화 및 테스트를 위해 api()와 apiInfo() 메서드 추가

--- src/main/java/com/codessquad/qna/QnaApplication.java
+++ src/main/java/com/codessquad/qna/QnaApplication.java
@@ -2,13 +2,43 @@ package com.codessquad.qna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSwagger2
 public class QnaApplication {
     public static void main(String[] args) {
         SpringApplication.run(QnaApplication.class, args);
     }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("spring-boot-qna")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Q&A API")
+                .version("1.0")
+                .contact(new Contact("Jane", "https://github.com/janeljs", "jisunlim818@gmail.com"))
+                .description("My First Q&A App API")
+                .build();
+    }
+
 }
 
