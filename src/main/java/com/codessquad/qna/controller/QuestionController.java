7938c6d048102782df8f5d1e4c3b795abb4f83c1
hiro <kiryuinh32@gmail.com>
2021-03-14 11:07:51 +0900

    IndexContorller 추가

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -8,8 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/questions")
 public class QuestionController {
 
     private final QuestionService questionService;
@@ -18,7 +20,7 @@ public class QuestionController {
         this.questionService = questionService;
     }
 
-    @PostMapping("/questions")
+    @PostMapping("/")
     public String question(Question question){
 
         questionService.registerQuestion(question);
@@ -26,15 +28,7 @@ public class QuestionController {
         return "redirect:/";
     }
 
-    @GetMapping("/")
-    public String questionList(Model model) {
-
-        model.addAttribute("questions", questionService.findQuestions());
-
-        return "/index";
-    }
-
-    @GetMapping("/questions/{index}")
+    @GetMapping("/{index}")
     public String showQuestionDetail(@PathVariable Long index, Model model) {
 
         model.addAttribute("question", questionService.findQuestion(index).get());
