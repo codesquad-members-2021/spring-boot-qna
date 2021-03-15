3951dede945beb56e9fcf3146fdfd6bd4f67d97c
MJbae <gently.gogo@gmail.com>
2021-03-15 09:22:45 +0900

    Refactor: 목록 조희 기능의 반환타입 변경(Iterable -> List)

--- src/main/java/com/codessquad/qna/service/UserService.java
+++ src/main/java/com/codessquad/qna/service/UserService.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -33,7 +34,7 @@ public class UserService {
                 });
     }
 
-    public Iterable<User> showAll() {
+    public List<User> showAll() {
         return userRepository.findAll();
     }
 
