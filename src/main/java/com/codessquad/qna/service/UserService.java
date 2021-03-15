aabf5bdb3019c153743d926e6f28dbb30ffa07cd
95degree <95degree@naver.com>
2021-03-15 18:20:08 +0900

    UserService 생성
    repository를 컨트롤러가 아닌 service에서 처리하기 위해 UserService를 생성했습니다.

---
+++ src/main/java/com/codessquad/qna/service/UserService.java
@@ -0,0 +1,16 @@
+package com.codessquad.qna.service;
+
+import com.codessquad.qna.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
