bba8bbd04e0919094620f70a73ed0e9a5cd13e64
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-03-15 17:35:20 +0900

    RequestMapping 사용

--- src/main/java/com/codessquad/qna/web/QuestionController.java
+++ src/main/java/com/codessquad/qna/web/QuestionController.java
@@ -8,8 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/qna")
 public class QuestionController {
     private final QnaRepository qnaRepository;
 
@@ -17,22 +19,22 @@ public class QuestionController {
         this.qnaRepository = qnaRepository;
     }
 
-    @PostMapping("/qna")
+    @PostMapping
     public String createNewQna(Qna qna) {
         if (qna == null) {
             return "redirect:/qna";
         }
         qnaRepository.save(qna);
-        return "redirect:/";
+        return "redirect:/qna";
     }
 
-    @GetMapping("/")
+    @GetMapping
     public String qnaList(Model model) {
         model.addAttribute("qnaList", qnaRepository.findAll());
         return "index";
     }
 
-    @GetMapping("qna/{qnaId}")
+    @GetMapping("/{qnaId}")
     public String showOneQuestion(@PathVariable long qnaId, Model model) {
         model.addAttribute("Qna", qnaRepository.findById(qnaId).orElseThrow(NoQuestionException::new));
         return "qna/show";
--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -10,8 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
@@ -21,7 +23,7 @@ public class UserController {
 
     private final Logger logger = LoggerFactory.getLogger(QuestionController.class);
 
-    @PostMapping("/")
+    @PostMapping
     public String create(User user) {
         if (user == null) {
             return "redirect:/";
@@ -30,33 +32,32 @@ public class UserController {
         return "redirect:/users";
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public String getList(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "user/list";
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public String getOneUserProfile(@PathVariable long id, Model model) {
         model.addAttribute("user", userRepository.findById(id).orElseThrow(NoUserException::new));
         return "user/profile";
     }
 
-    @GetMapping("/users/{id}/form")
+    @GetMapping("/{id}/form")
     public String editUserInfo(@PathVariable long id, Model model) {
         model.addAttribute("user", userRepository.findById(id).orElseThrow(NoUserException::new));
         return "user/updateForm";
     }
 
-    @PostMapping("/users/{id}/form")
-    public String update(@PathVariable long id, User updateUser) {
+    @PostMapping("/{id}/form")
+    public String update(@PathVariable long id, User updateUser, String newPassword) {
         User user = userRepository.findById(id).orElseThrow(NoUserException::new);
-        if(user.checkPassword(updateUser.getPassword())){
-            logger.info(updateUser.getPassword() + ": 올바르지 않은 패스워드입니다.정보가 유지됩니다.");
+        if(!(user.checkPassword(updateUser.getPassword()))){
+            user.update(updateUser, newPassword);
+            userRepository.save(user);
         }
-        user.update(updateUser);
-
-        userRepository.save(user);
+        logger.info("Error: 올바르지 않은 패스워드입니다.정보가 유지됩니다.");
         return "redirect:/users";
     }
 
