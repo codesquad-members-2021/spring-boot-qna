90cc867c3048dc59c9cce1fcaceb47fa7f488d0b
95degree <95degree@naver.com>
2021-03-15 18:02:12 +0900

    QuestionService 생성자 추가
    repository를 사용하기 위해 생성자로 의존성을 주입시켰습니다.

--- src/main/java/com/codessquad/qna/service/QuestionService.java
+++ src/main/java/com/codessquad/qna/service/QuestionService.java
@@ -1,4 +1,13 @@
 package com.codessquad.qna.service;
 
+import com.codessquad.qna.domain.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    @Autowired
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
 }
