da63aff083c8116e48c138ad990d073f89ce1609
rla36 <bhmarsd@naver.com>
2021-03-15 17:17:23 +0900

    Question의 setter을  생성자 injection으로 수정

--- src/main/java/com/codessquad/qna/web/domain/Question.java
+++ src/main/java/com/codessquad/qna/web/domain/Question.java
@@ -1,5 +1,7 @@
 package com.codessquad.qna.web.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.persistence.*;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -12,48 +14,40 @@ public class Question {
     @GeneratedValue
     private Long id;
 
-    @Column(nullable=false, length=20)
+    @Column(nullable = false, length = 20)
     private String writer;
 
-    @Column(nullable=false, length=20)
+    @Column(nullable = false, length = 20)
     private String title;
 
-    @Column(nullable=false, length=500)
+    @Column(nullable = false, length = 500)
     private String contents;
 
-    @Column(nullable=false, length=20)
+    @Column(nullable = false, length = 20)
     private String dateTime;
+
+    protected Question() {}
+
+    public Question(String writer, String title, String contents) {
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+    }
 
     public String getWriter() {
         return writer;
-    }
-
-    public void setWriter(String writer) {
-        this.writer = writer;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getContents() {
         return contents;
     }
 
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
-
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getDateTime() {
