4636a5e8eb3c6c03a9b198eed5409c8d574da829
tmdgusya <tmdgusya@naver.com>
2021-03-16 12:08:09 +0900

    fix : 약어 제거로 인한 수정

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-insert into USER (USER_PK, USER_ID, PASSWORD, NAME, EMAIL) values (1, 'roach', '1234', 'roach', 'roach@naver.com');
-insert into User (USER_PK, USER_ID, PASSWORD, NAME, EMAIL) values (2, 'honux', '1234', 'honux', 'honux@naver.com');
+insert into USER (ID, USER_ID, PASSWORD, NAME, EMAIL) values (1, 'roach', '1234', 'roach', 'roach@naver.com');
+insert into User (ID, USER_ID, PASSWORD, NAME, EMAIL) values (2, 'honux', '1234', 'honux', 'honux@naver.com');
