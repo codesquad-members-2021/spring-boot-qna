924b8da4b40109089581bc581f4fe1c9f9ad5417
정이삭 <isaac56@naver.com>
2021-03-16 16:26:09 +0900

    Change user/updateForm to request PUT method instead of POST
    User의 수정화면에서 POST 대신 PUT 요청을 보내도록 수정

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -5,10 +5,7 @@ import com.codessquad.qna.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -54,7 +51,7 @@ public class UserController {
         return "redirect:/users";
     }
 
-    @PostMapping("/{userId}/update")
+    @PutMapping("/{userId}/update")
     public String updateUser(User user) {
         userService.updateUser(user);
         return "redirect:/users";
