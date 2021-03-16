1fa0f23ef92801a5997dd48a99b171be53c9d1b3
Jihye Woo <woojihye2339@gmail.com>
2021-03-17 02:21:20 +0900

    edit : SessionUtils 수정
      > 로그인되지 않은 상태
        - SessionUtils 내부에서 바로 exception handling

--- src/main/java/com/codessquad/qna/web/utils/SessionUtils.java
+++ src/main/java/com/codessquad/qna/web/utils/SessionUtils.java
@@ -1,20 +1,25 @@
 package com.codessquad.qna.web.utils;
 
 import com.codessquad.qna.web.domain.user.User;
+import com.codessquad.qna.web.exception.UserNotFoundException;
 
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 public class SessionUtils {
 
-    public static Optional<User> getLoginUser(HttpSession session){
-        Object user = session.getAttribute("sessionedUser");
-        return Optional.ofNullable((User) user);
+    public static User getLoginUser(HttpSession session){
+        return userFromSession(session)
+                .orElseThrow(() -> new UserNotFoundException("No login user"));
     }
 
     public static boolean isLoginUser(HttpSession session){
-        return getLoginUser(session).isPresent();
+        return userFromSession(session).isPresent();
     }
 
+    private static Optional<User> userFromSession(HttpSession session){
+        User user = (User) session.getAttribute("sessionedUser");
+        return Optional.ofNullable(user);
+    }
 
 }
