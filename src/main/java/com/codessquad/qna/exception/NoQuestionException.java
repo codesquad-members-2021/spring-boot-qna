1eaa2d83862872c078d15cfd2e4749ae3c7a76ce
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-03-16 10:46:17 +0900

    포매팅

--- src/main/java/com/codessquad/qna/exception/NoQuestionException.java
+++ src/main/java/com/codessquad/qna/exception/NoQuestionException.java
@@ -1,6 +1,6 @@
 package com.codessquad.qna.exception;
 
-public class NoQuestionException extends RuntimeException{
+public class NoQuestionException extends RuntimeException {
     public NoQuestionException() {
         super("해당 질문이 존재하지 않습니다.");
     }
--- src/main/java/com/codessquad/qna/exception/NoUserException.java
+++ src/main/java/com/codessquad/qna/exception/NoUserException.java
@@ -1,6 +1,6 @@
 package com.codessquad.qna.exception;
 
-public class NoUserException extends RuntimeException{
+public class NoUserException extends RuntimeException {
 
     public NoUserException() {
         super("해당 User가 존재하지 않습니다.");
