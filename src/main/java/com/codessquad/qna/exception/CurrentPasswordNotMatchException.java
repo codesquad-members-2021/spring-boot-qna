2d4734b73f5c8a6d7331e034b889205795dab23b
jeong-inho <derosatam76@gmail.com>
2021-03-16 13:37:20 +0900

    Add user member variable in CurrentPasswordNotMatchException class 해당 예외가 발생할 때 유저 정보를 넘겨줄 필요성이 있기 때문에 user 멤버 변수 생성

--- src/main/java/com/codessquad/qna/exception/CurrentPasswordNotMatchException.java
+++ src/main/java/com/codessquad/qna/exception/CurrentPasswordNotMatchException.java
@@ -1,9 +1,17 @@
 package com.codessquad.qna.exception;
 
+import com.codessquad.qna.model.User;
+
 public class CurrentPasswordNotMatchException extends RuntimeException{
 
-    public CurrentPasswordNotMatchException() {
+    private final User user;
+
+    public CurrentPasswordNotMatchException(User user) {
         super("기존 비밀번호가 일치하지 않습니다.");
+        this.user = user;
     }
 
+    public User getUser() {
+        return user;
+    }
 }
