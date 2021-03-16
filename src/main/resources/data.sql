e804d652576fda43a14984bbe2281273561fa4e4
MJbae <gently.gogo@gmail.com>
2021-03-16 10:39:44 +0900

    Chore: 사용자 정보 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO USER (user_id, password, name, email) VALUES ('javajigi', 'test', '자바지기', 'javajigi@slipp.net');
 INSERT INTO USER (user_id, password, name, email) VALUES ('sanjigi', 'test', '산지기', 'sanjigi@slipp.net');
+INSERT INTO USER (user_id, password, name, email) VALUES ('123', '123', '123', '123@slipp.net');
+INSERT INTO USER (user_id, password, name, email) VALUES ('abc', 'abc', 'abc', 'abc@slipp.net');
