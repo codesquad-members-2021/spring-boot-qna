f378f3a9f054424f566ba1ef3113a3e0c90c34e6
tmdgusya <tmdgusya@naver.com>
2021-03-16 18:18:45 +0900

    fix : 필요없는 로직 삭제
    유저는 이제 세션에서 받아오므로 null 값인지를 검증해줄 필요가 없음.

--- src/main/java/com/codessquad/qna/util/PostValidation.java
+++ src/main/java/com/codessquad/qna/util/PostValidation.java
@@ -1,7 +1,6 @@
 package com.codessquad.qna.util;
 
 import com.codessquad.qna.dto.PostDto;
-import com.codessquad.qna.entity.User;
 
 public class PostValidation {
 
@@ -10,18 +9,11 @@ public class PostValidation {
 
     public static void validate(PostDto postDto) {
         validateTitle(postDto.getTitle());
-        validateAuthor(postDto.getAuthor());
     }
 
     private static void validateTitle(String title) {
         if (title.isEmpty()) {
             throw new IllegalArgumentException("유효하지 않은 제목입니다.");
-        }
-    }
-
-    private static void validateAuthor(User author) {
-        if (author == null) {
-            throw new IllegalArgumentException("유효하지 않은 유저 입니다.");
         }
     }
 
