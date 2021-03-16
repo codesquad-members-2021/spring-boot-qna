47bbf19083a71ecb65771540e0af5a6194d596c6
jeong-inho <derosatam76@gmail.com>
2021-03-16 13:38:27 +0900

    Add spacing

--- src/main/java/com/codessquad/qna/exception/IdOrPasswordNotMatchException.java
+++ src/main/java/com/codessquad/qna/exception/IdOrPasswordNotMatchException.java
@@ -1,6 +1,6 @@
 package com.codessquad.qna.exception;
 
-public class IdOrPasswordNotMatchException extends  RuntimeException{
+public class IdOrPasswordNotMatchException extends  RuntimeException {
 
     public IdOrPasswordNotMatchException() {
         super("아이디나 비밀번호가 일치하지 않습니다.");
