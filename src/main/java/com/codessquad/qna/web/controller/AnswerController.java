17f1fd3293aa3a65923650d4371c19c56aaaf690
Jihye Woo <woojihye2339@gmail.com>
2021-03-17 00:08:09 +0900

    feat : 질문 생성 메소드 추가
      > AnswerController에 create() 메소드 구현
        - @PostMapping("/questions/{questionId}/answers")
        - session으로 writer 정보 가져오기
        - questionId로 question 정보 가져오기
        - Answer 객체 생성 후 answerRepsoitory를 통해 DB에 저장

--- src/main/java/com/codessquad/qna/web/controller/AnswerController.java
+++ src/main/java/com/codessquad/qna/web/controller/AnswerController.java
@@ -1,20 +1,46 @@
 package com.codessquad.qna.web.controller;
 
+import com.codessquad.qna.web.domain.answer.Answer;
 import com.codessquad.qna.web.domain.answer.AnswerRepository;
+import com.codessquad.qna.web.domain.question.Question;
 import com.codessquad.qna.web.domain.question.QuestionRepository;
+import com.codessquad.qna.web.domain.user.User;
+import com.codessquad.qna.web.utils.SessionUtils;
+import javassist.NotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("/questions/{questionId}/answers")
 public class AnswerController {
 
     private final AnswerRepository answerRepository;
     private final QuestionRepository questionRepository;
 
 
-    public AnswerController(AnswerRepository answerRepository, QuestionRepository questionRepository){
+    public AnswerController(AnswerRepository answerRepository, QuestionRepository questionRepository) {
         this.answerRepository = answerRepository;
         this.questionRepository = questionRepository;
     }
 
+    @PostMapping("/")
+    public String create(@PathVariable Long questionId, @PathVariable Long id,
+                         String contents, HttpSession session) throws NotFoundException {
+        if (!SessionUtils.isLoginUser(session)) {
+            return "redirect:/users/login-form";
+        }
+        User user = SessionUtils.getLoginUser(session)
+                .orElseThrow(() -> new NotFoundException("No login user"));
+
+        Question question = questionRepository.findById(questionId).orElseThrow(IllegalArgumentException::new);
+
+        Answer answer = new Answer(user, question, contents);
+        answerRepository.save(answer);
+        return "redirect:/questions/" + questionId;
+    }
 
 }
