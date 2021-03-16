7451858c770d6396001c58b142f7222cad8a220b
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-16 19:37:30 +0900

    chore : 불필요한 어노테이션을 제거하라
    - 이미 HandlebarsHelper어노테이션에서 추가한 어노테이션이라 제거함

--- src/main/java/com/codessquad/qna/web/utils/HandlebarDateHelper.java
+++ src/main/java/com/codessquad/qna/web/utils/HandlebarDateHelper.java
@@ -1,12 +1,10 @@
 package com.codessquad.qna.web.utils;
 
-import org.springframework.stereotype.Component;
 import pl.allegro.tech.boot.autoconfigure.handlebars.HandlebarsHelper;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Component
 @HandlebarsHelper
 public class HandlebarDateHelper {
     private static final DateTimeFormatter MY_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
