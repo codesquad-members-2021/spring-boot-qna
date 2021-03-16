ceee34734531fa1bc244194234da3a52ef74ec36
정이삭 <isaac56@naver.com>
2021-03-16 15:31:47 +0900

    Replace List<Question> with QuestionRepository
    JPA를 사용하여 DB에 접근하도록 List<Question>를 QuestionRepository로 대체했습니다.

--- src/main/java/com/codessquad/qna/questions/QuestionService.java
+++ src/main/java/com/codessquad/qna/questions/QuestionService.java
@@ -1,32 +1,28 @@
 package com.codessquad.qna.questions;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class QuestionService {
-    private final List<Question> questions = Collections.synchronizedList(new ArrayList<>());
+    private final QuestionRepository questionRepository;
+
+    @Autowired
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
 
     public void addQuestion(Question question) {
-        synchronized (questions) {
-            question.setIndex(questions.size() + 1);
-            questions.add(question);
-        }
+        questionRepository.save(question);
     }
 
-    public List<Question> getQuestions() {
-        return questions;
+    public Iterable<Question> getQuestions() {
+        return questionRepository.findAll();
     }
 
-    public Optional<Question> getQuestion(int index) {
-        int actualIndex = index - 1;
-        if (actualIndex < 0 || questions.size() <= actualIndex) {
-            return Optional.empty();
-        }
-        return Optional.of(questions.get(actualIndex));
+    public Optional<Question> getQuestion(long index) {
+        return questionRepository.findById(index);
     }
 }
