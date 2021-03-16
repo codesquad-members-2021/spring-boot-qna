33541685faf671c2d4378fac580c0914fe30c86d
rla36 <bhmarsd@naver.com>
2021-03-16 11:00:03 +0900

    QuestionService추가
    QuestionService 레이어를 추가해 Controller와 Repository를
    이어주도록했습니다. 그리고 QuestionRepository엔 findAll 메소드가 List를
    리턴하도록 override해 QuestionService의 findAll메소드 역시 list를
    리턴할수있게 됐습니다.

---
+++ src/main/java/com/codessquad/qna/web/service/QuestionService.java
@@ -0,0 +1,30 @@
+package com.codessquad.qna.web.service;
+
+import com.codessquad.qna.web.domain.Question;
+import com.codessquad.qna.web.repository.QuestionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class QuestionService {
+
+    private QuestionRepository questionRepository;
+
+    private QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    public void save(Question question) {
+        questionRepository.save(question);
+    }
+
+    public List<Question> findAll() {
+        return questionRepository.findAll();
+    }
+
+    public Optional<Question> findById(Long id) {
+        return questionRepository.findById(id);
+    }
+}
