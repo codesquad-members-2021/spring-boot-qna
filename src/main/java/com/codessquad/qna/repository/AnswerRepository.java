d96aa4cc5cdaed1521e5b1050103d43fb30ec0e4
jeong-inho <derosatam76@gmail.com>
2021-03-16 18:29:20 +0900

    Delete spacing

--- src/main/java/com/codessquad/qna/repository/AnswerRepository.java
+++ src/main/java/com/codessquad/qna/repository/AnswerRepository.java
@@ -3,6 +3,5 @@ package com.codessquad.qna.repository;
 import com.codessquad.qna.model.Answer;
 import org.springframework.data.repository.CrudRepository;
 
-
 public interface AnswerRepository extends CrudRepository<Answer, Long> {
 }
