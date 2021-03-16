bdde82932df8425e19a6b8e9574fdd54588438e2
bong6981 <kbk5190@naver.com>
2021-03-16 14:54:37 +0900

    refactor : Refactor HttpSessionUtils
    - 코드를 간단히 만들기 위한 리팩토링

--- src/main/java/com/codessquad/qna/web/HttpSessionUtils.java
+++ src/main/java/com/codessquad/qna/web/HttpSessionUtils.java
@@ -8,17 +8,10 @@ public class HttpSessionUtils {
     public static final String USER_SESSION_KEY = "sessionedUser";
 
     public static boolean isLoginUser(HttpSession session) {
-        Object sessionedUser = session.getAttribute(USER_SESSION_KEY);
-        if (sessionedUser == null) {
-            return false;
-        }
-        return true;
+        return session.getAttribute(USER_SESSION_KEY) != null;
     }
 
     public static User getSessionedUser(HttpSession session) {
-        if (!isLoginUser(session)) {
-            return null;
-        }
-        return (User) session.getAttribute(USER_SESSION_KEY);
+        return isLoginUser(session) ?  (User) session.getAttribute(USER_SESSION_KEY) : null;
     }
 }
