cf0a52ac95209f9180d7a5e6d72bf4ceee1ff58d
jeong-inho <derosatam76@gmail.com>
2021-03-16 11:36:28 +0900

    Create handleCurrentPasswordException method in GlobalExceptionHandler class 개인정보 수정 요청에 기존 비밀번호가 일치하지 않은 경우를 처리하기 위해 handleCurrentPasswordException 메소드 생성

--- src/main/java/com/codessquad/qna/exception/GlobalExceptionHandler.java
+++ src/main/java/com/codessquad/qna/exception/GlobalExceptionHandler.java
@@ -4,6 +4,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.servlet.http.HttpSession;
+
+import static com.codessquad.qna.controller.HttpSessionUtils.getUserFromSession;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -19,14 +23,22 @@ public class GlobalExceptionHandler {
         return "/user/login";
     }
 
-    @ExceptionHandler(UserNotFoundException.class)
-    private String handleUserNotFoundException() {
-        return "redirect:/user/list";
+    @ExceptionHandler(CurrentPasswordNotMatchException.class)
+    private String handleCurrentPasswordNotMatchException(Model model, CurrentPasswordNotMatchException e, HttpSession session) {
+        model.addAttribute("user", getUserFromSession(session));
+        model.addAttribute("errorMessage", e.getMessage());
+        return "/user/updateForm";
     }
 
     @ExceptionHandler(IllegalUserAccessException.class)
-    private String handleIllegalUserAccessException() {
-        return "redirect:/user/login";
+    private String handleIllegalUserAccessException(Model model, IllegalUserAccessException e) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "/user/login";
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    private String handleUserNotFoundException() {
+        return "redirect:/user/list";
     }
 
 }
