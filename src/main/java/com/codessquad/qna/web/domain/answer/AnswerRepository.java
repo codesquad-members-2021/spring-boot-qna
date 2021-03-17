e961df9cb36adf342309f0c485664a924201ca41
Jihye Woo <woojihye2339@gmail.com>
2021-03-17 11:30:17 +0900

    fix : AnswerRepository 메소드 파라미터 추가
      > findByQuestionId() 파라미터 추가
        - Long 타입 : questionId

--- src/main/java/com/codessquad/qna/web/domain/answer/AnswerRepository.java
+++ src/main/java/com/codessquad/qna/web/domain/answer/AnswerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
-    List<Answer> findByQuestionId();
+    List<Answer> findByQuestionId(Long questionId);
 }
