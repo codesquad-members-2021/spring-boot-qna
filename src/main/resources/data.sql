95f63e85a3052076a89fc75ee0ed5595fa020203
rla36 <bhmarsd@naver.com>
2021-03-17 15:28:34 +0900

    테스트 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO USER (id, user_id, password, name, email) VALUES (1, 'javajigi', 'test', '자바지기', 'javajigi@slipp.net');
+INSERT INTO USER (id, user_id, password, name, email) VALUES (2, 'sanjigi', 'test', '산지기', 'sanjigi@slipp.net');
+INSERT INTO USER (id, user_id, password, name, email) VALUES (3, 'userId1', 'a', 'name1', 'email1@gmail.com');
