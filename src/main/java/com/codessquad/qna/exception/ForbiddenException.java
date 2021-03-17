1001f37c725d7c99167c271360a8f7f5ee97aa0c
K <PizzaCola.K@gmail.com>
2021-03-17 14:59:43 +0900

    feat: Default Exception message
    - Exception의 기본 생성자에 기본 메시지를 추가하였습니다.

--- src/main/java/com/codessquad/qna/exception/ForbiddenException.java
+++ src/main/java/com/codessquad/qna/exception/ForbiddenException.java
@@ -1,6 +1,11 @@
 package com.codessquad.qna.exception;
 
-public class ForbiddenException extends RuntimeException{
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException() {
+        super("Forbidden");
+    }
+
     public ForbiddenException(String message) {
         super(message);
     }
--- src/main/java/com/codessquad/qna/exception/UnauthorizedAccessException.java
+++ src/main/java/com/codessquad/qna/exception/UnauthorizedAccessException.java
@@ -1,6 +1,11 @@
 package com.codessquad.qna.exception;
 
-public class UnauthorizedAccessException extends RuntimeException{
+public class UnauthorizedAccessException extends RuntimeException {
+
+    public UnauthorizedAccessException() {
+        super("Unauthorized Access");
+    }
+
     public UnauthorizedAccessException(String message) {
         super(message);
     }
