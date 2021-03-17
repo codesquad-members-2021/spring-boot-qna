ccba2aa8cb0c9c785d8003964ac1b963b633a728
tmdgusya <tmdgusya@naver.com>
2021-03-17 15:33:49 +0900

    feat : ajax 를 통한 댓글 추가기능 구현

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,44 @@
+$(".submit-write input[type = submit]").click(addAnswer);
+
+function addAnswer(event) {
+    event.preventDefault();
+    console.log("click ?");
+
+    const queryString = $(".submit-write").serialize();
+    console.log("query :" + queryString);
+
+    const url = $(".submit-write").attr("action");
+    console.log("url : " + url)
+
+    $.ajax({
+        type : 'post',
+        url : url,
+        data : queryString,
+        dataType : 'json',
+        error : onError,
+        success : onSuccess
+    });
+}
+
+function onError() {
+
+}
+
+function onSuccess(data, status) {
+    console.log(data);
+    const answerTemplate = $("#answerTemplate").html();
+    console.log(data.author.userId, data.createDateTime, data.body, data.post.postId, data.id);
+    const template = answerTemplate.format(data.author.userId, data.createDateTime, data.body, data.post.postId, data.id);
+    $(".qna-comment-slipp-articles").prepend(template);
+    $("textarea[name=body]").val("");
+}
+
 String.prototype.format = function() {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
-    return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-  });
+    const args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+        return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+            ;
+    });
 };
