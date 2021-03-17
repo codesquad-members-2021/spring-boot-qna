7d8bcc18dbc394dcf0a8eead81a4b68e2d99231b
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-17 17:08:27 +0900

    refactor : 답변생성 메서드를 다시 AnswerController로 옮겨라
    - 답변을 생성하는 기능인만큼 AnswerController에 있는게 더 맞다고 생각해서 다시 옮김

--- src/main/java/com/codessquad/qna/web/answers/AnswersController.java
+++ src/main/java/com/codessquad/qna/web/answers/AnswersController.java
@@ -2,24 +2,42 @@ package com.codessquad.qna.web.answers;
 
 import com.codessquad.qna.web.exceptions.answers.AnswerNotFoundException;
 import com.codessquad.qna.web.exceptions.auth.UnauthorizedAccessException;
+import com.codessquad.qna.web.exceptions.questions.QuestionNotFoundException;
 import com.codessquad.qna.web.questions.Question;
+import com.codessquad.qna.web.questions.QuestionRepository;
 import com.codessquad.qna.web.users.User;
 import com.codessquad.qna.web.utils.SessionUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("/answers")
 public class AnswersController {
+    private final QuestionRepository questionRepository;
     private final AnswersRepository answersRepository;
 
-    public AnswersController(AnswersRepository answersRepository) {
+    public AnswersController(AnswersRepository answersRepository, QuestionRepository questionRepository) {
         this.answersRepository = answersRepository;
+        this.questionRepository = questionRepository;
     }
 
-    @DeleteMapping("/answers/{answerId}")
+    @PostMapping("/{questionId}")
+    public String createAnswer(@PathVariable("questionId") long questionId, String answerContents,
+                               HttpSession session) {
+        User sessionUser = SessionUtil.getLoginUser(session);
+        Question targetQuestion = questionRepository.findById(questionId)
+                .orElseThrow(QuestionNotFoundException::new);
+
+        answersRepository.save(new Answer(answerContents, targetQuestion, sessionUser));
+        return "redirect:/questions/" + questionId;
+    }
+
+    @DeleteMapping("/{answerId}")
     public String deleteAnswer(@PathVariable("answerId") long answerId, HttpSession session) {
         User sessionUser = SessionUtil.getLoginUser(session);
         Answer targetAnswer = answersRepository.findById(answerId)
--- src/main/java/com/codessquad/qna/web/questions/QuestionsController.java
+++ src/main/java/com/codessquad/qna/web/questions/QuestionsController.java
@@ -1,7 +1,5 @@
 package com.codessquad.qna.web.questions;
 
-import com.codessquad.qna.web.answers.Answer;
-import com.codessquad.qna.web.answers.AnswersRepository;
 import com.codessquad.qna.web.exceptions.auth.UnauthorizedAccessException;
 import com.codessquad.qna.web.exceptions.questions.QuestionNotFoundException;
 import com.codessquad.qna.web.users.User;
@@ -20,12 +18,9 @@ public class QuestionsController {
     private static final Logger LOGGER = LoggerFactory.getLogger(QuestionsController.class);
 
     private final QuestionRepository questionRepository;
-    private final AnswersRepository answersRepository;
 
-    public QuestionsController(QuestionRepository questionRepository,
-                               AnswersRepository answersRepository) {
+    public QuestionsController(QuestionRepository questionRepository) {
         this.questionRepository = questionRepository;
-        this.answersRepository = answersRepository;
     }
 
     @PostMapping
@@ -36,19 +31,6 @@ public class QuestionsController {
         LOGGER.info("question created " + newQuestion);
         return "redirect:/";
     }
-
-    @PostMapping("/{questionId}/answers")
-    public String createAnswer(@PathVariable("questionId") long questionId, String answerContents,
-                               HttpSession session) {
-        User sessionUser = SessionUtil.getLoginUser(session);
-
-        Question targetQuestion = questionRepository.findById(questionId)
-                .orElseThrow(QuestionNotFoundException::new);
-
-        answersRepository.save(new Answer(answerContents, targetQuestion, sessionUser));
-        return "redirect:/questions/" + questionId;
-    }
-
 
     @GetMapping
     public String getQuestionList(Model model) {
