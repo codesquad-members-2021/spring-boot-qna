59be0e4f304750eb0ab74d5aa0c3cb7937d8c20c
Shion <ehdrhelr@gmail.com>
2021-03-17 18:54:25 +0900

    refactor : 답변 삭제 Ajax 처리

--- src/main/java/com/codessquad/qna/controller/ApiAnswerController.java
+++ src/main/java/com/codessquad/qna/controller/ApiAnswerController.java
@@ -1,13 +1,11 @@
 package com.codessquad.qna.controller;
 
+import com.codessquad.qna.domain.Result;
 import com.codessquad.qna.domain.answer.Answer;
 import com.codessquad.qna.domain.user.User;
 import com.codessquad.qna.service.AnswerService;
 import com.codessquad.qna.utils.HttpSessionUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -30,5 +28,21 @@ public class ApiAnswerController {
         answer.setWriter(sessionedUser);
         return answerService.create(questionId, answer);
 
+    }
+
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable Long questionId, @PathVariable Long id, HttpSession session) {
+        if (!HttpSessionUtils.isLoginUser(session)) {
+            return Result.fail("로그인해야 합니다.");
+        }
+
+        Answer answer = answerService.findById(id);
+        User loginUser = HttpSessionUtils.getUserFromSession(session);
+        if (!answer.isWrittenBy(loginUser)) {
+            return Result.fail("자신의 글만 삭제할 수 있습니다.");
+        }
+
+        answerService.deleteById(id);
+        return Result.ok();
     }
 }
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -25,20 +25,48 @@ function addAnswer(e) {
     function onSuccess(data, status) {
         console.log(data);
         var answerTemplate = $("#answerTemplate").html();
-        var template = answerTemplate.format(data.writer.userId, data.date, data.contents, data.id, data.id);
+        var template = answerTemplate.format(data.writer.userId, data.date, data.contents, data.question.id, data.id);
         $(".qna-comment-slipp-articles").prepend(template);
 
         $(".answer-write textarea").val("");
     }
-
-    String.prototype.format = function () {
-        var args = arguments;
-        return this.replace(/{(\d+)}/g, function (match, number) {
-            return typeof args[number] != 'undefined'
-                ? args[number]
-                : match
-                ;
-        });
-    };
-
 }
+
+$("a.link-delete-article").click(deleteAnswer);
+
+function deleteAnswer(e) {
+    e.preventDefault();
+
+    var deleteBtn = $(this);
+    var url = deleteBtn.attr("href");
+    console.log("url : " + url);
+
+    $.ajax({
+        type: 'delete',
+        url: url,
+        dataType: 'json',
+        error: function (xhr, status) {
+            console.log("error");
+        },
+        success: function (data, status) {
+            console.log(data);
+            if (data.valid) {
+                deleteBtn.closest("article").remove();
+            } else {
+                alert(data.errorMessage);
+            }
+        }
+    });
+}
+
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+            ;
+    });
+};
+
+
