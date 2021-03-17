93adf2d5bd106b191fd69855bbdca9a0ee1955a7
gisungchoi <chlrltjd159@naver.com>
2021-03-17 21:01:32 +0900

    fix : ApiAnswerController의 url 변경
    맨 앞에 ' / '를 붙여준다.

--- src/main/java/com/codessquad/qna/controller/api/ApiAnswerController.java
+++ src/main/java/com/codessquad/qna/controller/api/ApiAnswerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 
 @RestController
-@RequestMapping("api/questions/{questionId}/answers")
+@RequestMapping("/api/questions/{questionId}/answers")
 public class ApiAnswerController {
 
     private final AnswerService answerService;
