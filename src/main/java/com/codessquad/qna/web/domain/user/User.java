78f9cfb8c91a2fd7260271e921cc6de2e2da5971
Jihye Woo <woojihye2339@gmail.com>
2021-03-18 01:20:29 +0900

    fix : 삭제된 User 클래스의 equals() 복구
      > equals()와 hashCode() 오버라이드

--- src/main/java/com/codessquad/qna/web/domain/user/User.java
+++ src/main/java/com/codessquad/qna/web/domain/user/User.java
@@ -86,6 +86,19 @@ public class User {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(userId, user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId);
+    }
+
+    @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
