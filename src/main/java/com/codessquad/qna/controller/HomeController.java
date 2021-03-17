eb1c3cb55edf2dde1c61306afebffdfb2948d748
bibi <bibi6666667@kakao.com>
2021-03-17 11:12:51 +0900

    HomeController 수정
    - 불필요한 파라미터 및 import class 삭제

--- src/main/java/com/codessquad/qna/controller/HomeController.java
+++ src/main/java/com/codessquad/qna/controller/HomeController.java
@@ -1,16 +1,13 @@
 package com.codessquad.qna.controller;
 
-import com.codessquad.qna.repository.Question;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String goHome(Model model) {
+    public String showQuestions() {
         return "redirect:/questions";
     }
 }
