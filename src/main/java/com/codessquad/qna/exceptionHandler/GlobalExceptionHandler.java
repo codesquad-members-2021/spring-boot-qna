11272dd63b3ff562ff9df610c4b801cdc59ddfc4
정이삭 <isaac56@naver.com>
2021-03-17 23:59:37 +0900

    Handle UserNotFoundInSessionException (GlobalExceptionHandler)
    GlobalExceptionHandler에서 UserNotFoundInSessionException 발생 시, login 화면으로 redirect하도록 하였습니다.
    (session이 expire 되었기 때문에 login화면으로 보내도록 했습니다.)

--- src/main/java/com/codessquad/qna/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/codessquad/qna/exceptionHandler/GlobalExceptionHandler.java
@@ -2,8 +2,11 @@ package com.codessquad.qna.exceptionHandler;
 
 import com.codessquad.qna.exception.QuestionNotFoundException;
 import com.codessquad.qna.exception.UserIdNotFoundException;
+import com.codessquad.qna.exception.UserNotFoundInSessionException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +18,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(QuestionNotFoundException.class)
     public String questionNotFoundException(QuestionNotFoundException ex) {
         return "/error/404";
+    }
+
+    @ExceptionHandler(UserNotFoundInSessionException.class)
+    public String userNotFoundInSessionException(UserNotFoundInSessionException ex, HttpServletResponse response) {
+        response.setStatus(HttpServletResponse.SC_FOUND);
+        return "redirect:/users/login";
     }
 }
