3d0475dcd3fbc87ae562cde567ce5bd3897d808e
gisungchoi <chlrltjd159@naver.com>
2021-03-17 15:02:16 +0900

    feat : ajax 코드 추가
    답변 등록 시 ajax로 동작하게 만들고 있다.

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,29 @@
-String.prototype.format = function() {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
-    return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-  });
-};
+$(".answer-write button[type=submit]" ).click(addAnswer);
+
+function addAnswer(e){
+    e.preventDefault();
+    console.log("click me")
+
+    var queryString = $(".answer-write").serialize();
+    console.log("query: " + queryString);
+
+    var url = $(".answer-write").attr("action");
+
+
+    $.ajax({
+
+        type : "post",
+        url : url,
+        data : queryString,
+        dataType : "json",
+        error : onError,
+        success : onSuccess
+    });
+}
+
+function onError(){
+}
+
+function onSuccess(data, status){
+
+}
