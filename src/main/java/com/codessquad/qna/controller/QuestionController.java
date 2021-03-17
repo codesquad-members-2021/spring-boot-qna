beeff765952040ae5d08a58c46eb32fa5730b98b
noel <68936@naver.com>
2021-03-18 01:15:20 +0900

    [feat] QuestionService 클래스 생성
    - 컨트롤러의 책임 분배

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -1,34 +1,27 @@
 package com.codessquad.qna.controller;
 
+import com.codessquad.qna.HttpSessionUtil;
 import com.codessquad.qna.domain.Question;
-import com.codessquad.qna.domain.User;
-import com.codessquad.qna.repository.AnswerRepository;
-import com.codessquad.qna.repository.QuestionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.codessquad.qna.service.QuestionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
-import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.util.Optional;
 
 @Controller
 public class QuestionController {
-    private final QuestionRepository questionRepository;
-    private final AnswerRepository answerRepository;
 
-    @Autowired
-    public QuestionController(QuestionRepository questionRepository, AnswerRepository answerRepository) {
-        this.questionRepository = questionRepository;
-        this.answerRepository = answerRepository;
+    private QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
     }
 
     @GetMapping("/qna/form") // 메인 메뉴에서 질문하기 버튼을 누를 경우
     private String questionButton(Model model, HttpSession session) {
-        final Object sessionValue = session.getAttribute("sessionedUser");
-        if (sessionValue != null) {
+        if (HttpSessionUtil.checkValidOf(session)) {
             return "qna/form";
         }
         // 비로그인 사용자가 질문하기 버튼에 접근할 경우
@@ -38,12 +31,10 @@ public class QuestionController {
 
     @PostMapping("/questions") // 게시글 작성하고 질문하기 버튼을 누를 경우
     private String createQuestions(Question question, Model model, HttpSession session) {
-        Objects.requireNonNull(question, "Exception: question이 NULL 값입니다.");
-        final Object sessionValue = session.getAttribute("sessionedUser");
-        if (sessionValue != null) {
-            User user = (User) sessionValue;
-            question.setWriter(user);
-            questionRepository.save(question);
+
+        if (HttpSessionUtil.checkValidOf(session)) {
+            question.setWriter(HttpSessionUtil.getLoginUserOf(session)); // 이부분은 나중에 없앨 수 있을 거 같다.
+            questionService.save(question);
             return "redirect:/";
         }
         model.addAttribute("errorMessage", "로그인 정보가 유효하지 않습니다.");
@@ -54,10 +45,7 @@ public class QuestionController {
     private String showQuestionContents(@PathVariable("id") Long targetId, Model model) {
         Objects.requireNonNull(targetId, "Exception: targetKey가 NULL 값입니다.");
 
-        Optional<Question> questionOptional = questionRepository.findById(targetId);
-        Question questionData = questionOptional.orElseThrow(NoSuchElementException::new);
-
-
+        Question questionData = questionService.findById(targetId);
         model.addAttribute("answer", questionData.getAnswers()); // 질문에 대응되는 답글만 가져온다.
         model.addAttribute("question", questionData);
         model.addAttribute("answerCount", questionData.getAnswers().size()); // 답글 수
@@ -66,19 +54,14 @@ public class QuestionController {
 
     @GetMapping("/questions/{id}/form") // 질문글에서 수정버튼을 눌렀을 때에 해당하는 맵핑
     private String modifyQuestionButton(@PathVariable("id") Long id, Model model, HttpSession session) {
-        Objects.requireNonNull(id, "Exception: id가 NULL 값입니다.");
 
-        Object sessionUser = session.getAttribute("sessionedUser");
-
-        if (sessionUser != null) {
-            User q = (User)sessionUser;
-            Question q2 = questionRepository.findById(id).orElseThrow(NoSuchElementException::new);
-
-            if(Objects.equals(q.getName(),q2.getWriter())){
-                model.addAttribute("question",q2);
+        if (HttpSessionUtil.checkValidOf(session)) {
+            if(questionService.confirmWriter(HttpSessionUtil.getLoginUserOf(session),questionService.findById(id))){
+                model.addAttribute("question",questionService.findById(id));
                 return "qna/updateForm";
             }
         }
+
         model.addAttribute("errorMessage", "로그인 정보가 유효하지 않습니다.");
         return "error/404"; //m 세션아이디와 다를 경우 이동
     }
@@ -87,10 +70,10 @@ public class QuestionController {
     @PutMapping("/questions/{id}")  // 질문 수정: 접근경로가 다른 메서드와 동일해도, 맵핑이 다르면 함수의 오버로드 같이 요청에 따라 다르게 인식하는건가?
     private String modifyQuestion(@PathVariable("id") Long id, String title, String contents) {
 
-        Question findQuestion = questionRepository.findById(id).orElseThrow(NoSuchElementException::new);
+        Question findQuestion = questionService.findById(id);
         findQuestion.setTitle(title);
         findQuestion.setContents(contents);
-        questionRepository.save(findQuestion);
+        questionService.save(findQuestion);
 
         return "redirect:/questions/{id}";
     }
@@ -98,16 +81,9 @@ public class QuestionController {
     @DeleteMapping("/questions/{id}") // 질문 삭제: 접근경로가 다른 메서드와 동일해도, 맵핑이 다르면 함수의 오버로드 같이 요청에 따라 다르게 인식하는건가?
     private String deleteQuestion(@PathVariable Long id, HttpSession session, Model model){
 
-        Objects.requireNonNull(id, "Exception: id가 NULL 값입니다.");
-
-        Object sessionUser = session.getAttribute("sessionedUser");
-
-        if (sessionUser != null) {
-            User q = (User)sessionUser;
-            Question q2 = questionRepository.findById(id).orElseThrow(NoSuchElementException::new);
-
-            if(Objects.equals(q.getName(),q2.getWriter())){
-                questionRepository.deleteById(id);
+        if (HttpSessionUtil.checkValidOf(session)) {
+            if(questionService.confirmWriter(HttpSessionUtil.getLoginUserOf(session),questionService.findById(id))){
+                questionService.deleteById(id);
                 return "redirect:/";
             }
         }
---
+++ src/main/java/com/codessquad/qna/service/QuestionService.java
@@ -0,0 +1,43 @@
+package com.codessquad.qna.service;
+
+import com.codessquad.qna.domain.Question;
+import com.codessquad.qna.domain.User;
+import com.codessquad.qna.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+/**
+ * Created by 68936@naver.com on 2021-03-17 오후 11:47
+ * Blog : https://velog.io/@san
+ * Github : https://github.com/sanhee
+ */
+
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    @Autowired
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    public void save(Question question){
+        questionRepository.save(question);
+    }
+
+    public Question findById(Long id){
+        return questionRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    public boolean confirmWriter(User loginUser, Question questionData){
+        return Objects.equals(loginUser.getName(),questionData.getWriter().getName());
+    }
+
+    public void deleteById(Long id){
+        questionRepository.deleteById(id);
+    }
+
+}
