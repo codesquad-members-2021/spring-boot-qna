c85d6bf3faa5a87b82ce9eb368a3c551a3ac1d1e
95degree <95degree@naver.com>
2021-03-18 15:39:41 +0900

    isLoginUser() 추가
    user가 로그인 session 정보를 가지고 있는지 없는지 확인하기 위해 isLoginUser()를 추가했습니다.

--- src/main/java/com/codessquad/qna/HttpSessionUtils.java
+++ src/main/java/com/codessquad/qna/HttpSessionUtils.java
@@ -1,5 +1,12 @@
 package com.codessquad.qna;
 
+import javax.servlet.http.HttpSession;
+
 public class HttpSessionUtils {
     public static final String USER_SESSION_KEY = "sessionedUser";
+
+    public static boolean isLoginUser(HttpSession session){
+        Object sessionedUser = session.getAttribute(USER_SESSION_KEY);
+        return sessionedUser != null;
+    }
 }
