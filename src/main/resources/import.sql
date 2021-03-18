81851b2faafaa5b71c643463dfd319af13684489
Sally Oh <min27604@hotmail.com>
2021-03-18 21:29:06 +0900

    feat: Create test data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('sally', '1234', '새리', 'sally@codesquad.com');
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('codesquad', '4321', '코쿼', 'code@codesquad.com');
