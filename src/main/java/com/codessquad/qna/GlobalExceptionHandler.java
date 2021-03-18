9a4a3cc92d37ccf28ba462ff5fbd9ece491b2001
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-03-18 19:42:37 +0900

    Change class name, MyAdvice to GlobalExceptionHandler

--- src/main/java/com/codessquad/qna/GlobalExceptionHandler.java
+++ src/main/java/com/codessquad/qna/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class MyAdvice {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler({NoUserException.class, NoQuestionException.class})
     public String handleNotExistException() {
