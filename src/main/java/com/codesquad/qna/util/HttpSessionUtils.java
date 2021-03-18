96642a989ec25c0b58049eac5dc14b5522505205
Polynomeer <polynomeer@gmail.com>
2021-03-19 01:42:57 +0900

    refactor: Simplify HttpSessionUtils::isLoginUser
    Simplified HttpSessionUtils::isLoginUser statements inside block.

--- src/main/java/com/codesquad/qna/util/HttpSessionUtils.java
+++ src/main/java/com/codesquad/qna/util/HttpSessionUtils.java
@@ -8,11 +8,7 @@ public class HttpSessionUtils {
     public static final String USER_SESSION_KEY = "sessionedUser";
 
     public static boolean isLoginUser(HttpSession session) {
-        Object sessionedUser = session.getAttribute(USER_SESSION_KEY);
-        if (sessionedUser == null) {
-            return false;
-        }
-        return true;
+        return session.getAttribute(USER_SESSION_KEY) != null;
     }
 
     public static User getUserFromSession(HttpSession session) {
