fee1a55e14c2a34164a696c29471e49b69e5fd73
Polynomeer <polynomeer@gmail.com>
2021-03-19 01:05:51 +0900

    chore: Remove unused code
    Removed unused codes which are commented.

--- src/main/java/com/codesquad/qna/config/MvcConfig.java
+++ src/main/java/com/codesquad/qna/config/MvcConfig.java
@@ -10,9 +10,6 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
         registry.addViewController("/users/form").setViewName("user/form");
-//        registry.addViewController("/users/login").setViewName("user/login");
-//        registry.addViewController("/questions/form").setViewName("qna/form");
     }
 }
