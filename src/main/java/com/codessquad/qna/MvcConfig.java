db70c73844ec0d9e540375503bc1e17a673da731
tmdgusya <tmdgusya@naver.com>
2021-03-18 15:28:58 +0900

    feat : Swagger2 를 통한 Api Document 생성

--- src/main/java/com/codessquad/qna/MvcConfig.java
+++ src/main/java/com/codessquad/qna/MvcConfig.java
@@ -1,19 +1,27 @@
 package com.codessquad.qna;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @ComponentScan
-public class MvcConfig extends WebMvcConfigurerAdapter {
+@EnableSwagger2
+public class MvcConfig {
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    @Bean
+    public Docket api() {
 
-        registry.addViewController("/").setViewName("/posts");
+        //connect url : http://localhost:8080/swagger-ui.html
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.codessquad.qna.controller"))
+                .paths(PathSelectors.any()).build();
+
     }
+
 }
