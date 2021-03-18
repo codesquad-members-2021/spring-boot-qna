807ef7ed3d0743befc9c5e78d99ef773d9dc16b5
bibi <bibi6666667@kakao.com>
2021-03-18 22:11:02 +0900

    DB테이블에 User클래스 맵핑
    어노테이션을 사용해 User클래스를 맵핑했습니다.

--- src/main/java/com/codessquad/qna/repository/User.java
+++ src/main/java/com/codessquad/qna/repository/User.java
@@ -1,7 +1,14 @@
 package com.codessquad.qna.repository;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable=false, length=20)
     private String userId;
     private String password;
     private String name;
@@ -19,28 +26,28 @@ public class User {
         return userId;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
     public void setUserId(String userId) {
         this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
