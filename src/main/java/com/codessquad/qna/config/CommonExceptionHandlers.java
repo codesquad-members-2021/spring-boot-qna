8313008b48eaf6a52d0c8ba040bd527fe890de4a
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-18 21:42:01 +0900

    fix: ExceptionHandler에 매개변수로 Exception 받아서 처리
    logger에 예외 종류와 예외 객체 전달하도록 수정

--- src/main/java/com/codessquad/qna/config/CommonExceptionHandlers.java
+++ src/main/java/com/codessquad/qna/config/CommonExceptionHandlers.java
@@ -2,6 +2,8 @@ package com.codessquad.qna.config;
 
 import com.codessquad.qna.exception.InsufficientAuthenticationException;
 import com.codessquad.qna.exception.LoginFailedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,21 +12,29 @@ import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class CommonExceptionHandlers {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(IllegalArgumentException.class)
-    public String illegalArgumentExceptionHandler() {
+    public String illegalArgumentExceptionHandler(IllegalArgumentException e) {
+        logger.error("IllegalArgumentException", e);
+
         return "/error/404";
     }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(LoginFailedException.class)
     public ModelAndView loginFailedExceptionHandler(LoginFailedException e) {
+        logger.error("LoginFailedException", e);
+
         return new ModelAndView("/error/401", "errMessage", e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(InsufficientAuthenticationException.class)
-    public String insufficientAuthenticationExceptionHandler() {
+    public String insufficientAuthenticationExceptionHandler(InsufficientAuthenticationException e) {
+        logger.error("InsufficientAuthenticationException", e);
+
         return "/error/403";
     }
 }
