b089d3bdf7a21dd171d4082810b87633dbccabd4
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-03-18 21:43:19 +0900

    Add question test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO USER (id, user_id, password, name, email) VALUES (1, 'javajigi', 'test', '자바지기', 'javajigi@slipp.net');
 INSERT INTO USER (id, user_id, password, name, email) VALUES (2, 'yeon', '1234', '나연', 'yeon@slipp.net');
+
+INSERT INTO QUESTION (id, writer_id, title, contents, post_time) VALUES (1, 2, '자바를 배울까요 파이썬을 배울까요', '무엇이 좋을까요', CURRENT_TIMESTAMP);
