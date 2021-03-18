04145309f31cf9faa7768fc0bd4eae2956f4599c
95degree <95degree@naver.com>
2021-03-18 16:08:53 +0900

    sessionUser name 변경
    이전에 userSession에서 sessionedUser로 name을 변경했습니다.

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
     public String login(String userId, String password, HttpSession session) {
         User user = userService.findUserByUserId(userId);
         if (user.isMatchingPassword(password)) {
-            session.setAttribute("userSession", user);
+            session.setAttribute(HttpSessionUtils.USER_SESSION_KEY, user);
             return "redirect:/";
         }
 
