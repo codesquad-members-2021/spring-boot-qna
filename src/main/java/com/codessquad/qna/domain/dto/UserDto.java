7151a44487a611cc1775f50d5c05a619e0c3a3e7
gisungchoi <chlrltjd159@naver.com>
2021-03-18 17:28:18 +0900

    feat : UserDto 정의
    api에 entiy를 그대로 보여주지 말고 dto를 반환한다.

---
+++ src/main/java/com/codessquad/qna/domain/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.codessquad.qna.domain.dto;
+
+import com.codessquad.qna.domain.User;
+
+public class UserDto {
+
+    private final Long id;
+    private final String userId;
+    private final String name;
+    private final String email;
+
+    public UserDto(User user) {
+        this.id = user.getId();
+        this.userId = user.getUserId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
+}
