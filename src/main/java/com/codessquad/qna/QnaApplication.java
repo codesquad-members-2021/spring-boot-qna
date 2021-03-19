2f1418f6ca817ae5829021766acf28295af2c6ba
gisungchoi <chlrltjd159@naver.com>
2021-03-19 11:33:43 +0900

    feat : EnableJpaAuditing 추가
    자동 설정을 위해 추가했다.

--- src/main/java/com/codessquad/qna/QnaApplication.java
+++ src/main/java/com/codessquad/qna/QnaApplication.java
@@ -2,12 +2,14 @@ package com.codessquad.qna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class QnaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QnaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QnaApplication.class, args);
+    }
 
 }
