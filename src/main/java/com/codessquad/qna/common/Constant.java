1f52afa03855d25376c29ffde1b8ac51664a83c1
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-20 01:33:28 +0900

    fix: 약어로 명명된 Const Constant로 변경
    약어 사용은 지양하도록 한다.

--- src/main/java/com/codessquad/qna/common/Constant.java
+++ src/main/java/com/codessquad/qna/common/Constant.java
@@ -2,6 +2,6 @@ package com.codessquad.qna.common;
 
 import java.time.format.DateTimeFormatter;
 
-public class Const {
+public class Constant {
     public static final DateTimeFormatter DEFAULT_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 }
