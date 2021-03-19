fde644ac8194d6c391e2b89ba97163c0a761435a
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-20 01:37:37 +0900

    refactor: 불필요한 생성자 제거
    Custom Exception을 구현한 클래스들에 정의돼있는 불필요한 생성자 삭제

--- src/main/java/com/codessquad/qna/exception/ResourceNotFoundException.java
+++ src/main/java/com/codessquad/qna/exception/ResourceNotFoundException.java
@@ -1,19 +1,7 @@
 package com.codessquad.qna.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException() {
-        super("해당 항목을 찾을 수 없습니다.");
-    }
-
     public ResourceNotFoundException(String message) {
         super(message);
-    }
-
-    public ResourceNotFoundException(Throwable e) {
-        super("해당 항목을 찾을 수 없습니다.", e);
-    }
-
-    public ResourceNotFoundException(String message, Throwable e) {
-        super(message, e);
     }
 }
