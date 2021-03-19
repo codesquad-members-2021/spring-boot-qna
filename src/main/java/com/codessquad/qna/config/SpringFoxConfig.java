4a26105bec8e2908579a4479b160015dac35742a
K <PizzaCola.K@gmail.com>
2021-03-19 22:47:03 +0900

    feat: Use swagger
    - swagger를 이용하여 API 문서화

---
+++ src/main/java/com/codessquad/qna/config/SpringFoxConfig.java
@@ -0,0 +1,20 @@
+package com.codessquad.qna.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("spring-boot-qna")
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
+
+}
