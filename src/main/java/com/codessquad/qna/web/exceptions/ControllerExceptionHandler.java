30f34ee2dc444feaa7821deb5f264a83ddd21b32
malloc72p <dase1102@gmail.com>
2021-03-19 23:45:06 +0900

    feat : 에러페이지를 구현하라
    - 400, 404, 405, 500, 그 외의 에러상황에서 쓸 수 있는 global-error페이지를 구현함
    - 에러상황 발생 시, 사이트의 디자인에 맞는 에러페이지를 보여주기 위해서 구현함
    - 에러페이지도 partial-block을 이용해서 중복코드를 최대한 제거하였다

--- src/main/java/com/codessquad/qna/web/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/codessquad/qna/web/exceptions/ControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import com.codessquad.qna.web.exceptions.users.NoLoginUserException;
 import com.codessquad.qna.web.exceptions.users.UserNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,37 +16,39 @@ public class ControllerExceptionHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(ControllerExceptionHandler.class);
 
     @ExceptionHandler(UnauthorizedAccessException.class)
-    public String handleUnauthorizedAccessException(UnauthorizedAccessException exception) {
-        logErrorState("허가되지 않은 접근이 발견됨!", exception);
-        return "redirect:/";
+    public String handleUnauthorizedAccessException(UnauthorizedAccessException exception, Model model) {
+        logErrorState("허가되지 않은 접근이 발견되었습니다!", exception, model);
+        return "/error/global-error";
     }
 
     @ExceptionHandler(NoLoginUserException.class)
-    public String handleNoLoginUserException(NoLoginUserException exception) {
-        logErrorState("현재 로그인된 상태가 아님", exception);
-        return "redirect:/";
+    public String handleNoLoginUserException(NoLoginUserException exception, Model model) {
+        logErrorState("현재 로그인된 상태가 아닙니다", exception, model);
+        return "/error/global-error";
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public String handleUserNotFoundException(UserNotFoundException exception) {
-        logErrorState("요청한 사용자 정보를 찾을 수 없음", exception);
-        return "redirect:/";
+    public String handleUserNotFoundException(UserNotFoundException exception, Model model) {
+        logErrorState("요청한 사용자 정보를 찾을 수 없습니다.", exception, model);
+        return "/error/global-error";
     }
 
     @ExceptionHandler(QuestionNotFoundException.class)
-    public String handleQuestionNotFoundException(QuestionNotFoundException exception) {
-        logErrorState("요청한 질문 정보를 찾을 수 없음", exception);
-        return "redirect:/";
+    public String handleQuestionNotFoundException(QuestionNotFoundException exception, Model model) {
+        logErrorState("요청한 질문 정보를 찾을 수 없습니다.", exception, model);
+        return "/error/global-error";
     }
 
     @ExceptionHandler(AnswerNotFoundException.class)
-    public String handleAnswerNotFoundException(AnswerNotFoundException exception) {
-        logErrorState("요청한 답변 정보를 찾을 수 없음", exception);
-        return "redirect:/";
+    public String handleAnswerNotFoundException(AnswerNotFoundException exception, Model model) {
+        logErrorState("요청한 답변 정보를 찾을 수 없습니다.", exception, model);
+        return "/error/global-error";
     }
 
-    private void logErrorState(String errorMessage, Exception exception) {
+    private void logErrorState(String errorMessage, Exception exception, Model model) {
         LOGGER.warn(errorMessage);
         exception.printStackTrace();
+        model.addAttribute("errorMessage", errorMessage);
+        model.addAttribute("extraErrorMessage", exception.getMessage());
     }
 }
