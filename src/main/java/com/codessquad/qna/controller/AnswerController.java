2366d4d38af2c0356f63d30b9a0d12619d0d72cb
noel <68936@naver.com>
2021-03-20 02:52:25 +0900

    [feat] 댓글수정하기 기능 구현

--- src/main/java/com/codessquad/qna/controller/AnswerController.java
+++ src/main/java/com/codessquad/qna/controller/AnswerController.java
@@ -1,6 +1,7 @@
 package com.codessquad.qna.controller;
 
 import com.codessquad.qna.domain.Answer;
+import com.codessquad.qna.domain.Question;
 import com.codessquad.qna.domain.User;
 import com.codessquad.qna.exception.type.NotFoundException;
 import com.codessquad.qna.exception.type.UnauthorizedException;
@@ -10,9 +11,8 @@ import com.codessquad.qna.utils.HttpSessionUtils;
 import com.codessquad.qna.utils.ValidUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.Objects;
@@ -57,6 +57,35 @@ public class AnswerController {
             throw new UnauthorizedException();
         }
         answerRepository.delete(selectedAnswer);
+        return "redirect:/questions/{question.id}";
+    }
+
+    @GetMapping("/questions/{question.id}/answers/{answerId}/update")
+    public String modifyAnswerButton(@PathVariable("question.id") Long targetId, @PathVariable Long answerId, Model model) {
+        Question questionData = questionService.findById(targetId); // findById() 내부에서 인자 예외처리
+
+        for(Answer answer : questionData.getAnswers()){
+            if (Objects.equals(answer.getAnswerId(),answerId)){
+                model.addAttribute("answer", answer); // 수정한 댓글만 가져온다.
+                model.addAttribute("question", questionData);
+                return "qna/updateShow";
+            }
+        }
+        throw new NotFoundException();
+    }
+
+    @PutMapping("/questions/{question.id}/answers/{answerId}")
+    public String updateAnswer(HttpSession session, @PathVariable("answerId") Long id, String replyContents) {
+        ValidUtils.checkIllegalArgumentOf(id);
+        HttpSessionUtils.checkValidOf(session);
+
+        User loginUser = HttpSessionUtils.getLoginUserOf(session);
+        Answer selectedAnswer = answerRepository.findById(id).orElseThrow(NotFoundException::new);
+        if (!Objects.equals(loginUser.getUserId(), selectedAnswer.getReplyId())) {
+            throw new UnauthorizedException();
+        }
+        selectedAnswer.setReplyContents(replyContents);
+        answerRepository.save(selectedAnswer);
         return "redirect:/questions/{question.id}";
     }
 
