a6d4b12ba45d6d2f903353d3f66ee47ee99b709d
jeong-inho <derosatam76@gmail.com>
2021-03-20 00:03:00 +0900

    Create UserAccountException class 유저 계정에 대해 아이디 중복, 정보 수정, 로그인 정보 불일치에 대해 처리하기 위한 UserAccountException 클래스 생성

---
+++ src/main/java/com/codessquad/qna/exception/UserAccountException.java
@@ -0,0 +1,9 @@
+package com.codessquad.qna.exception;
+
+public class UserAccountException extends RuntimeException {
+
+    public UserAccountException(String errorMessage) {
+        super(errorMessage);
+    }
+
+}
