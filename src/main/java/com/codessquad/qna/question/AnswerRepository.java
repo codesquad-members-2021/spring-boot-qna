dfeb2ebf03d437069dca313faffcb5c81f189765
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-20 00:08:22 +0900

    fix: AnswerRepository의 JPARepository사용 CrudRepository으로 변경
    특별한 이유가 없으면 CrudRepository를 사용하는게 더 좋다고 한다.

--- src/main/java/com/codessquad/qna/question/AnswerRepository.java
+++ src/main/java/com/codessquad/qna/question/AnswerRepository.java
@@ -1,6 +1,6 @@
 package com.codessquad.qna.question;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
+public interface AnswerRepository extends CrudRepository<Answer, Long> {
 }
