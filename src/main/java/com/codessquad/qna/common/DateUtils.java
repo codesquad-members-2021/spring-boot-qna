5e23d32e1841d1432d417f2b611202615aaacaff
고정완 (Can) <jw.gho@ecubelabs.com>
2021-03-19 14:34:20 +0900

    [refactor] DateTimeFormatter 를 상수로 추출

--- src/main/java/com/codessquad/qna/common/DateUtils.java
+++ src/main/java/com/codessquad/qna/common/DateUtils.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm");
+
     private DateUtils() {}
 
     public static String format(LocalDateTime localDateTime) {
-        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm"));
+        return localDateTime.format(formatter);
     }
 }
