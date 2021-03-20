a3f7f78d2077bebf44c39cb133598a880ad50fa9
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-20 11:52:16 +0900

    style : 홈컨트롤러의 메서드도 기능에 맞게 이름을 고쳐라
    - onRootRequest보다는 홈경로 요청을 처리한다는 의미로 이름을 수정했다.

--- src/main/java/com/codessquad/qna/web/controller/HomeController.java
+++ src/main/java/com/codessquad/qna/web/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @GetMapping
-    public String onRootRequest() {
+    public String processHomePathRequest() {
         return "redirect:/questions";
     }
 }
