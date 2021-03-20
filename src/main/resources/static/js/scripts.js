9894af8872438773130bdb0cde32d3ef5474154c
jeong-inho <derosatam76@gmail.com>
2021-03-20 16:02:25 +0900

    Create event in scripts.js 답변하기 버튼 클릭 시 form 태그에 의한 요청이 아닌 이벤트를 걸어 post 요청을 할 수 있도록 이벤트 생성

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,5 +1,5 @@
 String.prototype.format = function() {
-  var args = arguments;
+  let args = arguments;
   return this.replace(/{(\d+)}/g, function(match, number) {
     return typeof args[number] != 'undefined'
         ? args[number]
@@ -7,3 +7,52 @@ String.prototype.format = function() {
         ;
   });
 };
+
+const redirectUrl = {
+  LOGINPAGE : "http://localhost:8080/user/login"
+}
+
+const errorMessage = {
+  NEEDLOGIN : "로그인이 필요한 서비스입니다."
+}
+
+const request = {
+  post : function post(url, body) {
+    return fetch(url, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: body
+    })
+  }
+};
+
+function processResponse(errorMessage, url) {
+  return response => {
+    if (response.status === 200) {
+      return response.json();
+    }
+    alert(errorMessage);
+    window.location.href = url;
+  }
+}
+
+function addAnswer(e) {
+  e.preventDefault();
+  let url = document.querySelector('form.answer-write').action;
+  let body = 'contents=' + document.querySelector('form.answer-write textarea').value;
+  request.post(url, body)
+      .then(processResponse(errorMessage.NEEDLOGIN, redirectUrl.LOGINPAGE))
+      .then(answer => {
+        let answerTemplate = document.querySelector('#answerTemplate').innerHTML;
+        let template = answerTemplate.format(answer.writer.userId, answer.date, answer.contents);
+        document.querySelector('.qna-comment-slipp-articles').insertAdjacentHTML('beforebegin', template);
+        document.querySelector('form.answer-write textarea').value = '';
+      })
+}
+
+let answerWrite = document.querySelector('form.answer-write button[type=submit]');
+if (answerWrite !== null) {
+  answerWrite.addEventListener('click', e => addAnswer(e));
+}
