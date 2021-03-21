34c9f84725566db738f456b1ef6a70331028c6b6
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-03-21 21:23:13 +0900

    포맷팅 작업 진행

--- src/main/java/com/codessquad/qna/exception/AccessDeniedException.java
+++ src/main/java/com/codessquad/qna/exception/AccessDeniedException.java
@@ -1,6 +1,6 @@
 package com.codessquad.qna.exception;
 
-public class AccessDeniedException extends RuntimeException{
+public class AccessDeniedException extends RuntimeException {
     public AccessDeniedException() {
         super("수정할 수 있는 권한이 없습니다.");
     }
