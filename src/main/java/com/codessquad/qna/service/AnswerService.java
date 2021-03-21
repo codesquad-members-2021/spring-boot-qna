94e3096cfa0b8a00c895bc67ae4732b685f55a15
gisungchoi <chlrltjd159@naver.com>
2021-03-21 18:01:09 +0900

    refactor : AnswerService 의 메소드들의 반환 값을 Dto로 변환
    new Answer할 때 session에서 얻은 userDto를 Entity로 변환해 사용한다.

--- src/main/java/com/codessquad/qna/service/AnswerService.java
+++ src/main/java/com/codessquad/qna/service/AnswerService.java
@@ -3,12 +3,11 @@ package com.codessquad.qna.service;
 import com.codessquad.qna.domain.Answer;
 import com.codessquad.qna.domain.DisplayStatus;
 import com.codessquad.qna.domain.Question;
-import com.codessquad.qna.domain.User;
 import com.codessquad.qna.domain.dto.AnswerDto;
+import com.codessquad.qna.domain.dto.UserDto;
 import com.codessquad.qna.exception.NoSearchObjectException;
 import com.codessquad.qna.repository.AnswerRepository;
 import com.codessquad.qna.repository.QuestionRepository;
-import com.codessquad.qna.valid.UserValidator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,21 +25,24 @@ public class AnswerService {
     }
 
     @Transactional
-    public AnswerDto write(User writer, String contents, Long questionId) {
-        UserValidator.validate(writer);
+    public AnswerDto write(UserDto writer, String contents, Long questionId) {
         Question question = questionRepository.findById(questionId).orElseThrow(() -> new NoSearchObjectException("질문"));
-        Answer answer = new Answer(writer, question, contents);
+        Answer answer = new Answer(writer.returnEntity(), question, contents);
         return AnswerDto.createDto(answerRepository.save(answer));
     }
 
-    public Answer findById(Long answerId) {
+    private Answer findById(Long answerId) {
         return answerRepository.findById(answerId).orElseThrow(() -> new NoSearchObjectException("답변"));
     }
 
+    public AnswerDto findByIdToDto(Long answerId) {
+        return AnswerDto.createDto(findById(answerId));
+    }
+
     @Transactional
-    public Long delete(Long id, User user) {
+    public Long delete(Long id, UserDto user) {
         Answer answer = findById(id);
-        user.checkSameUser(answer.getWriter().getId());
+        answer.checkSameUser(user.getId());
         answer.deleteAnswer(DisplayStatus.CLOSE);
         return id;
     }
