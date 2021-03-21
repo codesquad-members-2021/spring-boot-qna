b2ab6538166f6058e839a900bb7d23ccce90342f
고정완 (Can) <jw.gho@ecubelabs.com>
2021-03-21 10:21:33 +0900

    [feat] AJAX를 활용해 답변 삭제 기능 구현
    https://youtu.be/xKjEmoUYNC8

--- src/main/resources/static/js/question-show.js
+++ src/main/resources/static/js/question-show.js
@@ -10,15 +10,16 @@ function addAnswer(e) {
         url: url,
         data: queryString,
         dataType: 'json',
-        error: onError,
-        success: onSuccess
+        error: onAddAnswerError,
+        success: onAddAnswerSuccess
     });
 }
 
-function onError() {
+function onAddAnswerError(xhr, status) {
+    alert(JSON.stringify(xhr));
 }
 
-function onSuccess(data, status) {
+function onAddAnswerSuccess(data, status) {
     var answerTemplate = $("#answerTemplate").html();
     var template = answerTemplate.format(
         data.writer.userId,
@@ -29,4 +30,26 @@ function onSuccess(data, status) {
     );
     $(".qna-comment-slipp-articles").prepend(template);
     $(".answer-write textarea").val("");
+}
+
+$(document).on("click", ".answer-delete", removeAnswer);
+
+function removeAnswer(e) {
+    e.preventDefault();
+
+    var url = $(this).attr("href");
+    var deleteComponent = $(this);
+    $.ajax({
+        type: 'delete',
+        url: url,
+        dataType: 'json',
+        error: onRemoveAnswerError,
+        success: function (data, status) {
+            deleteComponent.closest(".article").remove();
+        }
+    });
+}
+
+function onRemoveAnswerError(xhr, status) {
+    alert("자신의 글만 삭제할 수 있습니다.");
 }
