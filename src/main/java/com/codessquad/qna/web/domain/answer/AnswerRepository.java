b8b0be96b5b3f9230c11840c1b9169cf66291320
Jihye Woo <woojihye2339@gmail.com>
2021-03-21 18:43:47 +0900

    feat : soft-delete, Repository 클래스 조회 기능에 적용
      > SELECT 기능
       - @Query를 적용해서 isActive=true인 엔티티만 조회가 가능하도록 설정

--- src/main/java/com/codessquad/qna/web/domain/answer/AnswerRepository.java
+++ src/main/java/com/codessquad/qna/web/domain/answer/AnswerRepository.java
@@ -1,9 +1,21 @@
 package com.codessquad.qna.web.domain.answer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+
+    @Override
+    @Query("SELECT a FROM #{#entityName} a WHERE a.isActive=true")
+    List<Answer> findAll();
+
+    @Query("SELECT a FROM #{#entityName} a WHERE a.isActive=true AND a.id=?1")
+    Optional<Answer> findById(long id);
+
+    @Query("SELECT a FROM #{#entityName} a WHERE a.isActive=true AND a.question.id=?1")
     List<Answer> findByQuestionId(Long questionId);
 }
--- src/main/java/com/codessquad/qna/web/domain/question/QuestionRepository.java
+++ src/main/java/com/codessquad/qna/web/domain/question/QuestionRepository.java
@@ -1,6 +1,19 @@
 package com.codessquad.qna.web.domain.question;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    @Override
+    @Query("SELECT q FROM #{#entityName} q WHERE q.isActive=true")
+    List<Question> findAll();
+
+    @Query("SELECT q FROM #{#entityName} q WHERE q.isActive=true AND q.id=?1")
+    Optional<Question> findById(long id);
+
 
 public interface QuestionRepository extends CrudRepository<Question, Long> {
 }
