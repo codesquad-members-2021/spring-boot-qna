ce1ce71f4c17c585da0c48561cf8fadf01c2830b
MJbae <gently.gogo@gmail.com>
2021-03-21 20:40:40 +0900

    Chore: push pull 과정 중 발생한 conflict 해결

--- src/main/java/com/codessquad/qna/repository/AnswerRepository.java
+++ src/main/java/com/codessquad/qna/repository/AnswerRepository.java
@@ -7,17 +7,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AnswerRepository extends CrudRepository<Answer, Long> {
-<<<<<<< HEAD
-    List<Answer> findAllByAndDeletedFalse();
-
-    Optional<Answer> findByAnswerIdAndDeletedFalse(Long id);
-=======
 
     List<Answer> findAllByAndDeletedFalse();
 
     Optional<Answer> findByAnswerIdAndDeletedFalse(Long id);
 
-    @Override
-    List<Answer> findAll();
->>>>>>> 9055b4202d9c420f7c0f8159deecd4735089e8e3
 }
--- src/main/java/com/codessquad/qna/repository/QuestionRepository.java
+++ src/main/java/com/codessquad/qna/repository/QuestionRepository.java
@@ -7,18 +7,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface QuestionRepository extends CrudRepository<Question, Long> {
-<<<<<<< HEAD
-    List<Question> findAllByAndDeletedFalse();
-
-=======
-    @Override
-    List<Question> findAll();
-
-    @Override
-    void deleteById(Long id);
 
     List<Question> findAllByAndDeletedFalse();
 
->>>>>>> 9055b4202d9c420f7c0f8159deecd4735089e8e3
     Optional<Question> findByQuestionIdAndDeletedFalse(Long id);
 }
