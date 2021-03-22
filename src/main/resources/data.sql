494a896f359ae28919c34a3721cee170d5772531
bong6981 <kbk5190@naver.com>
2021-03-22 19:04:18 +0900

    feat : Add test data in data.sql
    - 질문 테스트 데이터 추가
    - 기존에 User 테스트 데이터에 추가해주지 않았던 id부분을 직접 지정하여
      Question 테스트 데이터에서 해당 User의 id를 정확하게 사용할 수 있게함

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
-INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('javajigi', 'test', '자바지기', 'javajigi@slipp.net')
-INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('sanjigi', 'test', '산지기', 'sanjigi@slipp.net')
-INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('hi', 'hi', 'hi', 'hi@slipp.net')
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES ('1', 'javajigi', 'test', '자바지기', 'javajigi@slipp.net')
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES ('2', 'sanjigi', 'test', '산지기', 'sanjigi@slipp.net')
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES ('3', 'hi', 'hi', 'hi', 'hi@slipp.net')
+
+INSERT INTO QUESTION (ID, WRITER_ID, TITLE, CONTENTS, CREATED_DATE_TIME) VALUES ('1', '3', 'hello', 'bye', '2021-03-22 17:03:01.106461')
+INSERT INTO QUESTION (ID, WRITER_ID, TITLE, CONTENTS, CREATED_DATE_TIME) VALUES ('2', '1', 'hoi', 'dui', '2021-03-20 16:03:03.106461')
