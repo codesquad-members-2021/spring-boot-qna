6712f0df12a10d42bda32b7dc9cfc926493a0b6d
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-03-22 17:10:16 +0900

    Service에서 List를 반환하기 위하여 CrudRepository 에서 JpaRepository로 변경

--- src/main/java/com/codessquad/qna/repository/UserRepository.java
+++ src/main/java/com/codessquad/qna/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package com.codessquad.qna.repository;
 
 import com.codessquad.qna.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserId(String userId);
 }
---
+++ src/main/java/com/codessquad/qna/service/UserService.java
@@ -0,0 +1,57 @@
+package com.codessquad.qna;
+
+import com.codessquad.qna.domain.User;
+import com.codessquad.qna.exception.NoUserException;
+import com.codessquad.qna.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean checkEmpty(User user) {
+        return user.getUserId().equals("")
+                || user.getPassword().equals("")
+                || user.getEmail().equals("")
+                || user.getName().equals("");
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public List<User> getUserList() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(long id) {
+        return userRepository.findById(id).orElseThrow(NoUserException::new);
+    }
+
+    public User getUserByUserId(String userId) {
+        return userRepository.findByUserId(userId);
+    }
+
+    public void updateUser(long id, User updateUser, String newPassword) {
+        User user = getUserById(id);
+        checkPassword(user, updateUser);
+
+        user.update(updateUser, newPassword);
+
+        save(user);
+    }
+
+    private void checkPassword(User originalUser, User updateUser) {
+        if (!originalUser.checkPassword(updateUser.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 맞지않습니다. 비밀번호를 확인해 주세요");
+        }
+    }
+
+}
