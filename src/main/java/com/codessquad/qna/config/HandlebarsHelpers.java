60092ea93974578bb9182e62daa83c1d9fdd6012
gisungchoi <chlrltjd159@naver.com>
2021-03-23 00:30:55 +0900

    feat : 페이징을 위한 helper 작성
    처음인지 확인하면 이전 버튼을 보여주지 않습니다.
    마지막인지 확인하면 다음 버튼을 보여주지 않습니다.
    밑의 5칸씩 보여주기 위해 메소드를 작성했습니다.

--- src/main/java/com/codessquad/qna/config/HandlebarsHelpers.java
+++ src/main/java/com/codessquad/qna/config/HandlebarsHelpers.java
@@ -1,11 +1,39 @@
 package com.codessquad.qna.config;
 
 import com.codessquad.qna.domain.DisplayStatus;
+import com.codessquad.qna.domain.Question;
+import org.springframework.data.domain.Page;
 import pl.allegro.tech.boot.autoconfigure.handlebars.HandlebarsHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @HandlebarsHelper
 public class HandlebarsHelpers {
-    public boolean checkStatus(final Object obj1){
-        return obj1 == DisplayStatus.OPEN;
+    public boolean checkStatus(final Object post) {
+        return post == DisplayStatus.OPEN;
     }
+
+    public boolean checkPageFirst(final Page<Question> page) {
+        return !page.isFirst();
+    }
+
+    public boolean checkPageLast(final Page<Question> page) {
+        return !page.isLast();
+    }
+
+    public List<Integer> checkPageNum(final Page<Question> page) {
+        List<Integer> pageNum = new ArrayList<>();
+        int count = 0;
+        int getPage = page.getNumber() == 0 ? 1 : page.getNumber();
+        for (int i = getPage; i <= page.getTotalPages(); i++) {
+            pageNum.add(i);
+            count++;
+            if (count == 5) {
+                break;
+            }
+        }
+        return pageNum;
+    }
+
 }
