b9bcbf827a8c5b7c94c8942224229ed8777a11a5
malloc72p <dase1102@gmail.com>
2021-03-22 23:19:31 +0900

    refactor : 좀 더 적절한 메서드명을 부여하라
    - 메서드를 호출한다는건 결국 객체에게 어떤 것을 요청함을 의미한다
    - 따라서 메서드명으로 request가 붙는건 그다지 좋은 선택이 아닌 것 같다
    - HomeController 어떤 것을 바라고 요청하는가? 라고 스스로에게 질문했다
    - home페이지 줘! 라고 요청한다고 생각했다.
    - 그러니 requestHome정도가 적당하겠다.
    - 근데 request는 생략할 수 있으므로, home이 적당한 것 같아서 이 이름을 선택했다

--- src/main/java/com/codessquad/qna/web/controller/HomeController.java
+++ src/main/java/com/codessquad/qna/web/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @GetMapping
-    public String processHomePathRequest() {
+    public String home() {
         return "redirect:/questions";
     }
 }
