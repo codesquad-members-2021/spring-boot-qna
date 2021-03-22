8244eb7c861728a539fa1b86ab1911ad744d9894
dong <ultraraptor@naver.com>
2021-03-22 15:27:21 +0900

    find by id 추가하기

--- src/main/java/com/codessquad/qna/domain/UserRepository.java
+++ src/main/java/com/codessquad/qna/domain/UserRepository.java
@@ -1,6 +1,10 @@
 package com.codessquad.qna.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserId(String userId);
+    //리턴타입 1 user
+    //리턴타입 2 optional
 }
