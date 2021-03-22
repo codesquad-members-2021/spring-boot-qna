3ad3214f4d77963de9a943cd36c4b9ca3297de9b
gisungchoi <chlrltjd159@naver.com>
2021-03-23 00:32:10 +0900

    refactor : 메인 페이지에 page 표시로 변경
    가져온 question들만 보여줍니다.

--- src/main/java/com/codessquad/qna/controller/HomeController.java
+++ src/main/java/com/codessquad/qna/controller/HomeController.java
@@ -1,27 +1,30 @@
 package com.codessquad.qna.controller;
 
 
-import com.codessquad.qna.domain.Question;
 import com.codessquad.qna.repository.QuestionRepository;
+import com.codessquad.qna.service.QuestionService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class HomeController {
 
     private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
-    public HomeController(QuestionRepository questionRepository) {
+    public HomeController(QuestionRepository questionRepository, QuestionService questionService) {
         this.questionRepository = questionRepository;
+        this.questionService = questionService;
     }
 
     @GetMapping("/")
-    public String welcome(Model model) {
-        List<Question> getQuestions = questionRepository.findAll();
-        model.addAttribute("Questions", getQuestions);
+    public String welcome(@PageableDefault(size = 15) Pageable pageable, Model model) {
+        model.addAttribute("Questions", questionService.findQuestionList(pageable));
+        model.addAttribute("next", pageable.next().getPageNumber());
+        model.addAttribute("pre", pageable.next().getPageNumber() - 2);
         return "index";
     }
 
