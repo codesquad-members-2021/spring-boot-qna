dfb7983203ada3c3154aea7b97a6eca62546e4f4
dong <ultraraptor@naver.com>
2021-03-23 17:15:31 +0900

    리퀘스트 맵핑 추가

--- src/main/java/com/codessquad/qna/Controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/Controller/QuestionController.java
@@ -12,11 +12,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import javax.servlet.http.HttpSession;
 
 @Controller
+
+@RequestMapping("/qna")
 public class QuestionController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
