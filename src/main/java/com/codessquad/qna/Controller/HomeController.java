db27a468a164faa6b777892302e5c04f70378429
dong <ultraraptor@naver.com>
2021-03-23 17:07:15 +0900

    첫 화면은 질문 리스트를 확인하는 페이지

--- src/main/java/com/codessquad/qna/Controller/HomeController.java
+++ src/main/java/com/codessquad/qna/Controller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String Home() {
-        return "/index";
+    public String home() {
+        return "redirect:/qna/list";
     }
 
 }
