be5abdc0bce666627d28c67e83f6acf614db9fd5
dong <ultraraptor@naver.com>
2021-03-23 12:10:03 +0900

    테스트를 위한 데이터 자동 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-INSERT INTO USER (id, user_id, password, name, email) VALUES (1, 'javajigi', 'tester', '자바지기', 'javajigi@slipp.net');
-INSERT INTO USER (id, user_id, password, name, email) VALUES (2, 'sanjigi', 'devops', '산삼을찾아', 'sanjigi@slipp.net');
-INSERT INTO USER (id, user_id, password, name, email) VALUES (3, 'dormamu', 'villrun', '돌마무', 'dormamamu@mcu.com');
-INSERT INTO USER (id, user_id, pass서word, name, email) VALUES (4, 'muyaho', 'grandfa', '알래스카무야호', 'MBC@muhandozen.co.kr');
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('javajigi', 'tester', '자바지기', 'javajigi@slipp.net');
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('sanjigi', 'devops', '산삼을찾아', 'sanjigi@slipp.net');
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('dormamu', 'villrun', '돌마무', 'dormamamu@mcu.com');
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('muyaho', 'grandfa', '알래스카무야호', 'MBC@muhandozen.co.kr');
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('dev', '1', 'devtesters', 'deee@eeeev.com');
