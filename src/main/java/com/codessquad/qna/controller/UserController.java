55777a8ef7b80a89ff1cfa898b05909c8dc8bffa
Tree <gmldbs1109@naver.com>
2021-03-24 03:29:17 +0900

    로그아웃 기능 구현

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -86,6 +86,10 @@ public class UserController {
         return "redirect:/";
     }
 
-
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("user");
+        return "redirect:/";
+    }
 
 }
