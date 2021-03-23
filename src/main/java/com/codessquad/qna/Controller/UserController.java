b57c4b47542fbe49341914696a5c956e096e0cee
dong <ultraraptor@naver.com>
2021-03-23 10:55:05 +0900

    SESSION_KEY_LOGIN_USER 문자열 사용을 위해 import static 추가함

--- src/main/java/com/codessquad/qna/Controller/UserController.java
+++ src/main/java/com/codessquad/qna/Controller/UserController.java
@@ -1,5 +1,5 @@
 package com.codessquad.qna.Controller;
-
+import static com.codessquad.qna.utils.SessionUtil.*;
 import com.codessquad.qna.domain.User;
 import com.codessquad.qna.domain.UserRepository;
 import org.slf4j.Logger;
@@ -19,7 +19,6 @@ public class UserController {
 
     private final UserRepository userRepository;
 
-    private final static String SESSION_KEY_USER_OBJECT = "loginUser";
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -64,19 +63,27 @@ public class UserController {
         return "user/updateForm";
     }
 
+    @GetMapping("/login")
+    public String loginForm() {
+        return "/user/login";
+    }
+
     @PostMapping("/login")
-    public String loginProcess(String userId, String passWord, HttpSession session) {
+    public String loginProcess(String userId, String password, HttpSession session) {
         User foundUser = userRepository.findByUserId(userId);
+
         if(foundUser == null) {//중복 코드가 심해진다.
+            logger.info("Login Failure");
             return "redirect:/user/form";
         }
 
-        if(!foundUser.isMatchingPassword(passWord)) {
+        if(!foundUser.isMatchingPassword(password)) {
+            logger.info("Login Failure");
             return "redirect:/user/form";
         }
+        logger.info("Login Success");
+        session.setAttribute(SESSION_KEY_LOGIN_USER,foundUser);
 
-        session.setAttribute(SESSION_KEY_USER_OBJECT,foundUser);
-
-        return  "redirect:/";
+        return  "redirect:/";//로그인 끝나면 메인페이지로 이
     }
 }
