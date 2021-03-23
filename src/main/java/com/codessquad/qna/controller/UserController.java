b65b1e534464028b22bf4b0a97d1f08bd44b19c3
hiro <kiryuinh32@gmail.com>
2021-03-23 15:14:47 +0900

    update시 HTTP메서드 수정

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -5,10 +5,7 @@ import com.codessquad.qna.domain.User;
 import com.codessquad.qna.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -118,7 +115,7 @@ public class UserController {
         return "/user/validationFail";
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public String update(@PathVariable Long id, User newUser, HttpSession session) throws Exception {
 
         User sessionedUser = (User)session.getAttribute("sessionedUser");
