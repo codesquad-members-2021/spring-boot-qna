209006e60e6a9aa8f864b4f03b89b65c5e9fb0bf
dong <ultraraptor@naver.com>
2021-03-24 18:19:46 +0900

    세션키 생성 알고리즘 삭제
    - 기존에 md-5 로 매번 키를 생성하던 방식은 로그인되지 않는 문제가 있었습니다.
    - 또한 클라이언트에게 보내주는 세션키는 tomcat에 내장된 암호화기에서 처리해주기 때문에 크게 의미없는거같아 삭제

--- src/main/java/com/codessquad/qna/utils/SessionUtil.java
+++ src/main/java/com/codessquad/qna/utils/SessionUtil.java
@@ -4,32 +4,17 @@ import com.codessquad.qna.domain.User;
 import com.codessquad.qna.exception.NotFoundException;
 
 import javax.servlet.http.HttpSession;
-import java.security.MessageDigest;
+
 
 public class SessionUtil {
 
-    private static String SESSION_KEY_LOGIN_USER;
+    private static final String SESSION_KEY_LOGIN_USER = "loginUser";
 
     private SessionUtil() {
-
     }
 
-    private static String generateSessionKey(User user) throws Exception {
-        StringBuffer stringBuffer = new StringBuffer();
-        MessageDigest mDigest = MessageDigest.getInstance("MD5");
-
-        mDigest.update((user.getUserId() + user.getId()).getBytes());
-        byte[] msgStr = mDigest.digest();
-
-        for (int i = 0; i < msgStr.length; i++) {
-            String tmpEncTxt = Integer.toHexString((int) msgStr[i] & 0x00ff);
-            stringBuffer.append(tmpEncTxt);
-        }
-        return stringBuffer.toString();
-    }
-
-    public static boolean isValidUser(HttpSession session, User OwnerUser) {
-        return OwnerUser.equals(session.getAttribute(SESSION_KEY_LOGIN_USER));
+    public static boolean isValidUser(HttpSession session, User ownerUser) {
+        return ownerUser.equals(session.getAttribute(SESSION_KEY_LOGIN_USER));
     }
 
     public static boolean isLoginUser(HttpSession session) {
@@ -45,7 +30,6 @@ public class SessionUtil {
     }
 
     public static void setLoginUser(HttpSession session, User user) {
-        SESSION_KEY_LOGIN_USER = "loginUser";
         session.setAttribute(SESSION_KEY_LOGIN_USER, user);
     }
 
