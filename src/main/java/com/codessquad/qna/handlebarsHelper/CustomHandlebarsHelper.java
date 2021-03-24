6383b975f93e2a356de17fc80a800f5bf60e6f7d
K <PizzaCola.K@gmail.com>
2021-03-24 17:23:49 +0900

    style: Change class name

--- src/main/java/com/codessquad/qna/handlebarsHelper/CustomHandlebarsHelper.java
+++ src/main/java/com/codessquad/qna/handlebarsHelper/CustomHandlebarsHelper.java
@@ -1,7 +1,6 @@
 package com.codessquad.qna.handlebarsHelper;
 
 import com.codessquad.qna.domain.Question;
-import com.github.jknack.handlebars.Options;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -11,9 +10,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @HandlebarsHelper
-public class handlebarsHelper {
+public class CustomHandlebarsHelper {
 
-    private final Logger logger = LoggerFactory.getLogger(handlebarsHelper.class);
+    private final Logger logger = LoggerFactory.getLogger(CustomHandlebarsHelper.class);
 
     private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
@@ -21,13 +20,13 @@ public class handlebarsHelper {
         return time.format(dateTimeFormatter);
     }
 
-    public CharSequence questionPagination(Page<Question> questions, int pageNumber, Options options) {
+    public CharSequence questionPagination(Page<Question> questions, int pageNumber) {
         logger.debug("pnum : " + pageNumber);
         logger.debug("total pnum : " + questions.getTotalPages());
         int PAGE = 5;
         int pageIndex = pageNumber - 1;
         int pageStart = (pageIndex / PAGE) * PAGE;
-        int pageEnd   = ((pageIndex / PAGE) + 1) * PAGE;
+        int pageEnd = ((pageIndex / PAGE) + 1) * PAGE;
         boolean nextPages = true;
         if (pageEnd > questions.getTotalPages() - 1) {
             pageEnd = pageIndex + 1;
@@ -36,21 +35,21 @@ public class handlebarsHelper {
         StringBuilder pagination = new StringBuilder();
         if (pageStart != 0) {
             pagination.append("<li><a href=\"/questions?page=")
-                      .append(pageStart)
-                      .append("\">«</a></li>");
+                    .append(pageStart)
+                    .append("\">«</a></li>");
         }
         for (int i = pageStart; i < pageEnd; i++) {
             int pageNum = i + 1;
             pagination.append("<li><a href=\"/questions?page=")
-                      .append(pageNum)
-                      .append("\">")
-                      .append(pageNum)
-                      .append("</a></li>");
+                    .append(pageNum)
+                    .append("\">")
+                    .append(pageNum)
+                    .append("</a></li>");
         }
         if (nextPages) {
             pagination.append("<li><a href=\"/questions?page=")
-                      .append(pageEnd + 1)
-                      .append("\">»</a></li>");
+                    .append(pageEnd + 1)
+                    .append("\">»</a></li>");
         }
         logger.debug(pagination.toString());
         return pagination.toString();
