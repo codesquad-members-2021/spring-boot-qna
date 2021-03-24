db8943eb7187ab8f7e0491f198ed368893cbe436
Tree <gmldbs1109@naver.com>
2021-03-24 10:39:14 +0900

    샘플데이터 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO USER(USER_ID, PASSWORD, NAME, EMAIL) VALUES ('TREE', '1234', '희윤', 'gmldbs1109@naver.com');
-INSERT INTO USER (user_id, password, name, email) VALUES ('javajigi', 'test', '자바지기', 'javajigi@slipp.net');
-INSERT INTO USER (user_id, password, name, email) VALUES ('sanjigi', 'test', '산지기', 'sanjigi@slipp.net');
+INSERT INTO USER(ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (1, 'tree', '1234', '희윤', 'gmldbs1109@naver.com');
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (2, 'javajigi', 'test', '자바지기', 'javajigi@slipp.net');
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (3, 'sanjigi', 'test', '산지기', 'sanjigi@slipp.net');
