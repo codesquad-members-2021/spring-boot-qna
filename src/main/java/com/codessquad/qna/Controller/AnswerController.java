78becbf0a256e7e66c560e74a80f5f467845476f
dong <ultraraptor@naver.com>
2021-03-24 17:38:13 +0900

    Answer 기능 구현 - 삭제 기능 구현하기
    - answer / answers 용어 통일 >> answers로
    - 실수로 지워졌던 400에러 페이지를 수정함
    - AnswerController에 Delete 기능 생성함

--- src/main/java/com/codessquad/qna/Controller/AnswerController.java
+++ src/main/java/com/codessquad/qna/Controller/AnswerController.java
@@ -4,18 +4,19 @@ import com.codessquad.qna.domain.Answer;
 import com.codessquad.qna.domain.AnswerRepository;
 import com.codessquad.qna.domain.QuestionRepostory;
 import com.codessquad.qna.domain.User;
+import com.codessquad.qna.exception.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 
-import static com.codessquad.qna.utils.SessionUtil.getLoginUser;
-import static com.codessquad.qna.utils.SessionUtil.isLoginUser;
+import static com.codessquad.qna.utils.SessionUtil.*;
 
 @Controller
 //@RequestMapping("/questions/{questionId}/answers") //대표 url을 이렇게 설정한 이유는 >> 앤서는 퀘스천에 종속적이기 때문
@@ -28,7 +29,7 @@ public class AnswerController {
     @Autowired
     private AnswerRepository answerRepository; //@@안티패턴
 
-    @PostMapping("/qna/{questionId}/answer") // 500에러 발생 :
+    @PostMapping("/qna/{questionId}/answers") // 500에러 발생 :
     public String createAnswer(@PathVariable Long questionId, String contents, HttpSession session) {
         if(!isLoginUser(session)) {
             logger.info("답변달기 - 실패 : 권한(로그인)되지 않은 사용자의 답변달기 시도가 실패함");
@@ -41,7 +42,15 @@ public class AnswerController {
         return String.format("redirect:/qna/%d",questionId);
     }
 
+    @DeleteMapping("/qna/{questionId}/answers/{answerId}")
+    public String removeAnswer(@PathVariable Long questionId, @PathVariable Long answerId,HttpSession session) {
+        User ownerUser = questionRepostory.findById(questionId).orElseThrow(NotFoundException::new).getWriter();
+        if(! isValidUser(session,ownerUser)) {
+            logger.info("답변달기 - 실패 : 권한(로그인)되지 않은 사용자의 답변달기 시도가 실패함");
+            return String.format("redirect:/qna/%d",questionId);
+        }
+        answerRepository.delete(answerRepository.findById(answerId).orElseThrow(NotFoundException::new));
+        return String.format("redirect:/qna/%d",questionId);
+    }
 
-    //@PostMapping("/qna/{questionId}/answers/{id}")
-    //public String remove
 }
