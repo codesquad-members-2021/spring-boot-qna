14c42d32511b741806c2bcac999cf637c405a2e9
dong <ultraraptor@naver.com>
2021-03-24 23:05:38 +0900

    쿼리문으로 인한 스프링프로젝트 빌드에러
    - 데이터베이스에 테스트용 ANSWER 를 추가했는데, 스프링이 정상적으로 실행되지 않아 주석처리, SQL을 조금더 학습해 문제 해결 예정
    - 해당 기능은 테스트 편의를 위한 기능이므로, 낮은 우선순위의 업무로 처리하겠습니다.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,20 @@
+
 INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('javajigi', 'tester', '자바지기', 'javajigi@slipp.net');
 INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('sanjigi', 'devops', '산삼을찾아', 'sanjigi@slipp.net');
 INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('dormamu', 'villrun', '돌마무', 'dormamamu@mcu.com');
 INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('muyaho', 'grandfa', '알래스카무야호', 'MBC@muhandozen.co.kr');
 INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('dev', '1', 'devtesters', 'deee@eeeev.com');
+
+
 INSERT INTO QUESTION (ID, WRITER_ID, TITLE, CONTENTS, CREATED_DATE_TIME)
 VALUES (null,1, '자바스크립트 함 배워보쉴?', '자바스크립..',{ts '2021-03-05 18:23:32'});
 
 INSERT INTO QUESTION (ID, WRITER_ID, TITLE, CONTENTS, CREATED_DATE_TIME)
 VALUES (null,5, '오늘저녁은 치킨이닭', '베그왕김베그..',{ts '2021-03-19 11:11:11'});
+
+
+--INSERT INTO ANSWER (ID, CONTENTS, CREATED_DATE_TIME, QUESTION_ID, WRITER_ID)
+--VALUES (null ,'양념치킨을 고려해보면 어떨까 달콤하고',{ts '2021-03-24 12:34:56'},2,5)
+
+--INSERT INTO ANSWER (ID, CONTENTS, CREATED_DATE_TIME, QUESTION_ID, WRITER_ID)
+--VALUES (null ,'그돈이면 차라리 따끈하고 뜨끈한 국밥이 네그릇인데',{ts '2021-03-24 12:34:56'},2,5)
