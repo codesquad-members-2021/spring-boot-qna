63e40dfc25c1c7f4afd315565fc1f45d254c4747
정이삭 <isaac56@naver.com>
2021-03-25 16:37:00 +0900

    Implements adding answer with ajax
    ajax로 답변 추가하기 구현

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,43 @@
-String.prototype.format = function() {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
-    return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-  });
+$(".answer-write button[type=submit]").click(addAnswer);
+
+function addAnswer(e) {
+    e.preventDefault();
+    console.log("click!!");
+
+    var queryString = $(".answer-write").serialize();
+    console.log("query: " + queryString);
+
+    var url = $(".answer-write").attr("action");
+    console.log("url: " + url);
+
+    $.ajax({
+        type: 'post',
+        url: url,
+        data: queryString,
+        dataType: 'json',
+        error: function (xhr, status) {
+            console.log("Error");
+            console.log("xhr: " + xhr);
+            console.log("status: " + status);
+            alert(xhr.message);
+        },
+        success: function (data, status) {
+            console.log("Success");
+            console.log("data: " + data);
+            console.log("status: " + status);
+            var answerTemplate = $("#answerTemplate").html();
+            var template = answerTemplate.format(data.writer.name, data.formattedWriteDateTime, data.contents, data.question.id, data.id);
+            $(".qna-comment-slipp-articles").append(template);
+        }
+    })
+}
+
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+            ;
+    });
 };
