6d72f4af26297550270349812b524bd20004ea04
dong <ultraraptor@naver.com>
2021-03-25 16:41:55 +0900

    AnswerController의 일정 기능을 AnswerService로 이동함.
    - 리소스 전달하는 컨트롤러 고유의 기능만 남기고 서비스 처리 로직 부분은 서비스객체로 이관함

--- src/main/java/com/codessquad/qna/controller/AnswerController.java
+++ src/main/java/com/codessquad/qna/controller/AnswerController.java
@@ -5,6 +5,7 @@ import com.codessquad.qna.domain.AnswerRepository;
 import com.codessquad.qna.domain.QuestionRepostory;
 import com.codessquad.qna.domain.User;
 import com.codessquad.qna.exception.NotFoundException;
+import com.codessquad.qna.service.AnswerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -21,35 +22,21 @@ public class AnswerController {
 
     private static final Logger logger = LoggerFactory.getLogger(AnswerController.class);
 
-    private final QuestionRepostory questionRepostory;
-    private final AnswerRepository answerRepository;
+    private final AnswerService answerService;
 
-    public AnswerController(QuestionRepostory questionRepostory, AnswerRepository answerRepository) {
-        this.questionRepostory = questionRepostory;
-        this.answerRepository = answerRepository;
+    public AnswerController(AnswerService answerservice) {
+        this.answerService = answerservice;
     }
 
     @PostMapping("/qna/{questionId}/answers")
     public String createAnswer(@PathVariable Long questionId, String contents, HttpSession session) {
-        if (!isLoginUser(session)) {
-            logger.info("답변달기 - 실패 : 권한(로그인)되지 않은 사용자의 답변달기 시도가 실패함");
-            return "redirect:/user/login";
-        }
-        User loginUser = getLoginUser(session);
-        Answer answer = new Answer(loginUser, questionRepostory.getOne(questionId), contents);
-        answerRepository.save(answer);
-        logger.info("답변달기 - 성공 : 답변이 정상적으로 추가됨");
+        answerService.createAnswer(questionId,contents,session);
         return String.format("redirect:/qna/%d", questionId);
     }
 
     @DeleteMapping("/qna/{questionId}/answers/{answerId}")
     public String removeAnswer(@PathVariable Long questionId, @PathVariable Long answerId, HttpSession session) {
-        User ownerUser = questionRepostory.findById(questionId).orElseThrow(NotFoundException::new).getWriter();
-        if (!isValidUser(session, ownerUser)) {
-            logger.info("답변달기 - 실패 : 권한(로그인)되지 않은 사용자의 답변달기 시도가 실패함");
-            return String.format("redirect:/qna/%d", questionId);
-        }
-        answerRepository.delete(answerRepository.findById(answerId).orElseThrow(NotFoundException::new));
+        answerService.removeAnswer(questionId,answerId,session);
         return String.format("redirect:/qna/%d", questionId);
     }
 
---
+++ src/main/java/com/codessquad/qna/service/AnswerService.java
@@ -0,0 +1,55 @@
+package com.codessquad.qna.service;
+
+import com.codessquad.qna.controller.AnswerController;
+import com.codessquad.qna.domain.Answer;
+import com.codessquad.qna.domain.AnswerRepository;
+import com.codessquad.qna.domain.QuestionRepostory;
+import com.codessquad.qna.domain.User;
+import com.codessquad.qna.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+import static com.codessquad.qna.utils.SessionUtil.*;
+
+@Service
+public class AnswerService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AnswerService.class);
+
+    private final QuestionRepostory questionRepostory;
+    private final AnswerRepository answerRepository;
+
+
+    public AnswerService(QuestionRepostory questionRepostory, AnswerRepository answerRepository) {
+        this.questionRepostory = questionRepostory;
+        this.answerRepository = answerRepository;
+    }
+
+
+    public void createAnswer(Long questionId, String contents, HttpSession session) {
+        if (!isLoginUser(session)) {
+            logger.info("답변달기 - 실패 : 권한(로그인)되지 않은 사용자의 답변달기 시도가 실패함");
+            //return "redirect:/user/login";
+            //@Todo 예외처리
+        }
+        User loginUser = getLoginUser(session);
+        Answer answer = new Answer(loginUser, questionRepostory.getOne(questionId), contents);
+        answerRepository.save(answer);
+        logger.info("답변달기 - 성공 : 답변이 정상적으로 추가됨");
+    }
+
+
+    public void removeAnswer(Long questionId, Long answerId, HttpSession session) {
+        User ownerUser = questionRepostory.findById(questionId).orElseThrow(NotFoundException::new).getWriter();
+        if (!isValidUser(session, ownerUser)) {
+            logger.info("답변달기 - 실패 : 권한(로그인)되지 않은 사용자의 답변달기 시도가 실패함");
+            //return String.format("redirect:/qna/%d", questionId);
+            //@Todo 에외처리
+        }
+        answerRepository.delete(answerRepository.findById(answerId).orElseThrow(NotFoundException::new));
+
+    }
+}
