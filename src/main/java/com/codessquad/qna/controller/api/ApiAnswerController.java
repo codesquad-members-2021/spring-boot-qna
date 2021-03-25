0fba8e1de2cae2addb7b050c854bbd880c32f6f2
정이삭 <isaac56@naver.com>
2021-03-25 16:13:57 +0900

    Change AnswerController to ApiAnswerController
    1. AnswerController를 ApiAnswerController로 이름 변경했습니다.
    2. RestController로 변경하여 json을 반환하도록 했습니다.

--- src/main/java/com/codessquad/qna/controller/api/ApiAnswerController.java
+++ src/main/java/com/codessquad/qna/controller/api/ApiAnswerController.java
@@ -1,36 +1,32 @@
-package com.codessquad.qna.controller;
+package com.codessquad.qna.controller.api;
 
+import com.codessquad.qna.entity.Answer;
 import com.codessquad.qna.entity.User;
 import com.codessquad.qna.service.AnswerService;
 import com.codessquad.qna.util.HttpSessionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
-@Controller
-@RequestMapping("/questions/{questionId}/answers")
-public class AnswerController {
+@RestController
+@RequestMapping("/api/questions/{questionId}/answers")
+public class ApiAnswerController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final AnswerService answerService;
 
     @Autowired
-    public AnswerController(AnswerService answerService) {
+    public ApiAnswerController(AnswerService answerService) {
         this.answerService = answerService;
     }
 
     @PostMapping
-    public String create(@PathVariable long questionId, String contents, HttpSession session) {
+    public Answer create(@PathVariable long questionId, String contents, HttpSession session) {
         logger.debug("{}번 질문에 답변 생성 요청", questionId);
         User user = HttpSessionUtils.getUser(session);
-        answerService.addAnswer(questionId, user, contents);
-        return "redirect:/questions/" + questionId;
+        return answerService.addAnswer(questionId, user, contents);
     }
 
     @DeleteMapping("/{answerId}")
