8bc2dcda1930f468a7d18f841acc2295cf1e148e
bibi <bibi6666667@kakao.com>
2021-03-26 16:14:24 +0900

    Feat : 로그인 과정에 필요한 아이디,비밀번호 확인 메서드 추가

--- src/main/java/com/codessquad/qna/repository/UserRepository.java
+++ src/main/java/com/codessquad/qna/repository/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByUserId(String userId);
     List<User> findAll();
+    Optional<User> findByUserIdAndPassword(String userId, String password);
 }
