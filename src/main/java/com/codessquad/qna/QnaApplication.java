3b2abbc6d9d9e9b89cc54e8e85527894a8453d1a
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-27 17:04:17 +0900

    build : swagger를 추가하라
    - swagger를 이용해서 api를 자동 문서화하는것을 해 보았다

--- src/main/java/com/codessquad/qna/QnaApplication.java
+++ src/main/java/com/codessquad/qna/QnaApplication.java
@@ -3,13 +3,14 @@ package com.codessquad.qna;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableSwagger2
 public class QnaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QnaApplication.class, args);
     }
-
 }
