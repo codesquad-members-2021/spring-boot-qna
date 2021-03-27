ddc19cbb6e39e5115bd82349bf2ff4a4c151b01d
pbg0205 <pbg0205@naver.com>
2021-03-28 04:33:51 +0900

    feat : pagination 구현
    - repository : Crud Repository -> Jpa Repository로 변경
    
    - PageUtil을 이용한 pagination 구현

--- src/main/java/com/codessquad/qna/service/QuestionService.java
+++ src/main/java/com/codessquad/qna/service/QuestionService.java
@@ -3,10 +3,14 @@ package com.codessquad.qna.service;
 import com.codessquad.qna.domain.question.Question;
 import com.codessquad.qna.domain.question.QuestionRepository;
 import com.codessquad.qna.exception.QuestionNotFoundException;
+import com.codessquad.qna.web.PageUtil;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
 
 @Service
 public class QuestionService {
@@ -34,8 +38,19 @@ public class QuestionService {
     /**
      * 질문 리스트 조회
      */
-    public List<Question> findUnRemovedList() {
-        return questionRepository.findAllByDeletedFalse();
+    public Page<Question> findUnRemovedList(Pageable pageable) {
+        int page = pageable.getPageNumber() == 0 ? 0 : pageable.getPageNumber() - 1;
+        pageable = PageRequest.of(page, 5, Sort.Direction.DESC, "id");
+        return questionRepository.findAllByDeletedFalse(pageable);
+    }
+
+    /**
+     * 현재 페이지, 전체 페이지 조회
+     */
+    public PageUtil getPageUtil(Page<Question> questions) {
+        int currentPage = questions.getNumber() + 1;
+        int totalPages = questions.getTotalPages();
+        return new PageUtil(currentPage, totalPages);
     }
 
     /**
---
+++ src/main/java/com/codessquad/qna/web/PageUtil.java
@@ -0,0 +1,77 @@
+package com.codessquad.qna.web;
+
+import io.swagger.models.auth.In;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageUtil {
+    private static final int PAGE_COUNT = 6;
+
+    private int previousPage;
+    private List<Integer> pages;
+    private int nextPage;
+    private int max;
+    private int currentPage;
+
+    public PageUtil(int currentPage, int totalPages) {
+        this.previousPage = 0;
+        this.currentPage = currentPage;
+        this.max = totalPages;
+        this.pages = createList();
+        this.nextPage = createNext();
+    }
+
+    private List<Integer> createList() {
+        List<Integer> list = new ArrayList<>();
+
+        if (max <= 5) {
+            loopList(list, 1, max);
+            return list;
+        }
+
+        if (currentPage / PAGE_COUNT < 1) {
+            loopList(list, 1, 5);
+            return list;
+        }
+        int multi = currentPage / PAGE_COUNT < 2 ? 1 : currentPage / PAGE_COUNT - 1;
+        int start = multi * 5 + 1;
+        int last = start + 4;
+
+        if (last > max) {
+            last = max;
+        }
+
+        loopList(list, start, last);
+        this.previousPage = start - 1;
+
+        return list;
+    }
+
+    private void loopList(List<Integer> list, int start, int last) {
+        for (int pageNumber = start; pageNumber <= last; pageNumber++) {
+            list.add(pageNumber);
+        }
+    }
+
+    private int createNext() {
+        int next = pages.get(pages.size() - 1);
+        if (next == max) {
+            return 0;
+        }
+        return next + 1;
+    }
+
+    public int getPreviousPage() {
+        return previousPage;
+    }
+
+    public List<Integer> getPages() {
+        return pages;
+    }
+
+    public int getNextPage() {
+        return nextPage;
+    }
+}
