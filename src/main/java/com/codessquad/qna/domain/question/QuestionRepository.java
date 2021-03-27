cf53c0a2c70ccb7dc60597901010ca41a3b5496f
pbg0205 <pbg0205@naver.com>
2021-03-28 04:40:21 +0900

    chore : 불필요 메서드 제거

--- src/main/java/com/codessquad/qna/domain/question/QuestionRepository.java
+++ src/main/java/com/codessquad/qna/domain/question/QuestionRepository.java
@@ -4,9 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<Question> findAll();
     Page<Question> findAllByDeletedFalse(Pageable pageable);
 }
