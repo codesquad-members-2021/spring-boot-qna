3e610b851c87260df6b3d48af8cca1813fa51741
dong <ultraraptor@naver.com>
2021-03-28 17:24:31 +0900

    @RequestMapping으로 중복을 제거

--- src/main/java/com/codessquad/qna/controller/AnswerController.java
+++ src/main/java/com/codessquad/qna/controller/AnswerController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("/qna/{questionId}")
 public class AnswerController {
 
     private static final Logger logger = LoggerFactory.getLogger(AnswerController.class);
@@ -21,13 +23,13 @@ public class AnswerController {
         this.answerService = answerservice;
     }
 
-    @PostMapping("/qna/{questionId}/answers")
+    @PostMapping("/answers")
     public String create(@PathVariable Long questionId, String contents, HttpSession session) {
         answerService.createAnswer(questionId, contents, session);
         return ("redirect:/qna/" + questionId);
     }
 
-    @DeleteMapping("/qna/{questionId}/answers/{answerId}")
+    @DeleteMapping("/answers/{answerId}")
     public String remove(@PathVariable Long questionId, @PathVariable Long answerId, HttpSession session) {
         answerService.removeAnswer(questionId, answerId, session);
         return ("redirect:/qna/" + questionId);
