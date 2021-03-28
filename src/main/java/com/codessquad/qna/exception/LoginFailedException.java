9b006695178282a72295fa82ca56a7d6588f4e64
dong <ultraraptor@naver.com>
2021-03-28 16:36:30 +0900

    얽혀있는 문제 해결 1단계
    LoginFailedException 에 비어있는 기본 생성자 제거

--- src/main/java/com/codessquad/qna/exception/LoginFailedException.java
+++ src/main/java/com/codessquad/qna/exception/LoginFailedException.java
@@ -1,10 +1,6 @@
 package com.codessquad.qna.exception;
 
 public class LoginFailedException extends RuntimeException {
-
-    public LoginFailedException() {
-    }
-
     public LoginFailedException(String message) {
         super(message);
     }
