6667d7315f904508cc467e2f5fcff4bf730a5927
Jihye Woo <woojihye2339@gmail.com>
2021-03-28 13:03:08 +0900

    feat : 답변 데이터를 서버로 전송
      > serialize()
        - form에 입력한 답변 데이터를 key-value 형태로 묶어준다
        - contents="입력한 답변"
    
      > ajax 사용
        - ajax을 통해 서버로 데이터 전송
        - type = "post"
        - url = 답변하기 form의 action url

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -2,8 +2,25 @@ $(".submit-write button[type=submit]").click(addAnswer);
 
 function addAnswer(e) {
     e.preventDefault();
+    var queryString = $(".submit-write").serialize();
+
+    var url = $(".submit-write").attr("action");
+    $.ajax({
+        type : 'post',
+        url : url,
+        data : queryString,
+        dataType : 'json',
+        error: onError,
+        success : onSuccess,
+    });
 }
 
+function onError(){
+    console.log("failure");
+}
+function onSuccess(){
+    console.log("success");
+}
 
 String.prototype.format = function () {
     var args = arguments;
