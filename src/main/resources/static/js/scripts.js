a058e12e29b1736ecb1f3541751aa1ca94b4c289
Jihye Woo <woojihye2339@gmail.com>
2021-03-28 12:56:07 +0900

    feat : 버튼 클릭 이벤트 핸들러 등록
      > script.js에 이벤트 핸들러 추가
        - '답변하기' 버튼 클릭 시, addAnswer() 함수 호출

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,16 @@
-String.prototype.format = function() {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
-    return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-  });
+$(".submit-write button[type=submit]").click(addAnswer);
+
+function addAnswer(e) {
+    e.preventDefault();
+}
+
+
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+            ;
+    });
 };
