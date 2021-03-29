748e03186ef294f2490ba7b89f7ed65975781fe2
Jihye Woo <woojihye2339@gmail.com>
2021-03-30 02:19:16 +0900

    feat : Swagger 문서화
      > build.gradle에서 의존성 설정
       - implementation 설정으로 'io.springfox:springfox-boot-starter:3.0.0'
    
     > MvcConfig 설정
      - Swagger Configuration 설정

--- src/main/java/com/codessquad/qna/web/config/MvcConfig.java
+++ src/main/java/com/codessquad/qna/web/config/MvcConfig.java
@@ -1,13 +1,23 @@
 package com.codessquad.qna.web.config;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.builders.ApiInfoBuilder;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableJpaAuditing
+@EnableSwagger2
 public class MvcConfig implements WebMvcConfigurer {
 
     @Override
@@ -16,5 +26,23 @@ public class MvcConfig implements WebMvcConfigurer {
 
         registry.addViewController("/users/form").setViewName("user/form");
         registry.addViewController("/users/login-form").setViewName("user/loginForm");
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("my-slipp")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("My Slipp Q&A Board")
+                .contact(new Contact("woody", "https://github.com/jihye-woo/", "woojihye2339@gmail.com"))
+                .version("1.0.0")
+                .build();
     }
 }
