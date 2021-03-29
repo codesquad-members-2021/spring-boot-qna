363782c5b71cec92ea73097d7220694ed30869ed
jeong-inho <derosatam76@gmail.com>
2021-03-29 18:16:40 +0900

    Feat : Answer 클래스 생성
    답변 기능을 위해 Answer entity에 해당하는 Answer 클래스 생성

---
+++ src/main/java/com/codessquad/qna/model/Answer.java
@@ -0,0 +1,91 @@
+package com.codessquad.qna.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_answer_question"))
+    private Question question;
+
+    @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_answer_writer"))
+    private User writer;
+
+    @Lob
+    @Column(nullable = false)
+    private String contents;
+
+    @Column(nullable = false)
+    private String dateTime;
+
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public void save(User writer, Question question) {
+        this.writer = writer;
+        this.question = question;
+        this.dateTime = LocalDateTime.now().format(dateTimeFormatter);
+    }
+
+    public boolean matchWriter(User user) {
+        return this.writer.matchUserId(user);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public User getWriter() {
+        return writer;
+    }
+
+    public void setWriter(User writer) {
+        this.writer = writer;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "id=" + id +
+                ", question=" + question +
+                ", writer=" + writer +
+                ", contents='" + contents + '\'' +
+                ", dateTime='" + dateTime + '\'' +
+                '}';
+    }
+}
