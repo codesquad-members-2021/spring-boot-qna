3279e713793c007faca71138cce6404725d2e10c
jeong-inho <derosatam76@gmail.com>
2021-03-29 18:17:52 +0900

    Feat : AnswerRepository 생성
    Answer db에 접근하기 위해 AnswerRepository 인터페이스 생성

---
+++ src/main/java/com/codessquad/qna/repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.codessquad.qna.repository;
+
+import com.codessquad.qna.model.Answer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AnswerRepository extends CrudRepository<Answer, Long> {
+}
