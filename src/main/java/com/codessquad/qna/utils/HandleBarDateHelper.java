7c2e590c27bd34a6a58fcac4ccddb96e367ad51e
dong <ultraraptor@naver.com>
2021-03-29 18:06:33 +0900

    시간이 포멧팅되지 않아 이상한 문자가 섞여있던 문제가 발생 오류를 수정합니다
    - 핸들바 핼퍼 클래스를 하나 제작합니다
    - list와 show 페이지에 {{date createdDateTime}} 와 같은 형식으로 적용합니다

---
+++ src/main/java/com/codessquad/qna/utils/HandleBarDateHelper.java
@@ -0,0 +1,15 @@
+package com.codessquad.qna.utils;
+
+import pl.allegro.tech.boot.autoconfigure.handlebars.HandlebarsHelper;
+
+import java.time.LocalDateTime;
+
+import static com.codessquad.qna.utils.DateUtil.DATE_AND_TIME_FORMAT;
+
+@HandlebarsHelper
+public class HandleBarDateHelper {
+    public String date(LocalDateTime time) {
+        return time.format(DATE_AND_TIME_FORMAT);
+        //{{#date createdatetime}}
+    }
+}
