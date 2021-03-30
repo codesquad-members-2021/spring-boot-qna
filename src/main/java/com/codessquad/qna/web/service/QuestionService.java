38cd26911ec971dda598b2871b016ec8c47ba5f8
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-30 20:32:11 +0900

    feat : 페이지네이션 기능을 구현하라
    - 질문목록 페이지의 맨 아래에 출력될 페이지 리스트를 반환하는 pageList메서드를 작성했다
    - 매개변수는 현재 페이지의 번호이다. 이걸로 시작페이지와 끝 페이지를 반환한다

--- src/main/java/com/codessquad/qna/web/service/QuestionService.java
+++ src/main/java/com/codessquad/qna/web/service/QuestionService.java
@@ -8,9 +8,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class QuestionService {
     private final QuestionRepository questionRepository;
+    private static final int PAGE_SIZE = 5;
+    private static final int BLOCK_SIZE = 5;
 
     public QuestionService(QuestionRepository questionRepository) {
         this.questionRepository = questionRepository;
@@ -22,8 +27,22 @@ public class QuestionService {
         questionRepository.save(question);
     }
 
-    public Iterable<Question> questions() {
-        return questionRepository.findAllByDeleted(false, PageRequest.of(0,5));
+    public Iterable<Question> questions(int pageNumber) {
+        return questionRepository.findAllByDeleted(false, PageRequest.of(pageNumber, PAGE_SIZE));
+    }
+
+    public List<Long> pageList(long currentPage) {
+        List<Long> list = new ArrayList<>();
+        long startPage = currentPage - (currentPage % BLOCK_SIZE);
+        long endPage = startPage + BLOCK_SIZE;
+        long numberOfQuestions = questionRepository.countAllByDeletedFalse();
+        if (numberOfQuestions / PAGE_SIZE < endPage) {
+            endPage = numberOfQuestions / PAGE_SIZE;
+        }
+        for (long i = startPage; i < endPage; i++) {
+            list.add(i);
+        }
+        return list;
     }
 
     public Question questionDetail(long id) {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,113 +5,65 @@ VALUES (2, 'Scarecrow', '1234', 'Jonathan Crane', 'crow@bar.me', {ts '2021-03-12
         {ts '2021-03-12 18:10:32'});
 
 INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('1 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('2 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, TRUE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('3 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, TRUE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('4 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('5 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('6 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('7 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('8 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('9 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('10 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('11 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('12 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('13 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('14 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('15 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('16 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('17 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
-INSERT INTO QUESTION (TITLE, CONTENTS, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
-VALUES ('18 Riddle: "What can''t you have for breakfast and lunch."',
-        'Answer: Dinner',
-        {ts '2021-03-12 18:23:32'},
-        {ts '2021-03-12 18:23:32'},
-        1, FALSE);
+VALUES ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE),
+       ('Riddle: "What can''t you have for breakfast and lunch."', 'Answer: Dinner', {ts '2021-03-12 18:23:32'}, {ts '2021-03-12 18:23:32'}, 1, FALSE);
+
 
 INSERT INTO ANSWER(ID, CONTENTS, QUESTION_ID, CREATED_DATE_TIME, MODIFIED_DATE_TIME, WRITER_ID, DELETED)
 values (NULL, 'Ha!', 1, {ts '2021-03-12 18:25:32'}, {ts '2021-03-12 18:25:32'}, 1, FALSE);
