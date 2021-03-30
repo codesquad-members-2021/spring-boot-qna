276bf41d1c031ed2aabc3e453b39e5599ae0472d
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-03-30 20:30:56 +0900

    test : 페이지네이션 테스트 코드를 작성하라
    - 페이지 넘버를 매개변수로 사용해서 페이지리스트를 획득할 수 있어야 한다
    - 맨 앞의 값, 중간값, 맨 뒤의 값을 넣었을 때, 올바른 페이지 리스트가 반환되어야만 한다

---
+++ src/test/java/com/codessquad/qna/web/service/QuestionServiceTest.java
@@ -0,0 +1,65 @@
+package com.codessquad.qna.web.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class QuestionServiceTest {
+    @Autowired
+    private QuestionService questionService;
+
+    @Test
+    @DisplayName("현재 페이지가 0번이면 0부터 4까지 나와야 합니다")
+    void testWhenCurrentPageIsZero() {
+        long currentPage = 0;
+        List<Long> expectedPageList = new ArrayList<>();
+        for (long i = 0; i < 5; i++) {
+            expectedPageList.add(i);
+        }
+        List<Long> pageList = questionService.pageList(currentPage);
+        assertThat(pageList).isEqualTo(expectedPageList);
+    }
+
+    @Test
+    @DisplayName("현재 페이지가 3번이면 0부터 4까지 나와야 합니다")
+    void testWhenCurrentPageIsThree() {
+        long currentPage = 3;
+        List<Long> expectedPageList = new ArrayList<>();
+        for (long i = 0; i < 5; i++) {
+            expectedPageList.add(i);
+        }
+        List<Long> pageList = questionService.pageList(currentPage);
+        assertThat(pageList).isEqualTo(expectedPageList);
+    }
+
+    @Test
+    @DisplayName("현재 페이지가 4번이면 0부터 4까지 나와야 합니다")
+    void testWhenCurrentPageIsFour() {
+        long currentPage = 3;
+        List<Long> expectedPageList = new ArrayList<>();
+        for (long i = 0; i < 5; i++) {
+            expectedPageList.add(i);
+        }
+        List<Long> pageList = questionService.pageList(currentPage);
+        assertThat(pageList).isEqualTo(expectedPageList);
+    }
+
+    @Test
+    @DisplayName("현재 페이지가 5번이면 5부터 9까지 나와야 합니다")
+    void testWhenCurrentPageIsFive() {
+        long currentPage = 5;
+        List<Long> expectedPageList = new ArrayList<>();
+        for (long i = 5; i < 10; i++) {
+            expectedPageList.add(i);
+        }
+        List<Long> pageList = questionService.pageList(currentPage);
+        assertThat(pageList).isEqualTo(expectedPageList);
+    }
+}
