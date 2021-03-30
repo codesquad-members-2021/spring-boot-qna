f720b01d6369b4dc3bdcd9498d05bf0b7cbf92bd
jeong-inho <derosatam76@gmail.com>
2021-03-30 17:11:51 +0900

    Feat : ErrorMessage 이넘에 질문 삭제에 실패할 때 보여줄 DELETE_FAILED 상수 추가

--- src/main/java/com/codessquad/qna/utils/ErrorMessage.java
+++ src/main/java/com/codessquad/qna/utils/ErrorMessage.java
@@ -5,6 +5,7 @@ public enum ErrorMessage {
     USER_NOT_FOUND("해당 유저를 찾을 수 없습니다."),
     QUESTION_NOT_FOUND("해당 질문을 찾을 수 없습니다."),
     ANSWER_NOT_FOUND("해당 답변을 찾을 수 없습니다."),
+    DELETE_FAILED("해당 질문을 삭제할 수 없습니다."),
     INVALID_USER("접근 권한이 없는 유저입니다."),
     DUPLICATED_ID("이미 사용중인 아이디입니다."),
     LOGIN_FAILED("아이디 또는 비밀번호가 틀립니다. 다시 로그인 해주세요."),
