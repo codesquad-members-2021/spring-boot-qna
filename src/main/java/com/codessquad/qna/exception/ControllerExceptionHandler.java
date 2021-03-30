93d6463544c17fd2986a2d18c02749bd0ab15786
dong <ultraraptor@naver.com>
2021-03-30 12:44:37 +0900

    에러페이지로 이동하는 경우는 중복이 많아 프라이빗 매서드로 변경

--- src/main/java/com/codessquad/qna/exception/ControllerExceptionHandler.java
+++ src/main/java/com/codessquad/qna/exception/ControllerExceptionHandler.java
@@ -12,32 +12,33 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(LoginFailedException.class)
     public String loginFailed(LoginFailedException exception, Model model) {
-        initializeModel(exception, model);
-        return "/error/login_failed";
+        return errorPageSender(exception,model);
     }
 
     @ExceptionHandler(NotFoundException.class)
     public String notFoundException(NotFoundException exception, Model model) {
-        initializeModel(exception, model);
-        return "/error/error_page";
+        return errorPageSender(exception,model);
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     public String unauthorizedException(UnauthorizedException exception, Model model) {
-        initializeModel(exception, model);
-        return "/error/error_page";
+        return errorPageSender(exception,model);
     }
 
     @ExceptionHandler(UnacceptableDuplicationException.class)
     public String unacceptableDuplicationException(UnacceptableDuplicationException exception, Model model) {
-        initializeModel(exception, model);
-        return "/error/error_page";
+        return errorPageSender(exception,model);
     }
 
     private void initializeModel(Exception exception, Model model) {
         String exceptionMessage = exception.getMessage();
         logger.warn(exceptionMessage);
         model.addAttribute("errorMessage", exceptionMessage);
+    }
+
+    private String errorPageSender(Exception exception, Model model) {
+        initializeModel(exception, model);
+        return "/error/error_page";
     }
 
 }
