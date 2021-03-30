e4e43f80726b269b6acb04c4930c4a6be7b193e2
dong <ultraraptor@naver.com>
2021-03-30 12:52:42 +0900

    주석제거

--- src/main/java/com/codessquad/qna/exception/UnauthorizedException.java
+++ src/main/java/com/codessquad/qna/exception/UnauthorizedException.java
@@ -5,6 +5,3 @@ public class UnauthorizedException extends RuntimeException {
         super(message);
     }
 }
-// 얼럿으로 왜 실패했는제
-// location >> 자바스크립트 코드
-// location.href = "something" 스크립트 코드 두개로 커버 가능!
