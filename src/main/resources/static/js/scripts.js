9cf03a47e3dbb64ee7afe65ccb93b0706cdbcc26
dong <ultraraptor@naver.com>
2021-03-31 15:13:06 +0900

    AJAX 기능을 이용해 답변을 추가하는 기능을 구현하는 자바스크립트 함수를 추가하였습니다
    - 자바스크립트 함수 createAnswer , $(document).ready(() (제이쿼리 사용) 을 추가

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -7,3 +7,32 @@ String.prototype.format = function() {
         ;
   });
 };
+
+
+$(document).ready(() => {//powered by pyro
+  let currHerf = location.pathname
+  $(".sub-nav-li").each((index, item) => {
+    let hrefOfItem = $(item).children().first().attr('href');
+    if (currHref === hrefOfItem) {
+      $(item).addClass('active');
+    }
+  });
+
+  $('#id-button-submit-answer').on('click', createAnswer);
+  $(".delete-answer-form button[type='submit']").on('click', deleteAnswer);
+})
+
+
+function createAnswer(event) {
+  let formElement = $(".submit-write");
+  let queryString = formElement.serialize();
+  let url = formElement.attr('action');
+
+  $.ajax({
+    type: 'post',
+    url: url,
+    data: queryString,
+    success: onCreateAnswerSuccess,
+    error: onCreateAnswerFailed
+  });
+}
