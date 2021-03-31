20d7c698b15ee0bf7d8a6e2a4f9235f830e673b3
dong <ultraraptor@naver.com>
2021-03-31 16:38:59 +0900

    콜백함수 작성 중 중간 커밋  .. 1단계

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,53 +1,58 @@
-String.prototype.format = function() {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
-    return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-  });
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+            ;
+    });
 };
 
 $(document).ready(() => {//powered by pyro
-  /*
-  let currHerf = location.pathname
-  $(".sub-nav-li").each((index, item) => {
+    /*
+    let currHerf = location.pathname
+    $(".sub-nav-li").each((index, item) => {
 
-    let hrefOfItem = $(item).children().first().attr('href');
-    if (currHref === hrefOfItem) {
-      $(item).addClass('active');
-    }
-  });
-  */
-  //@Todo 뭐하는건지 공부해보기
+      let hrefOfItem = $(item).children().first().attr('href');
+      if (currHref === hrefOfItem) {
+        $(item).addClass('active');
+      }
+    });
+    */
+    //@Todo 뭐하는건지 공부해보기
 
-  $('#id-button-submit-answer').on('click', createAnswer);
-  //$(".delete-answer-form button[type='submit']").on('click', deleteAnswer);
+    $('#id-button-submit-answer').on('click', createAnswer);
+    //$(".delete-answer-form button[type='submit']").on('click', deleteAnswer);
 })
 
 
 function createAnswer(event) {
-  console.log("createAnswer");
-  event.preventDefault();
-  let formElement = $(".submit-write");
-  let queryString = formElement.serialize();
-  let url = formElement.attr('action');
+    console.log("createAnswer");
+    event.preventDefault();
+    let formElement = $(".submit-write");
+    let queryString = formElement.serialize();
+    let url = formElement.attr('action');
 
-  $.ajax({
-    type: 'post',
-    url: url,
-    data: queryString,
-    success: onCreateAnswerSuccess,
-    error: onCreateAnswerFailed
-  });
+    $.ajax({
+        type: 'post',
+        url: url,
+        data: queryString,
+        success: onCreateAnswerSuccess,
+        error: onCreateAnswerFailed
+    });
 }
 
-function onCreateAnswerSuccess() {
-  console.log("Success");
+function onCreateAnswerSuccess(data, status) {
+    console.log(status + " : " + data);
+    let answerTemplate = $("#answerTemplate").html();
+    let template = answerTemplate.format(data.writer.userId, data.formattedCreateDate, data.contents, data.question.id, data.id);
+    console.log("answerTemplate : " + answerTemplate);
+    console.log("template : " + template);
+    $(".qna-comment-slipp-articles").prepend(template);
+    $("textarea[name=contents]").val("");
 }
-
 
 function onCreateAnswerFailed() {
-  console.log("Failed T.T");
+    console.log("Failed T.T");
 }
 
