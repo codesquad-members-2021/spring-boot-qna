5df65233b6e6e7ae9593b5f2698d7def6f1a490e
정이삭 <isaac56@naver.com>
2021-03-31 16:56:01 +0900

    feat: Handle BindException, ConstraintViolationException in GlobalExceptionHandler
    GlobalExceptionHandler에서 BindException과 ConstraintViloationException을 처리하도록 수정

--- src/main/java/com/codessquad/qna/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/codessquad/qna/exceptionHandler/GlobalExceptionHandler.java
@@ -3,17 +3,21 @@ package com.codessquad.qna.exceptionHandler;
 import com.codessquad.qna.exception.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice(annotations = Controller.class)
 public class GlobalExceptionHandler {
     @ExceptionHandler({
             NotFoundException.class,
             UserAlreadyExistException.class,
-            CannotDeleteQuestionException.class
+            CannotDeleteQuestionException.class,
+            BindException.class,
+            ConstraintViolationException.class
     })
     public String exceptionPage(RuntimeException ex, Model model) {
         model.addAttribute("errorMessage", ex.getMessage());
