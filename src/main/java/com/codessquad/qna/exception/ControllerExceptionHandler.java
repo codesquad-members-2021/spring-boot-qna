0c100f9d2839b235cd7ab2e617f16f3ce52146c8
dong <ultraraptor@naver.com>
2021-03-31 16:18:57 +0900

    클라이언트에서 요청에 대해서
    - 성공했을때 >> 상태코드 200
    - 실패시 >> 상태코드 400 Bad request
    - 해당 페이지 세션의 상태에 따라 적절한 반응이 나가도록 수정

--- src/main/java/com/codessquad/qna/exception/ControllerExceptionHandler.java
+++ src/main/java/com/codessquad/qna/exception/ControllerExceptionHandler.java
@@ -2,15 +2,18 @@ package com.codessquad.qna.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
 
     @ExceptionHandler(LoginFailedException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String loginFailed(LoginFailedException exception, Model model) {
         return errorPageSender(exception, model);
     }
