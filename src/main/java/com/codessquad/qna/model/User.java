c8f10481a4b18668f0c559e80144f4aee47721b7
jeong-inho <derosatam76@gmail.com>
2021-03-31 12:28:01 +0900

    Style : User 클래스의 개행 추가

--- src/main/java/com/codessquad/qna/model/User.java
+++ src/main/java/com/codessquad/qna/model/User.java
@@ -4,16 +4,20 @@ import javax.persistence.*;
 
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable=false, length=20)
     private String userId;
+
     @Column(nullable = false)
     private String password;
+
     @Column(nullable = false)
     private String name;
+
     @Column(nullable = false)
     private String email;
 
