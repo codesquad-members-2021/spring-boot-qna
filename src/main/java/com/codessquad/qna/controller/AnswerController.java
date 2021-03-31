baaa61f39acde2933ae1b6c8903a51e7cdf67a80
95degree <95degree@naver.com>
2021-03-31 10:40:38 +0900

    feat #16 update_failed.html 추가
    - 기존 비밀번호가 틀릴시 update_failed.html로 넘어감

--- src/main/java/com/codessquad/qna/controller/AnswerController.java
+++ src/main/java/com/codessquad/qna/controller/AnswerController.java
@@ -1,9 +1,12 @@
 package com.codessquad.qna.controller;
 
 import com.codessquad.qna.HttpSessionUtils;
+import com.codessquad.qna.domain.Answer;
 import com.codessquad.qna.domain.User;
 import com.codessquad.qna.service.AnswerService;
+import com.codessquad.qna.service.QuestionService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
